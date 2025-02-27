0b2b0bdff1caf0a213592777b781e149b7274f12
Tim Rickwood <48654585+timrickwood@users.noreply.github.com>
2025-02-27 14:22:45 +0000

    FDOS-166 update placeholder readme (#14)
    * FDOS-166 Work in progress
    
    * FDOS-166 Update readme and pr template
    
    * FDOS-166 Branch name and commit message case handling
    
    * FDOS-166 Identify githook by name in readme

--- scripts/githooks/check-branch-name.sh
+++ scripts/githooks/check-branch-name.sh
@@ -4,9 +4,10 @@ set -e
 exit_code=0
 
 function check_git_branch_name {
-    VALID_FORMAT=$(check_git_branch_name_format $1)
+    BRANCH=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+    VALID_FORMAT=$(check_git_branch_name_format "$BRANCH")
     if [[  ! -z "$VALID_FORMAT" ]] ; then
-      echo "Branch name $BRANCH_NAME does not match the naming pattern"
+      echo "Branch name $1 does not match the naming pattern"
       echo Naming pattern = task or hotfix/hyphenated JIRA ref followed by underscore or hyphen followed by max 45 alphanumerics, hyphens or underscores starting with an alphanumeric
       return 1
     fi
@@ -14,7 +15,7 @@ function check_git_branch_name {
 
 function check_git_branch_name_format {
     BUILD_BRANCH="$1"
-    if [ "$BUILD_BRANCH" != 'main' ] && ! [[ $BUILD_BRANCH =~ (hotfix|task)\/(FDOS|DOSIS|SIA)-([0-9]{1,5})(_|-)([A-Za-z0-9])([A-Za-z0-9_-]{9,45})$ ]]  ; then
+    if [ "$BUILD_BRANCH" != 'main' ] && ! [[ $BUILD_BRANCH =~ (hotfix|task)\/(fdos|dosis|sia)-([0-9]{1,5})(_|-)([A-Za-z0-9])([A-Za-z0-9_-]{9,45})$ ]]  ; then
       echo 1
     fi
 }
