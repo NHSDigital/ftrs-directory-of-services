286396ce20bd4619aaecfaaaca811e8047161a59
Michaela McGrath <michaela.mcgrath1@nhs.net>
2025-03-07 15:41:51 +0000

    FDOS-181 Setup secrets for future Lambda (#25)
    * FDOS-181 Setup rds secrets for lambda functions
    
    * FDOS-181 Fix terraform format script
    
    * FDOS-181 Fix terraform format script

--- infrastructure/data_migration.tfvars
+++ infrastructure/data_migration.tfvars
@@ -4,9 +4,10 @@ project      = "ftrs-dos-data-migration"
 migration_pipeline_store_bucket_name = "pipeline-store"
 s3_versioning                        = false
 
-rds_database       = "data_migration"
-rds_port           = 5432
-rds_engine         = "aurora-postgresql"
-rds_engine_version = "16.4"
-rds_engine_mode    = "provisioned"
-rds_instance_class = "db.serverless"
+source_rds_database = "data_migration"
+target_rds_database = "Core"
+rds_port            = 5432
+rds_engine          = "aurora-postgresql"
+rds_engine_version  = "16.4"
+rds_engine_mode     = "provisioned"
+rds_instance_class  = "db.serverless"
--- infrastructure/stacks/data_migration/rds.tf
+++ infrastructure/stacks/data_migration/rds.tf
@@ -38,7 +38,7 @@ module "rds" {
   manage_master_user_password = false
   master_username             = random_pet.rds_username[0].id
   master_password             = random_password.rds_password[0].result
-  database_name               = var.rds_database
+  database_name               = var.source_rds_database
 
   iam_database_authentication_enabled = true
 
@@ -54,7 +54,7 @@ module "rds" {
 resource "aws_secretsmanager_secret" "rds_username" {
   count = local.deploy_databases ? 1 : 0
 
-  name = "${var.project}/${var.environment}/rds_username"
+  name = "${var.project}/${var.environment}/rds-username"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_username" {
@@ -67,7 +67,7 @@ resource "aws_secretsmanager_secret_version" "rds_username" {
 resource "aws_secretsmanager_secret" "rds_password" {
   count = local.deploy_databases ? 1 : 0
 
-  name = "${var.project}/${var.environment}/rds_password"
+  name = "${var.project}/${var.environment}/rds-password"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_password" {
@@ -75,4 +75,42 @@ resource "aws_secretsmanager_secret_version" "rds_password" {
 
   secret_id     = aws_secretsmanager_secret.rds_password[0].id
   secret_string = random_password.rds_password[0].result
+}
+
+resource "aws_secretsmanager_secret" "source_rds_credentials" {
+  count = local.deploy_databases ? 1 : 0
+
+  name = "/${var.project}/${var.environment}/source-rds-credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "source_rds_credentials" {
+  count = local.deploy_databases ? 1 : 0
+
+  secret_id = aws_secretsmanager_secret.source_rds_credentials[0].id
+  secret_string = jsonencode({
+    host     = "NOT_SET",
+    port     = var.rds_port,
+    username = "NOT_SET",
+    password = "NOT_SET",
+    dbname   = var.source_rds_database
+  })
+}
+
+resource "aws_secretsmanager_secret" "target_rds_credentials" {
+  count = local.deploy_databases ? 1 : 0
+
+  name = "/${var.project}/${var.environment}/target-rds-credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "target_rds_credentials" {
+  count = local.deploy_databases ? 1 : 0
+
+  secret_id = aws_secretsmanager_secret.target_rds_credentials[0].id
+  secret_string = jsonencode({
+    host     = "NOT_SET",
+    port     = var.rds_port,
+    username = "NOT_SET",
+    password = "NOT_SET",
+    dbname   = var.target_rds_database
+  })
 }
