67e41a012584942c6c333c3e9bf48df438bd5714
DomBaker <dominic.baker1@nhs.net>
2025-03-10 12:45:44 +0000

    DOSIS-418 Add lambda for gp search
    Signed-off-by: DomBaker <dominic.baker1@nhs.net>

---
+++ infrastructure/stacks/gp_search/lambda/variables.tf
@@ -0,0 +1,7 @@
+variable "gp_search_lambda_name" {
+  description = "Name of API Lambda for the GP Search API POC pipeline test"
+}
+
+variable "gp_search_lambda_tracing_mode" {
+  description = "Mode for X-Ray tracing for GP Search Lambda"
+}
