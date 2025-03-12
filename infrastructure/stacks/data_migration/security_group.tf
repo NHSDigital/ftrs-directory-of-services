3070db5d188d0863438579b3939818867adfb319
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-03-12 10:36:56 +0000

    FDOS-49 Allow vpn traffic to data migration rds cluster

--- infrastructure/stacks/data_migration/security_group.tf
+++ infrastructure/stacks/data_migration/security_group.tf
@@ -6,3 +6,12 @@ resource "aws_security_group" "rds_security_group" {
 
   vpc_id = data.aws_vpc.vpc.id
 }
+
+resource "aws_vpc_security_group_ingress_rule" "rds_allow_ingress_from_vpn" {
+  count                        = local.deploy_databases ? 1 : 0
+  security_group_id            = aws_security_group.rds_security_group[0].id
+  referenced_security_group_id = data.aws_security_group.vpn_security_group.id
+  from_port                    = var.rds_port
+  ip_protocol                  = data.aws_ec2_client_vpn_endpoint.client_vpn_endpoint.transport_protocol
+  to_port                      = var.rds_port
+}
