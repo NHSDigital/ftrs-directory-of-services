8d5845ad9dc4557d0b33f9893343add036261c0a
DomBaker <dominic.baker1@nhs.net>
2025-03-13 10:53:49 +0000

    DOSIS-418 Add gp search lambda and update base lambda module

--- infrastructure/gp_search.tfvars
+++ infrastructure/gp_search.tfvars
@@ -3,4 +3,3 @@ gp_search_bucket_name = "gp-search-s3"
 
 #lambda settings
 gp_search_lambda_name = "gp-search-lambda"
-gp_search_lambda_tracing_mode = Active
--- infrastructure/stacks/gp_search/lambda/functions/search_function.py
+++ infrastructure/stacks/gp_search/lambda/functions/search_function.py
@@ -1,5 +1,9 @@
+from aws_lambda_powertools import Tracer
 import json
 
+tracer = Tracer()  # Sets service via POWERTOOLS_SERVICE_NAME env var
+
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
     # TODO implement
     return {
