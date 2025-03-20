43d57e65ac1a8ca3ee100ea456d741dc4cd4f757
DomBaker <dominic.baker1@nhs.net>
2025-03-20 14:20:21 +0000

    DOSIS-418 Add archive file to gp search

--- infrastructure/stacks/gp_search/search_lambda.tf
+++ infrastructure/stacks/gp_search/search_lambda.tf
@@ -1,10 +1,16 @@
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "./lambda/functions/search_function.py"
+  output_path = "./lambda/functions_zip/search_function.zip"
+}
 module "lambda" {
-  source                = "../../modules/lambda"
-  source_path           = "./lambda/functions/search_function"
-  function_name         = "${var.project}-${var.gp_search_lambda_name}-${var.environment}"
-  description           = "GP search Lambda"
-  handler               = "index.lambda_handler"
-  attach_policy_jsons   = false
-  attach_tracing_policy = true
-  policy_jsons          = []
+  source                 = "../../modules/lambda"
+  source_path            = "./lambda/functions/search_function.py"
+  function_name          = "${var.project}-${var.gp_search_lambda_name}-${var.environment}"
+  description            = "GP search Lambda"
+  handler                = "index.lambda_handler"
+  attach_policy_jsons    = false
+  local_existing_package = "./lambda/functions_zip/search_function.zip"
+  attach_tracing_policy  = true
+  policy_jsons           = []
 }
