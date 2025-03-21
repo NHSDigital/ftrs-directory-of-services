10b0c3bcdc239c4baddf5e704fa360092879555e
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-03-21 15:27:41 +0000

    FDOS-184 Create extract lambda for data migration

--- infrastructure/stacks/data_migration/lambda.tf
+++ infrastructure/stacks/data_migration/lambda.tf
@@ -6,3 +6,35 @@ resource "aws_lambda_layer_version" "python_dependency_layer" {
   filename         = "${path.module}/${var.project}-python-dependency-layer-${var.application_tag}.zip"
   source_code_hash = filebase64sha256("${path.module}/${var.project}-python-dependency-layer-${var.application_tag}.zip")
 }
+
+module "extract_lambda" {
+  source                  = "../../modules/lambda"
+  function_name           = "${local.prefix}-${var.extract_name}${local.workspace_suffix}"
+  description             = "Lambda to extract data for the DoS mirgration"
+  handler                 = var.extract_lambda_handler
+  runtime                 = var.lambda_runtime
+  local_existing_package  = "${path.module}/${var.project}-lambda-${var.application_tag}.zip"
+  ignore_source_code_hash = false
+  timeout                 = var.extract_lambda_connection_timeout
+  memory_size             = var.extract_lambda_memory_size
+
+  subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
+  security_group_ids = [aws_security_group.extract_lambda_security_group.id]
+
+  policy_jsons = [data.aws_iam_policy_document.s3_access_policy.json]
+
+  layers = [aws_lambda_layer_version.python_dependency_layer.arn]
+}
+
+data "aws_iam_policy_document" "s3_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
