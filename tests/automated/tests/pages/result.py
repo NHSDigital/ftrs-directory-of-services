3b50c98180c3b0c512b716b631e70df067727827
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2025-03-21 16:32:39 +0000

    DOSIS-999 Added ui test and need to update readme

---
+++ tests/automated/tests/pages/result.py
@@ -0,0 +1,14 @@
+"""
+This module contains DuckDuckGoResultPage,
+the page object for the DuckDuckGo result page.
+"""
+
+from playwright.sync_api import Page
+from typing import List
+
+
+class UserTestMfaHelpPage:
+
+    def __init__(self, page: Page) -> None:
+        self.page = page
+        
