c675a5ae55fd10eb0750810bcbe216611ce247e4
emab1 <178916340+emab1@users.noreply.github.com>
2025-03-27 07:11:48 +0000

    FDOS-149 Add unit tests

--- services/data-migration/pipeline/transform.py
+++ services/data-migration/pipeline/transform.py
@@ -1,91 +1,58 @@
 import logging
 from datetime import datetime
 from pathlib import Path
+from typing import Any, Dict, List
 from uuid import uuid4
 
 import pandas as pd
-from pydantic import BaseModel
 
 from packages.data_models import Endpoints, Organisation
 
-# Logging has been created to track unmappable/un-transformable data
-# Logging includes totals and percentage calculations of transformed and un-transformed data
-# Transformed data is output into a suitable format
-# Implemented as a Python function that could be called via CLI or Lambda invocation
-# Implemented transformations and any issues must be documented in the register of data issues created in FDOS-151
+
+def create_organisation(row: pd.Series, current_timestamp: str) -> Organisation:
+    return Organisation(
+        id=uuid4(),
+        identifier_ODS_ODSCode=row["odscode"],
+        active=True,
+        name=row["name"],
+        telecom=None,
+        type=row["type"],
+        createdBy="ROBOT",
+        createdDateTime=current_timestamp,
+        modifiedBy="ROBOT",
+        modifiedDateTime=current_timestamp,
+    )
 
 
-class GPPractice(BaseModel):
-    organisation: Organisation
-    endpoints: list[Endpoints] | None
+def create_endpoint(
+    endpoint_data: Dict[str, str], organisation_id: uuid4, current_timestamp: str
+) -> Endpoints:
+    return Endpoints(
+        id=uuid4(),
+        identifier_oldDoS_id=endpoint_data.get("endpointid"),
+        status="active",
+        connectionType=endpoint_data.get("transport"),
+        name=None,
+        description=endpoint_data.get("businessscenario"),
+        payloadType=""
+        if endpoint_data.get("transport") == "telno"
+        else endpoint_data.get("interaction"),
+        address=endpoint_data.get("address"),
+        managedByOrganisation=organisation_id,
+        service=None,
+        order=endpoint_data.get("endpointorder"),
+        isCompressionEnabled=endpoint_data.get("iscompressionenabled") == "compressed",
+        format=""
+        if endpoint_data.get("transport") == "telno"
+        else endpoint_data.get("format"),
+        createdBy="ROBOT",
+        createdDateTime=current_timestamp,
+        modifiedBy="ROBOT",
+        modifiedDateTime=current_timestamp,
+    )
 
 
-def create_GP_practices_list(
-    df: pd.DataFrame, current_timestamp: str
-) -> list[GPPractice]:
-    gp_practices = []
-    for index, row in df.iterrows():
-        if df.empty:
-            logging.error("No data found")
-            return
-
-        organisation = Organisation(
-            id=uuid4(),
-            identifier_ODS_ODSCode=row["odscode"],
-            active=True,
-            name=row["name"],
-            telecom=None,  # data dictionary doesnt give a data type for this so i've made it str and none
-            type=row["type"],
-            createdBy="ROBOT",
-            createdDateTime=current_timestamp,
-            modifiedBy="ROBOT",
-            modifiedDateTime=current_timestamp,
-        )
-
-        endpoints_data = row.get("endpoints", [])
-        endpoints = []
-        if endpoints_data is None:
-            logging.error("No endpoints found for the organisation")
-            gp_practices.append(GPPractice(organisation=organisation, endpoints=None))
-            continue
-        for endpoint_data in endpoints_data:
-            endpoint = Endpoints(
-                id=uuid4(),
-                identifier_oldDoS_id=endpoint_data.get("endpointid"),
-                status="active",
-                connectionType=endpoint_data.get("transport"),
-                name=None,
-                description=endpoint_data.get("businessscenario"),
-                payloadType=""
-                if endpoint_data.get("transport") == "telno"
-                else endpoint_data.get("interaction"),
-                address=endpoint_data.get("address"),
-                managedByOrganisation=organisation.id,
-                service=None,  # defaulting to none - will need to be linked properly
-                order=endpoint_data.get("endpointorder"),
-                isCompressionEnabled=endpoint_data.get("iscompressionenabled")
-                == "compressed",
-                format=""
-                if endpoint_data.get("transport") == "telno"
-                else endpoint_data.get("format"),
-                createdBy="ROBOT",
-                createdDateTime=current_timestamp,
-                modifiedBy="ROBOT",
-                modifiedDateTime=current_timestamp,
-            )
-            endpoints.append(endpoint)
-        gp_practices.append(GPPractice(organisation=organisation, endpoints=endpoints))
-
-    return gp_practices
-
-
-def convert_list_to_dict(gp_practices: list[GPPractice]) -> dict:
-    gp_dicts = [gp.model_dump() for gp in gp_practices]
-    gp_dicts = convert_UUID_to_string(gp_dicts)
-    return gp_dicts
-
-
-def convert_UUID_to_string(gp_dicts: dict) -> list[GPPractice]:
+def convert_UUID_to_string(gp_dicts: dict) -> Dict[str, Any]:
     for gp_dict in gp_dicts:
         gp_dict["organisation"]["id"] = str(gp_dict["organisation"]["id"])
         if gp_dict["endpoints"]:
@@ -98,8 +65,36 @@ def convert_UUID_to_string(gp_dicts: dict) -> list[GPPractice]:
     return gp_dicts
 
 
-def logging_metrics(gp_practices: list[GPPractice]) -> None:
-    logging.info(f"Total number of GP practices: {len(gp_practices)}")
+def create_GP_practices_dict(
+    df: pd.DataFrame, current_timestamp: str
+) -> List[Dict[str, Any]]:
+    if df.empty:
+        logging.error("No data found")
+        return []
+
+    gp_practices = []
+    for _, row in df.iterrows():
+        organisation = create_organisation(row, current_timestamp)
+        endpoints_data = row.get("endpoints", [])
+        if endpoints_data is None:
+            logging.error("No endpoints found for the organisation")
+            gp_practices.append(
+                {"organisation": organisation.model_dump(), "endpoints": None}
+            )
+            continue
+
+        endpoints = [
+            create_endpoint(ed, organisation.id, current_timestamp)
+            for ed in endpoints_data
+        ]
+        gp_practices.append(
+            {
+                "organisation": organisation.model_dump(),
+                "endpoints": [ep.model_dump() for ep in endpoints],
+            }
+        )
+
+    return gp_practices
 
 
 def transform(input_path: Path, output_path: Path) -> None:
@@ -108,9 +103,9 @@ def transform(input_path: Path, output_path: Path) -> None:
     current_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     df = pd.read_parquet(input_path)
 
-    gp_practices = create_GP_practices_list(df, current_timestamp)
-    gp_dicts = convert_list_to_dict(gp_practices)
-    gp_df = pd.DataFrame(gp_dicts)
+    gp_practices = create_GP_practices_dict(df, current_timestamp)
+    gp_practices = convert_UUID_to_string(gp_practices)
+    gp_df = pd.DataFrame(gp_practices)
 
     gp_df.to_parquet(
         output_path / f"dos-gp-practice-transform-{current_timestamp}.parquet",
@@ -118,8 +113,6 @@ def transform(input_path: Path, output_path: Path) -> None:
         index=False,
         compression="zstd",
     )
-
-    logging_metrics(gp_df)
 
 
 def main(args: list[str] | None = None) -> None:
--- services/data-migration/tests/unit/test_transform.py
+++ services/data-migration/tests/unit/test_transform.py
@@ -1,24 +1,11 @@
+from datetime import datetime
 from pathlib import Path
 
+import pandas as pd
 import pytest
 from pytest_mock import MockerFixture
 
 from pipeline.transform import main, transform
-
-
-def test_load(mocker: MockerFixture) -> None:
-    """
-    Test that transform logs the input path and an error message
-    """
-    mock_logging_info = mocker.patch("pipeline.transform.logging.info")
-    mock_logging_error = mocker.patch("pipeline.transform.logging.error")
-
-    transform(Path("test_input_path"), Path("test_output_path"))
-
-    mock_logging_info.assert_called_once_with(
-        "Transforming data from test_input_path to test_output_path"
-    )
-    mock_logging_error.assert_called_once_with("Not implemented yet")
 
 
 def test_main_parses_args(mocker: MockerFixture) -> None:
@@ -84,3 +71,271 @@ def test_main_throws_error_on_invalid_args(mocker: MockerFixture) -> None:
 
     assert exc.value.code == expected_exit_code
     assert transform_mock.called is False
+
+
+@pytest.mark.parametrize(
+    "input_data, expected_output",
+    [
+        (
+            # Input data
+            pd.DataFrame(
+                {
+                    "odscode": ["A123"],
+                    "name": ["Test Org"],
+                    "type": ["GP Practice"],
+                    "endpoints": [
+                        [
+                            {
+                                "endpointid": "1",
+                                "transport": "email",
+                                "businessscenario": "scenario1",
+                                "interaction": "interaction1",
+                                "address": "address1",
+                                "endpointorder": 1,
+                                "iscompressionenabled": "uncompressed",
+                                "format": "PDF",
+                            }
+                        ]
+                    ],
+                }
+            ),
+            # Expected output
+            pd.DataFrame(
+                {
+                    "organisation": [
+                        {
+                            "id": "123e4567-e89b-12d3-a456-426614174000",
+                            "identifier_ODS_ODSCode": "A123",
+                            "active": True,
+                            "name": "Test Org",
+                            "telecom": None,
+                            "type": "GP Practice",
+                            "createdBy": "ROBOT",
+                            "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                            "modifiedBy": "ROBOT",
+                            "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                        }
+                    ],
+                    "endpoints": [
+                        [
+                            {
+                                "id": "123e4567-e89b-12d3-a456-426614174000",
+                                "identifier_oldDoS_id": 1,
+                                "status": "active",
+                                "connectionType": "email",
+                                "name": None,
+                                "description": "scenario1",
+                                "payloadType": "interaction1",
+                                "address": "address1",
+                                "managedByOrganisation": "123e4567-e89b-12d3-a456-426614174000",
+                                "service": None,
+                                "order": 1,
+                                "isCompressionEnabled": False,
+                                "format": "PDF",
+                                "createdBy": "ROBOT",
+                                "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                                "modifiedBy": "ROBOT",
+                                "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                            }
+                        ]
+                    ],
+                }
+            ),
+        ),
+        (
+            # Input data with multiple endpoints and varied compression settings
+            pd.DataFrame(
+                {
+                    "odscode": "B456",
+                    "name": "Org 1",
+                    "type": "GP Practice",
+                    "endpoints": [
+                        [
+                            {
+                                "endpointid": "2",
+                                "transport": "sms",
+                                "businessscenario": "scenario2",
+                                "interaction": "interaction2",
+                                "address": "address2",
+                                "endpointorder": 2,
+                                "iscompressionenabled": "compressed",
+                                "format": "XML",
+                            }
+                        ],
+                        [
+                            {
+                                "endpointid": "3",
+                                "transport": "fax",
+                                "businessscenario": "scenario3",
+                                "interaction": "interaction3",
+                                "address": "address3",
+                                "endpointorder": 3,
+                                "iscompressionenabled": "uncompressed",
+                                "format": "TXT",
+                            }
+                        ],
+                    ],
+                }
+            ),
+            # Expected output
+            pd.DataFrame(
+                {
+                    "organisation": [
+                        {
+                            "id": "123e4567-e89b-12d3-a456-426614174000",
+                            "identifier_ODS_ODSCode": "B456",
+                            "active": True,
+                            "name": "Org 1",
+                            "telecom": None,
+                            "type": "GP Practice",
+                            "createdBy": "ROBOT",
+                            "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                            "modifiedBy": "ROBOT",
+                            "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                        },
+                    ],
+                    "endpoints": [
+                        [
+                            {
+                                "id": "123e4567-e89b-12d3-a456-426614174000",
+                                "identifier_oldDoS_id": 2,
+                                "status": "active",
+                                "connectionType": "sms",
+                                "name": None,
+                                "description": "scenario2",
+                                "payloadType": "interaction2",
+                                "address": "address2",
+                                "managedByOrganisation": "123e4567-e89b-12d3-a456-426614174000",
+                                "service": None,
+                                "order": 2,
+                                "isCompressionEnabled": True,
+                                "format": "XML",
+                                "createdBy": "ROBOT",
+                                "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                                "modifiedBy": "ROBOT",
+                                "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                            },
+                            {
+                                "id": "123e4567-e89b-12d3-a456-426614174000",
+                                "identifier_oldDoS_id": 3,
+                                "status": "active",
+                                "connectionType": "fax",
+                                "name": None,
+                                "description": "scenario3",
+                                "payloadType": "interaction3",
+                                "address": "address3",
+                                "managedByOrganisation": "123e4567-e89b-12d3-a456-426614174000",
+                                "service": None,
+                                "order": 3,
+                                "isCompressionEnabled": False,
+                                "format": "TXT",
+                                "createdBy": "ROBOT",
+                                "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                                "modifiedBy": "ROBOT",
+                                "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                            },
+                        ],
+                    ],
+                },
+            ),
+        ),
+        (
+            # Input data with transport == telno
+            pd.DataFrame(
+                {
+                    "odscode": ["C789"],
+                    "name": ["Test Org"],
+                    "type": ["GP Practice"],
+                    "endpoints": [
+                        [
+                            {
+                                "endpointid": "1",
+                                "transport": "telno",
+                                "businessscenario": "scenario1",
+                                "interaction": "interaction1",
+                                "address": "address1",
+                                "endpointorder": 1,
+                                "iscompressionenabled": "uncompressed",
+                                "format": "PDF",
+                            }
+                        ]
+                    ],
+                }
+            ),
+            # Expected output
+            pd.DataFrame(
+                {
+                    "organisation": [
+                        {
+                            "id": "123e4567-e89b-12d3-a456-426614174000",
+                            "identifier_ODS_ODSCode": "C789",
+                            "active": True,
+                            "name": "Test Org",
+                            "telecom": None,
+                            "type": "GP Practice",
+                            "createdBy": "ROBOT",
+                            "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                            "modifiedBy": "ROBOT",
+                            "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                        }
+                    ],
+                    "endpoints": [
+                        [
+                            {
+                                "id": "123e4567-e89b-12d3-a456-426614174000",
+                                "identifier_oldDoS_id": 1,
+                                "status": "active",
+                                "connectionType": "telno",
+                                "name": None,
+                                "description": "scenario1",
+                                "payloadType": "",
+                                "address": "address1",
+                                "managedByOrganisation": "123e4567-e89b-12d3-a456-426614174000",
+                                "service": None,
+                                "order": 1,
+                                "isCompressionEnabled": False,
+                                "format": "",
+                                "createdBy": "ROBOT",
+                                "createdDateTime": datetime(2025, 3, 27, 12, 0),
+                                "modifiedBy": "ROBOT",
+                                "modifiedDateTime": datetime(2025, 3, 27, 12, 0),
+                            }
+                        ]
+                    ],
+                }
+            ),
+        ),
+    ],
+)
+def test_transform(
+    mocker: MockerFixture, input_data: pd.DataFrame, expected_output: pd.DataFrame
+) -> None:
+    """
+    Test the transform function to ensure input data is transformed correctly.
+    """
+
+    input_path = Path("mock_input_path")
+    output_path = Path("mock_output_path")
+
+    mocker.patch("pandas.read_parquet", return_value=input_data)
+
+    captured_df = None
+
+    def capture_to_parquet(self: pd.DataFrame, *args: tuple, **kwargs: dict) -> None:
+        nonlocal captured_df
+        captured_df = self
+
+    mocker.patch.object(pd.DataFrame, "to_parquet", new=capture_to_parquet)
+
+    mocker.patch(
+        "pipeline.transform.uuid4", return_value="123e4567-e89b-12d3-a456-426614174000"
+    )
+    mocker.patch(
+        "pipeline.transform.datetime",
+        **{"now.return_value.strftime.return_value": datetime(2025, 3, 27, 12, 0)},
+    )
+
+    transform(input_path, output_path)
+
+    assert captured_df is not None, "The DataFrame was not captured."
+    pd.testing.assert_frame_equal(captured_df, expected_output)
