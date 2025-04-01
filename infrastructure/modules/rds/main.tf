dc7ec870b4dbf74dd517139b8fa8661c6889b921
james-willis-nhse <james.willis12@nhs.net>
2025-04-01 16:52:01 +0100

    DOSIS-496 adding in create db subnet and security

--- infrastructure/modules/rds/main.tf
+++ infrastructure/modules/rds/main.tf
@@ -13,8 +13,10 @@ module "cluster" {
     }
   }
 
-  vpc_id = var.vpc_id
-  #db_subnet_group_name = var.rds_db_subnet_group
+  vpc_id                 = var.vpc_id
+  create_db_subnet_group = true
+  create_security_group  = true
+  db_subnet_group_name   = var.rds_db_subnet_group
   security_group_rules = {
     ex1_ingress = {
       cidr_blocks = var.rds_ingress_cidr
