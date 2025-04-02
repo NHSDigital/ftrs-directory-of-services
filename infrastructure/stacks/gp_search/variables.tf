b29a9498a0ec5a8491e952e1a99b9d74b628f61f
DomBaker <dominic.baker1@nhs.net>
2025-04-02 10:10:25 +0100

    DOSIS-418 Add service setup and remove old function and zip

--- infrastructure/stacks/gp_search/variables.tf
+++ infrastructure/stacks/gp_search/variables.tf
@@ -5,3 +5,13 @@ variable "gp_search_bucket_name" {
 variable "gp_search_lambda_name" {
   description = "Name of API Lambda for the GP Search API POC pipeline test"
 }
+
+variable "application_tag" {
+  description = "The version or tag of the data mirgation application"
+  type        = string
+}
+
+variable "commit_hash" {
+  description = "The commit hash of the data mirgation application"
+  type        = string
+}
