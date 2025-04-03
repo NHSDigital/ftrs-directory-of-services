596bc48cca022d1241104606c069286cdbeaa640
DomBaker <dominic.baker1@nhs.net>
2025-04-03 17:00:51 +0100

    DOSIS-418 remove duplication of name variables in tfvars

--- infrastructure/stacks/gp_search/s3.tf
+++ infrastructure/stacks/gp_search/s3.tf
@@ -1,4 +1,4 @@
 module "s3" {
   source      = "../../modules/s3"
-  bucket_name = "${var.project}-${var.gp_search_bucket_name}-${var.environment}"
+  bucket_name = "${var.project}-s3-${var.environment}"
 }
--- infrastructure/stacks/gp_search/variables.tf
+++ infrastructure/stacks/gp_search/variables.tf
@@ -1,11 +1,3 @@
-variable "gp_search_bucket_name" {
-  description = "Name of S3 bucket for the GP Search API POC pipeline test"
-}
-
-variable "gp_search_lambda_name" {
-  description = "Name of API Lambda for the GP Search API POC pipeline test"
-}
-
 variable "application_tag" {
   description = "The version or tag of the data mirgation application"
   type        = string
