c7cf4a7f400dd8d3827990aafeabc87ec00ae699
DomBaker <dominic.baker1@nhs.net>
2025-04-03 14:27:23 +0100

    DOSIS-418 Remove debugging for tf state for IS

--- scripts/workflow/action-infra-stack.sh
+++ scripts/workflow/action-infra-stack.sh
@@ -131,15 +131,6 @@ echo "Pulling environment variables from $ENVIRONMENTS_DIR"
 terraform-initialise
 #
 terraform workspace select "$WORKSPACE" || terraform workspace new "$WORKSPACE"
-echo "===============STATE===================="
-terraform state list
-echo "=================END_STATE==============="
-
-terraform state rm data.archive_file.lambda
-
-echo "===============STATE_AFTER===================="
-terraform state list
-echo "=================END_STATE==============="
 # plan
 if [ -n "$ACTION" ] && [ "$ACTION" = 'plan' ] ; then
 
