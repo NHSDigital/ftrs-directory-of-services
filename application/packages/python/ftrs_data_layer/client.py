356d75101da0c8198acdb266a7c88c3da194c14e
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2025-04-04 17:08:33 +0100

    FDOS-268 Add explicit region to dynamodb

--- application/packages/python/ftrs_data_layer/client.py
+++ application/packages/python/ftrs_data_layer/client.py
@@ -7,18 +7,22 @@ from mypy_boto3_dynamodb import DynamoDBClient, DynamoDBServiceResource
 @cache
 def get_dynamodb_client(
     endpoint_url: str | None = None,
+    region_name: str = "eu-west-2",
 ) -> DynamoDBClient:
     """
     Cached DynamoDB client for accessing the DynamoDB service.
     """
-    return boto3.client("dynamodb", endpoint_url=endpoint_url)
+    return boto3.client("dynamodb", endpoint_url=endpoint_url, region_name=region_name)
 
 
 @cache
 def get_dynamodb_resource(
     endpoint_url: str | None = None,
+    region_name: str = "eu-west-2",
 ) -> DynamoDBServiceResource:
     """
     Cached DynamoDB client for accessing the DynamoDB service.
     """
-    return boto3.resource("dynamodb", endpoint_url=endpoint_url)
+    return boto3.resource(
+        "dynamodb", endpoint_url=endpoint_url, region_name=region_name
+    )
