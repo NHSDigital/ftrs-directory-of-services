1b708b0c10c2f4dd24c2653b3de1cc6c0a7e11d2
james-willis-nhse <james.willis12@nhs.net>
2025-04-07 16:43:56 +0100

    DOSIS-496 Adding RDS instance to TF State

--- scripts/workflow/action-infra-stack.sh
+++ scripts/workflow/action-infra-stack.sh
@@ -142,10 +142,14 @@ terraform-initialise
 terraform workspace select "$WORKSPACE" || terraform workspace new "$WORKSPACE"
 # plan
 if [ -n "$ACTION" ] && [ "$ACTION" = 'plan' ] ; then
-  echo "=============IMPORT==============="
+  echo "=============GET STATE BEFORE==============="
   terraform state list
-  #terraform import module.gp_search_rds.module.cluster.aws_rds_cluster_instance.this["2"] ftrs-dos-integrated-search-gp-search-rds-dev-2
+  echo "=============IMPORT==============="
+  terraform import module.gp_search_rds.module.cluster.aws_rds_cluster_instance.this["2"] ftrs-dos-integrated-search-gp-search-rds-dev-2
   echo "=============END IMPORT==============="
+  echo "=============GET STATE AFTER==============="
+  terraform state list
+  echo "=============DEBUG END==============="
   terraform plan -out $STACK.tfplan \
     -var-file "$ROOT_DIR/$INFRASTRUCTURE_DIR/$COMMON_TF_VARS_FILE" \
     -var-file "$ROOT_DIR/$INFRASTRUCTURE_DIR/$STACK_TF_VARS_FILE" \
