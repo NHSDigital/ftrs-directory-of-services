d42801d316fd90a553561f0bee0637ade56c21eb
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-04-07 12:02:55 +0100

    FDOS-265 Work in progress

---
+++ infrastructure/stacks/github_runner/data.tf
@@ -0,0 +1,4 @@
+# data "aws_s3_bucket" "artefact_bucket" {
+#   bucket = local.artefacts_bucket
+# }
+
---
+++ infrastructure/stacks/github_runner/variables.tf
@@ -0,0 +1,4 @@
+variable "github_org" {
+  description = "The name of git hub organisation - eg NHSDigital"
+}
+
