59760267bf6aa60bca6ed0240a86c26b2d6443b1
DomBaker <dominic.baker1@nhs.net>
2025-04-08 15:48:01 +0100

    DOSIS-418 Add number of policy jsons to gp search lambda

--- infrastructure/stacks/gp_search/search_lambda.tf
+++ infrastructure/stacks/gp_search/search_lambda.tf
@@ -1,15 +1,16 @@
 module "lambda" {
-  source                = "../../modules/lambda"
-  function_name         = "${var.project}-${var.environment}-${var.lambda_name}"
-  description           = "GP search Lambda"
-  handler               = "gp_search_function.lambda_handler"
-  s3_bucket_name        = local.artefacts_bucket
-  s3_key                = "${terraform.workspace}/${var.commit_hash}/${var.gp_search_service_name}-lambda-${var.application_tag}.zip"
-  attach_policy_jsons   = false
-  attach_tracing_policy = true
-  tracing_mode          = "Active"
-  policy_jsons          = [data.aws_iam_policy_document.vpc_access_policy.json]
-  layers                = ["arn:aws:lambda:eu-west-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:46"]
+  source                 = "../../modules/lambda"
+  function_name          = "${var.project}-${var.environment}-${var.lambda_name}"
+  description            = "GP search Lambda"
+  handler                = "gp_search_function.lambda_handler"
+  s3_bucket_name         = local.artefacts_bucket
+  s3_key                 = "${terraform.workspace}/${var.commit_hash}/${var.gp_search_service_name}-lambda-${var.application_tag}.zip"
+  attach_policy_jsons    = false
+  attach_tracing_policy  = true
+  tracing_mode           = "Active"
+  number_of_policy_jsons = "1"
+  policy_jsons           = [data.aws_iam_policy_document.vpc_access_policy.json]
+  layers                 = ["arn:aws:lambda:eu-west-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:46"]
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
   security_group_ids = [aws_security_group.gp_search_lambda_security_group.id]
