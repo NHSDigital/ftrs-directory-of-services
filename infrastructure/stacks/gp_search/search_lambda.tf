fe1dff34fcfd873d9d2cdcc083c9bcdb1e19c839
DomBaker <dominic.baker1@nhs.net>
2025-04-08 15:26:00 +0100

    DOSIS-418 Add policy for gp search lambda

--- infrastructure/stacks/gp_search/search_lambda.tf
+++ infrastructure/stacks/gp_search/search_lambda.tf
@@ -8,9 +8,23 @@ module "lambda" {
   attach_policy_jsons   = false
   attach_tracing_policy = true
   tracing_mode          = "Active"
-  policy_jsons          = []
+  policy_jsons          = [data.aws_iam_policy_document.vpc_access_policy.json]
   layers                = ["arn:aws:lambda:eu-west-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:46"]
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
   security_group_ids = [aws_security_group.gp_search_lambda_security_group.id]
+}
+
+data "aws_iam_policy_document" "vpc_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface"
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
