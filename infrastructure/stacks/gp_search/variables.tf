8211ec433ab33939b8dfd28cbdbfd840bcb02c41
DomBaker <dominic.baker1@nhs.net>
2025-04-08 13:15:16 +0100

    DOSIS-418 Add missing variable references in gp search stack

--- infrastructure/stacks/gp_search/variables.tf
+++ infrastructure/stacks/gp_search/variables.tf
@@ -1,8 +1,13 @@
+variable "s3_bucket_name" {
+  description = "The name of the gp search bucket"
+}
+variable "lambda_name" {
+  description = "The name of the gp search lambda"
+}
 variable "application_tag" {
   description = "The version or tag of the gp search application"
   type        = string
 }
-
 variable "commit_hash" {
   description = "The commit hash of the gp search application"
   type        = string
