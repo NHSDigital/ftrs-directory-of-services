afd2fbc0dc8647ef9f684b71492a1904a9d26e78
DomBaker <dominic.baker1@nhs.net>
2025-04-15 11:55:21 +0100

    DOSIS-423 Update gp search function to accept multiple secrets

--- services/gp-search/functions/gp_search_function.py
+++ services/gp-search/functions/gp_search_function.py
@@ -14,7 +14,7 @@ patch_all()
 
 # Environment variable
 NAMESPACE = os.environ["NAMESPACE"]
-SECRET_NAME = os.environ["DB_SECRET_NAME"]
+SECRET_NAMES = os.environ["DB_SECRET_NAME"].split(",")
 REGION_NAME = os.environ.get("AWS_REGION", "eu-west-2")
 
 # Powertools setup
@@ -26,7 +26,7 @@ metrics = Metrics(namespace=NAMESPACE)
 @tracer.capture_method
 def get_db_credentials() -> json:
     client = boto3.client("secretsmanager", region_name=REGION_NAME)
-    secret_value = client.get_secret_value(SecretId=SECRET_NAME)
+    secret_value = client.get_secret_value(SecretId=SECRET_NAMES)
     return json.loads(secret_value["SecretString"])
 
 
