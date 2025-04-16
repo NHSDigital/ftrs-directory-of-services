f254ed3748ce7a1b19447f6e7b54db341e27f656
DomBaker <dominic.baker1@nhs.net>
2025-04-16 12:08:11 +0100

    DOSIS-423 Add egress for secret manager and update egress to rds

--- infrastructure/stacks/gp_search/search_lambda.tf
+++ infrastructure/stacks/gp_search/search_lambda.tf
@@ -37,11 +37,20 @@ module "lambda" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "lambda_allow_egress_to_rds" {
-  security_group_id            = aws_security_group.gp_search_lambda_security_group.id
-  referenced_security_group_id = data.aws_security_group.rds_security_group.id
+  security_group_id            = data.aws_security_group.rds_security_group.id
+  referenced_security_group_id = aws_security_group.gp_search_lambda_security_group.id
   from_port                    = var.rds_port
   ip_protocol                  = "tcp"
   to_port                      = var.rds_port
+}
+
+resource "aws_vpc_security_group_egress_rule" "lambda_allow_egress_to_anywhere" {
+  security_group_id = aws_security_group.gp_search_lambda_security_group.id
+  from_port         = "443"
+  to_port           = "443"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "A rule to allow outgoing connections AWS APIs from the gp search lambda security group"
 }
 
 data "aws_iam_policy_document" "vpc_access_policy" {
