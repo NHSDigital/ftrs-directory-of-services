a45e142e993ada142686a1aff261a9b76ecd1a79
DomBaker <dominic.baker1@nhs.net>
2025-04-17 15:28:00 +0100

    DOSIS-423 Add uec secret services policy to gp search lambda

--- services/gp-search/functions/gp_search_function.py
+++ services/gp-search/functions/gp_search_function.py
@@ -14,7 +14,7 @@ patch_all()
 
 # Environment variable
 NAMESPACE = os.environ["NAMESPACE"]
-SECRET_NAMES = os.environ["DB_SECRET_NAME"].split(",")
+DB_SECRET_NAME = os.environ["DB_SECRET_NAME"]
 REGION_NAME = os.environ.get("AWS_REGION", "eu-west-2")
 
 # Powertools setup
@@ -26,8 +26,8 @@ metrics = Metrics(namespace=NAMESPACE)
 @tracer.capture_method
 def get_db_credentials() -> json:
     client = boto3.client("secretsmanager", region_name=REGION_NAME)
-    secret_value = client.get_secret_value(SecretId=SECRET_NAMES)
-    return json.loads(secret_value["SecretString"])
+    db_secret_value = client.get_secret_value(SecretId=DB_SECRET_NAME)
+    return json.loads(db_secret_value["SecretString"])
 
 
 @tracer.capture_method
