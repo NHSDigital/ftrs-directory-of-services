80a6664685ed652ec3a4222b5839d6af90be90f7
prjh2-nhs <178916511+prjh2-nhs@users.noreply.github.com>
2025-04-30 13:37:32 +0100

    FDOS-183 Refactor unit test and env template

--- services/data-migration/tests/unit/utils/test_db_config.py
+++ services/data-migration/tests/unit/utils/test_db_config.py
@@ -1,7 +1,12 @@
 from pipeline.utils.db_config import DatabaseConfig
 
 
-def test_initialise_config() -> None:
+def test_db_config_successful() -> None:
+    """
+    Test that the DatabaseConfig class correctly constructs the connection string,
+    returns accurate database details, generates the correct URI,
+    and formats its string representation properly.
+    """
     db_config = DatabaseConfig(
         host="host",
         port="port",
@@ -10,54 +15,26 @@ def test_initialise_config() -> None:
         db_name="dbname",
     )
 
+    # Test initialise_config
     assert (
         db_config.connection_string == "postgresql://username:password@host:port/dbname"
     )
 
-
-def test_get_db_details() -> None:
-    db_config = DatabaseConfig(
-        host="host",
-        port="host",
-        user="host",
-        password="host",
-        db_name="dbname",
-    )
-
+    # Test get_db_details
     db_details = db_config.get_db_details()
-
     assert db_details == {
         "host": "host",
-        "port": "host",
-        "user": "host",
-        "password": "host",
+        "port": "port",
+        "user": "username",
+        "password": "password",
         "db_name": "dbname",
     }
 
-
-def test_get_db_uri() -> None:
-    db_config = DatabaseConfig(
-        host="host",
-        port="port",
-        user="username",
-        password="password",
-        db_name="dbname",
-    )
-
+    # Test get_db_uri
     db_uri = db_config.get_db_uri()
-
     assert db_uri == "postgresql://username:password@host:port/dbname"
 
-
-def test_db_config_to_string() -> None:
-    db_config = DatabaseConfig(
-        host="host",
-        port="port",
-        user="username",
-        password="password",
-        db_name="dbname",
-    )
-
+    # Test db_config_to_string
     assert (
         str(db_config)
         == "DatabaseConfig(host=host, port=port, user=username, password=****, db_name=dbname)"
