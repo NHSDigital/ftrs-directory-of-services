0374fa87a583f0faa35aa8096da35775efb98849
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-04-30 17:48:38 +0100

    FDOS-22399 Correct the names as per the naming convention

--- infrastructure/stacks/read_only_viewer/ssm-parameters.tf
+++ infrastructure/stacks/read_only_viewer/ssm-parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "dos_aws_account_id_mgmt" {
-  name        = "/dos/aws_account_id_mgmt"
+  name        = "/dos/aws_account_id_mgmt${local.workspace_suffix}"
   description = "Id of the management account"
   type        = "SecureString"
   tier        = "Standard"
