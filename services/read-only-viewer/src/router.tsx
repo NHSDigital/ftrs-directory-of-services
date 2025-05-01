4392cb10774a320a0745615f66004f0ff3a22376
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2025-05-01 13:32:25 +0100

    FDOS-302 Fix linting issues

--- services/read-only-viewer/src/router.tsx
+++ services/read-only-viewer/src/router.tsx
@@ -1,18 +1,17 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
 export const createRouter = (options?: { scrollRestoration?: boolean }) => {
   const router = createTanStackRouter({
     routeTree,
     ...options,
-  })
+  });
 
   return router;
-}
+};
 
-
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: ReturnType<typeof createRouter>
+    router: ReturnType<typeof createRouter>;
   }
 }
