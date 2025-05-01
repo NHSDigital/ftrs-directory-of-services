5543152bc3749e0982ea5426c7e657a6046d8264
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2025-05-01 13:12:44 +0100

    FDOS-302 Add SSR support for read-only-viewer

---
+++ services/read-only-viewer/src/router.tsx
@@ -0,0 +1,17 @@
+import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+export const createRouter = () => {
+  const router = createTanStackRouter({
+    routeTree
+  })
+
+  return router;
+}
+
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>
+  }
+}
--- services/read-only-viewer/src/routes/__root.tsx
+++ services/read-only-viewer/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Container, Footer, Header } from "nhsuk-react-components";
 import appStylesUrl from "../styles/App.scss?url";
+import type { PropsWithChildren } from "react";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -16,26 +17,45 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <HeadContent />
-      <Header transactional>
-        <Header.Container>
-          <Header.Logo href="/" />
-          <Header.ServiceName href="/">
-            FtRS Read-Only Viewer
-          </Header.ServiceName>
-        </Header.Container>
-      </Header>
-      <Container className="ftrs-page-container">
+      <RootDocument>
         <Outlet />
-      </Container>
-      <Footer>
-        <Footer.List>
-          <Footer.ListItem href="/">Home</Footer.ListItem>
-        </Footer.List>
-        <Footer.Copyright>
-          &copy; {new Date().getFullYear()} NHS England
-        </Footer.Copyright>
-      </Footer>
+      </RootDocument>
       <TanStackRouterDevtools />
     </>
   ),
+  notFoundComponent: () => (
+    <RootDocument>
+      <Container className="ftrs-page-container">
+        <h1 className="nhsuk-heading-l">Page not found</h1>
+        <p>This page does not exist.</p>
+        <p>
+          <a href="/">Return to the homepage</a>
+        </p>
+      </Container>
+    </RootDocument>
+  ),
 });
+
+const RootDocument: React.FC<PropsWithChildren> = ({ children }) => (
+  <>
+    <Header transactional>
+      <Header.Container>
+        <Header.Logo href="/" />
+        <Header.ServiceName href="/">
+          FtRS Read-Only Viewer
+        </Header.ServiceName>
+      </Header.Container>
+    </Header>
+    <Container className="ftrs-page-container">
+      {children}
+    </Container>
+    <Footer>
+      <Footer.List>
+        <Footer.ListItem href="/">Home</Footer.ListItem>
+      </Footer.List>
+      <Footer.Copyright>
+        &copy; {new Date().getFullYear()} NHS England
+      </Footer.Copyright>
+    </Footer>
+  </>
+)
