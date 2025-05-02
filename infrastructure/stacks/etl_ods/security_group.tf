5c7e58c4967ce5631ac71a203ef0ea1866d0343a
Thomas Cordall <thomas.cordall@kainos.com>
2025-05-02 13:28:47 +0100

    SIA-497 added security group rule for outbound taffic

--- infrastructure/stacks/etl_ods/security_group.tf
+++ infrastructure/stacks/etl_ods/security_group.tf
@@ -2,14 +2,11 @@ resource "aws_security_group" "processor_lambda_security_group" {
   name        = "${local.resource_prefix}-${var.extract_name}${local.workspace_suffix}-sg"
   description = "Security group for etl ods extract lambda"
   vpc_id      = data.aws_vpc.vpc.id
-}
-
-resource "aws_security_group_rule" "lambda_https_egress" {
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = data.aws_security_group.processor_lambda_security_group.id
-  description       = "Allow outbound HTTPS for API access"
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outbound HTTPS for API access"
+  }
 }
