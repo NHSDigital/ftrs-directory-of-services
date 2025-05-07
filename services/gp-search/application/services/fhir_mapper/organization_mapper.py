d7af5ff7920d8b39ce77df53c516ef31f0cd1538
lukeb-nhs <luke.biddell1@nhs.net>
2025-05-07 13:47:57 +0100

    DOSIS-1045 Use pydantic model for dynamo and other improvements

--- services/gp-search/application/services/fhir_mapper/organization_mapper.py
+++ services/gp-search/application/services/fhir_mapper/organization_mapper.py
@@ -1,77 +1,62 @@
-"""Organization Mapper Service - Maps organization raw_data to FHIR Organization resources."""
-
-#  amazonq-ignore-next-line
-from typing import Any
-
 from fhir.resources.R4B.address import Address
 from fhir.resources.R4B.contactpoint import ContactPoint
 from fhir.resources.R4B.identifier import Identifier
 from fhir.resources.R4B.organization import Organization
+from services.repository.dynamo import OrganizationRecord, OrganizationValue
 
 
 class OrganizationMapper:
-    """Service to map organization raw_data to FHIR Organization resources."""
+    def map_to_organization_resource(
+        self, organization_record: OrganizationRecord
+    ) -> Organization:
+        organization_value = organization_record.value
 
-    def map_to_organization(self, raw_data: dict[str, Any]) -> Organization:
-        """Map organization raw_data from DynamoDB to a FHIR Organization resource.
+        organization_id = organization_value.id
+        name = organization_value.name
+        active = organization_value.active
+        identifier = self._create_identifier(organization_value)
+        telecom = self._create_telecom(organization_value)
+        address = self._create_dummy_address()
 
-        Args:
-            raw_data: The organization raw_data from DynamoDB
-
-        Returns:
-            A FHIR Organization resource
-
-        """
-        # Set basic properties - handle both raw and parsed formats
-
-        raw_organisation = raw_data.get("value", {})
-
-        organization_id = raw_organisation.get("id")
-        name = raw_organisation.get("name")
-        active = raw_organisation.get("active")
-
-        # Map ODS code to identifier
-
-        identifier = self._create_identifier(raw_organisation)
-
-        # Map telecom if available
-        telecom_list = self._process_telecom_data(raw_organisation)
-
-        address = self._create_address(raw_organisation)
-
-        org_dict = {
-            "id": organization_id,
-            "identifier": identifier,
-            "active": active,
-            "name": name,
-            "telecom": telecom_list,
-            "address": address,
-        }
-        org = Organization.model_validate(org_dict)
+        org = Organization.model_validate(
+            {
+                "id": organization_id,
+                "identifier": identifier,
+                "active": active,
+                "name": name,
+                "telecom": telecom,
+                "address": address,
+            }
+        )
 
         return org
 
-    def _create_identifier(self, raw_organisation: dict) -> list[Identifier]:
+    def _create_identifier(
+        self, organization_value: OrganizationValue
+    ) -> list[Identifier]:
         identifier = Identifier.model_validate(
             {
                 "use": "official",
                 "system": "https://fhir.nhs.uk/Id/ods-organization-code",
-                "value": raw_organisation.get("identifier_ODS_ODSCode"),
+                "value": organization_value.identifier_ODS_ODSCode,
             }
         )
 
         return [identifier]
 
-    def _process_telecom_data(
-        self,
-        data: dict[str, Any],
-    ) -> list[ContactPoint] | None:
-        telecom = data.get("telecom")
+    def _create_telecom(
+        self, organization_value: OrganizationValue
+    ) -> list[ContactPoint]:
+        telecom_entry = ContactPoint.model_validate(
+            {
+                "system": "phone",
+                "value": organization_value.telecom,
+            }
+        )
 
-        return [telecom] if telecom else None
+        return [telecom_entry]
 
-    def _create_address(self, data: dict[str, Any]) -> list[Address]:
-        # TODO: Dynamo DB data does not include address, make dummy data
+    def _create_dummy_address(self) -> list[Address]:
         address = Address.model_validate(
             {
                 "line": ["Dummy Medical Practice", "Dummy Street"],
