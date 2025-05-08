1d6f56baa55700ea5be84c804dd34edb654fc989
lukeb-nhs <luke.biddell1@nhs.net>
2025-05-08 17:11:04 +0100

    DOSIS-1045 Temporarily remove xray

--- services/gp-search/functions/gp_search_function.py
+++ services/gp-search/functions/gp_search_function.py
@@ -1,30 +1,19 @@
 import logging
-import os
 
-import boto3
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_xray_sdk.core import patch_all, xray_recorder
-from boto3.dynamodb.conditions import Key
 
 from functions.ftrs_service.ftrs_service import FtrsService
-
-# Patch all supported libraries for X-Ray (includes boto3, requests, etc.)
-patch_all()
 
 # Setup logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# DynamoDB setup
-dynamodb = boto3.resource("dynamodb")
-table_name = os.environ.get("DYNAMODB_TABLE_NAME")
-table = dynamodb.Table(table_name)
-
 
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    ods_code = event["pathParameters"]["odsCode"]
+
     ftrs_service = FtrsService()
 
-    ods_code = event["pathParameters"]["odsCode"]
     fhir_resource = ftrs_service.endpoints_by_ods(ods_code=ods_code)
     fhir_resource_json = fhir_resource.model_dump_json()
 
@@ -33,29 +22,3 @@ def lambda_handler(event: dict, context: LambdaContext) -> dict:
         "headers": {"Content-Type": "application/fhir+json"},
         "body": fhir_resource_json,
     }
-
-
-@xray_recorder.capture("lambda_handler")
-def lambda_handler_original(event: dict, context: LambdaContext) -> dict:
-    logger.info("Received event: %s", event)
-
-    xray_recorder.put_annotation("Operation", "QueryTable")
-    xray_recorder.put_metadata("TableName", table_name)
-
-    try:
-        with xray_recorder.in_subsegment("DynamoDBQuery"):
-            response = table.query(
-                IndexName="ods-code-index",
-                KeyConditionExpression=Key("ods-code").eq("P83010"),
-            )
-
-        logger.info(
-            "Fetched %d items from table %s.",
-            len(response.get("Items", [])),
-            table_name,
-        )
-        return {"statusCode": 200, "body": response.get("Items", [])}
-    except Exception as e:
-        logging.exception("Failed to gather items from table")
-        xray_recorder.put_annotation("Error", str(e))
-        return {"statusCode": 500, "body": str(e)}
