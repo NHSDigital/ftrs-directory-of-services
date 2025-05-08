d6695916d4d15f349aaac5e74aa511edaaee9458
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-05-08 15:45:25 +0100

    FDOS-279 FDOS-189 Amend dynamodb table lookup

--- infrastructure/stacks/data_migration/lambda.tf
+++ infrastructure/stacks/data_migration/lambda.tf
@@ -172,9 +172,9 @@ data "aws_iam_policy_document" "dynamodb_access_policy" {
       "dynamodb:DeleteItem"
     ]
     resources = flatten([
-      for table in data.aws_dynamodb_table.dynamodb_tables : [
-        table.arn,
-        "${table.arn}/index/*"
+      for table in local.dynamodb_tables : [
+        table.value.arn,
+        "${table.value.arn}/index/*"
       ]
     ])
   }
