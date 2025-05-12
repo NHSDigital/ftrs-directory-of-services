96ead9e514f2bea3776222c5acfe8ed94ea809d9
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-05-12 17:21:09 +0100

    FDOS-315 Pipeline issue fix

--- infrastructure/stacks/opensearch/opensearch_vpce.tf
+++ infrastructure/stacks/opensearch/opensearch_vpce.tf
@@ -1,5 +1,5 @@
 resource "aws_opensearchserverless_vpc_endpoint" "vpc_endpoint" {
-  name               = "${local.stack_name}-vpce${local.workspace_suffix}"
+  name               = "${var.stack_name}-vpce${local.workspace_suffix}"
   vpc_id             = data.aws_vpc.vpc.id
   subnet_ids         = data.aws_subnets.private_subnets.ids
   security_group_ids = [aws_security_group.opensearch_security_group.id]
