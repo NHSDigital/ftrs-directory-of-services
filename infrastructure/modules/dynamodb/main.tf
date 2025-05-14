28cad6315655299e0e2c95fe1c47dc1c6cfbd585
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-05-14 11:09:22 +0100

    FDOS-315 Enable PITR for DynamoDB table and disable streams for all other tables

--- infrastructure/modules/dynamodb/main.tf
+++ infrastructure/modules/dynamodb/main.tf
@@ -2,14 +2,15 @@ module "dynamodb_table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
   version = "3.3.0"
 
-  name                = "${var.table_name}${local.workspace_suffix}"
-  hash_key            = var.hash_key
-  range_key           = var.range_key
-  autoscaling_enabled = var.autoscaling_enabled
-  stream_enabled      = var.stream_enabled
-  stream_view_type    = var.stream_view_type
-  attributes          = var.attributes
-  billing_mode        = var.billing_mode
+  name                           = "${var.table_name}${local.workspace_suffix}"
+  hash_key                       = var.hash_key
+  range_key                      = var.range_key
+  autoscaling_enabled            = var.autoscaling_enabled
+  stream_enabled                 = var.stream_enabled
+  stream_view_type               = var.stream_view_type
+  attributes                     = var.attributes
+  billing_mode                   = var.billing_mode
+  point_in_time_recovery_enabled = var.point_in_time_recovery_enabled
 
   server_side_encryption_enabled = true
 }
