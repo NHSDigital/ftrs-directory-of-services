d7a30735bac9fab38517d07b05581a9b966bdc19
Thomas Cordall <thomas.cordall@kainos.com>
2025-05-15 10:42:32 +0100

    SIA-499 added queue definition to terraform

---
+++ infrastructure/modules/queue/main.tf
@@ -0,0 +1,9 @@
+resource "aws_sqs_queue" "queue_name" {
+  name                       = var.queue_name
+  delay_seconds              = var.delay_seconds
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  max_message_size           = var.max_message_size
+  message_retention_seconds  = var.message_retention_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+  sqs_managed_sse_enabled    = var.sqs_managed_sse_enabled
+}
