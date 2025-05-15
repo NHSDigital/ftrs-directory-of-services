fdc7c8af285c6cc328e1c377efded66c5dd3fb41
Thomas Cordall <thomas.cordall@kainos.com>
2025-05-15 15:01:53 +0100

    SIA-499 removed queue from modules

--- infrastructure/modules/queue/locals.tf
+++
@@ -1,7 +0,0 @@
-# ==============================================================================
-# Context
-
-locals {
-  workspace_suffix      = "${terraform.workspace}" == "default" ? "" : "-${terraform.workspace}"
-  environment_workspace = "${terraform.workspace}" == "default" ? "" : "${terraform.workspace}"
-}
