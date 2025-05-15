f43838b0cefe22fff65aecbf52343929f5ff1da2
Thomas Cordall <thomas.cordall@kainos.com>
2025-05-15 16:09:14 +0100

    SIA-499 changed queue variables

--- infrastructure/stacks/load_data/queue.tf
+++ infrastructure/stacks/load_data/queue.tf
@@ -1,9 +1,9 @@
 resource "aws_sqs_queue" "transformed_queue" {
-  name                       = "transformed-queue"
-  delay_seconds              = 10
-  visibility_timeout_seconds = 30
-  max_message_size           = 2048
-  message_retention_seconds  = 86400
-  receive_wait_time_seconds  = 2
-  sqs_managed_sse_enabled    = true
+  name                       = var.queue_name
+  delay_seconds              = var.delay_seconds
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  max_message_size           = var.max_message_size
+  message_retention_seconds  = var.message_retention_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+  sqs_managed_sse_enabled    = var.sqs_managed_sse_enabled
 }
