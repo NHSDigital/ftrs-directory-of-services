3fbef1d2567e52840b5e005ff67d290fa7e78da3
Thomas Cordall <thomas.cordall@kainos.com>
2025-05-15 10:42:32 +0100

    SIA-499 added queue definition to terraform

---
+++ infrastructure/modules/queue/variables.tf
@@ -0,0 +1,33 @@
+# ==============================================================================
+# Mandatory variables
+
+variable "queue_name" {
+  description = "The name of the queue"
+}
+
+variable "delay_seconds" {
+  description = "The number of seconds a message should be invisible to consumers"
+}
+
+variable "visibility_timeout_seconds" {
+  description = "How long a message remains invisible to other consumers after being received by one consumer"
+}
+
+variable "max_message_size" {
+  description = "The maximum size of the message"
+}
+
+variable "message_retention_seconds" {
+  description = "How long the SQS queue keeps a message"
+}
+
+variable "receive_wait_time_seconds" {
+  description = "Time period that a request could wait for a message to become available in the sqs queue"
+}
+
+variable "sqs_managed_sse_enabled" {
+  description = "Enables Server-Side Encryption for messages stored in the queue"
+}
+
+
+
---
+++ infrastructure/stacks/load_data/queue.tf
@@ -0,0 +1,9 @@
+resource "aws_sqs_queue" "transformed_queue" {
+  name                       = "transformed-queue"
+  delay_seconds              = 10
+  visibility_timeout_seconds = 30
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 2
+  sqs_managed_sse_enabled    = true
+}
