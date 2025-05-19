fc4ea709f4351ce5876b95c755e4f8111954567a
Tamara Goldschmidt <77108863+tamaragoldschmidt@users.noreply.github.com>
2025-05-19 11:45:32 +0100

    DOSIS-10451 Automated testsf (#127)
    * DOSIS-10451 adding in tests for lambda
    
    * DOSIS-10451 Adding in no endpoint test
    
    * DOSIS-10451 Updating in no endpoint test
    
    * DOSIS-10451 Tidying up the tests

--- tests/service_automation/tests/step_definitions/is_infra_steps/test_lambda.py
+++ tests/service_automation/tests/step_definitions/is_infra_steps/test_lambda.py
@@ -1,5 +1,6 @@
 import pytest
 import boto3
+import json
 from pytest_bdd import scenarios, given, when, then, parsers
 from loguru import logger
 from utilities.infra.lambda_util import LambdaWrapper
@@ -30,14 +31,60 @@ def invoke_lambda(aws_lambda_client, odscode, flambda_name):
     lambda_params = {
         "odsCode": odscode
     }
+    lambda_payload = aws_lambda_client.invoke_function(flambda_name, lambda_params)
+    return lambda_payload
 
+
+@when(('I invoke the lambda with the ods code value not set'), target_fixture='fLambda_payload')
+def invoke_lambda_no_odscode(aws_lambda_client, flambda_name):
+    lambda_params = {
+        "odsCode": ""
+    }
+    lambda_payload = aws_lambda_client.invoke_function(flambda_name, lambda_params)
+    return lambda_payload
+
+
+@when(('I invoke the lambda with an empty event'), target_fixture='fLambda_payload')
+def invoke_lambda_empty_event(aws_lambda_client, flambda_name):
+    lambda_params = {
+    }
     lambda_payload = aws_lambda_client.invoke_function(flambda_name, lambda_params)
     return lambda_payload
 
 
 @then(parsers.parse('the lambda response contains the ods code "{odscode}"'))
-def lambda_response_message(fLambda_payload, odscode):
-    logger.debug("Checking for ods code {} in lambda response: {}", odscode, fLambda_payload)
+def lambda_ods_code(fLambda_payload, odscode):
+    response = json.loads(fLambda_payload["body"])
     assert fLambda_payload["statusCode"] == 200
-    assert odscode in fLambda_payload["body"]
+    assert response["entry"][0]["resource"]["identifier"][0]["system"] =="https://fhir.nhs.uk/Id/ods-organization-code" and \
+        response["entry"][0]["resource"]["identifier"][0]["value"] == odscode
+
+
+@then(parsers.parse('the lambda response contains an empty bundle'))
+def lambda_empty_response(fLambda_payload):
+    response = json.loads(fLambda_payload["body"])
+    assert fLambda_payload["statusCode"] == 200
+    assert response["entry"] == []
+
+
+@then(parsers.parse('the lambda returns the error message "{error_message}" with status code "{status_code}"'))
+def lambda_error_message(fLambda_payload, error_message, status_code):
+    response = json.loads(fLambda_payload["body"])
+    assert fLambda_payload["statusCode"] == 500
+    assert response["issue"][0]["details"]["coding"][0]["code"] == "INTERNAL_SERVER_ERROR" and \
+    response["issue"][0]["details"]["coding"][0]["display"] == error_message
+
+
+@then('the lambda response does not contain an endpoint resource')
+def lambda_no_endpoints(fLambda_payload):
+    response = json.loads(fLambda_payload["body"])
+    # Check if any entry in response has a resourceType of "Endpoint"
+    endpoint_exists = any(
+        entry.get("resource", {}).get("resourceType") == "Endpoint"
+        for entry in response.get("entry", [])
+    )
+    assert fLambda_payload["statusCode"] == 200
+    assert len(response["entry"]) == 1
+    assert endpoint_exists is False
+
 
