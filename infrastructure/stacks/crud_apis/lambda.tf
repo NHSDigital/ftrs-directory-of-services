bcaf2084b699ef6699db4a46f15b20a6c258a097
AITR1 <178915313+AITR1@users.noreply.github.com>
2025-05-19 11:06:46 +0100

    SIA-498 Update for the function url

--- infrastructure/stacks/crud_apis/lambda.tf
+++ infrastructure/stacks/crud_apis/lambda.tf
@@ -99,7 +99,7 @@ data "aws_iam_policy_document" "dynamodb_access_policy" {
   }
 }
 
-resource "aws_lambda_function_url" "organisation_api_lambda_url" {
-  function_name      = "${local.resource_prefix}-${var.organisation_api_lambda_name}"
+resource "aws_lambda_function_url" "organisation_api" {
+  function_name      = module.organisation_api_lambda.function_name
   authorization_type = "AWS_IAM"
 }
