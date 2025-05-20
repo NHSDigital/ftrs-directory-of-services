1e792d08cf540fe007363d589bd5c0eff56b5b4f
nhs-shruthi-gowda <shruthi.gowda1@nhs.net>
2025-05-20 14:47:12 +0100

    FDOS-234 added unit test and some clean up

--- services/crud-apis/healthcare_service/app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from healthcare_service.app.router import healthcare
-
-app = FastAPI()
-app.include_router(healthcare.router)
--- services/crud-apis/healthcare_service/hcserver.py
+++ services/crud-apis/healthcare_service/hcserver.py
@@ -5,7 +5,7 @@ def run() -> None:
     """
     Run the FastAPI application with Uvicorn server.
     """
-    uvicorn.run("healthcare_service.app.main:app", host="0.0.0.0", port=6000, reload=True)
+    uvicorn.run("healthcare_service.app.handler_healthcare_service:app", host="0.0.0.0", port=7000, reload=True)
 
 if __name__ == "__main__":
    run()
