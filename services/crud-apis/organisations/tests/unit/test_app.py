f3fb1de3086b8c023fa61722af0e2c33b4793378
emab1 <178916340+emab1@users.noreply.github.com>
2025-05-21 11:47:17 +0100

    FDOS-237 API ReadMany organisations

--- services/crud-apis/organisations/tests/unit/test_app.py
+++ services/crud-apis/organisations/tests/unit/test_app.py
@@ -14,6 +14,7 @@ from organisations.app import (
     get_outdated_fields,
     get_repository,
     get_table_name,
+    readMany_organisations,
     update_organisation,
 )
 from organisations.settings import AppSettings
@@ -248,6 +249,21 @@ def test_get_org_id_not_found(mock_get_repository: MagicMock) -> None:
 
     assert str(e.value.status_code) == "404"
     assert str(e.value.detail) == "Organisation not found"
+
+
+@patch("organisations.app.get_repository")
+def test_readMany_organisations_not_found(
+    mock_get_repository: MagicMock,
+) -> None:
+    mock_repo = MagicMock()
+    mock_repo.get_all.return_value = None
+    mock_get_repository.return_value = mock_repo
+
+    with pytest.raises(HTTPException) as e:
+        readMany_organisations(AppSettings(ENVIRONMENT="test"))
+
+    assert str(e.value.status_code) == "404"
+    assert str(e.value.detail) == "Unable to retrieve all organisations"
 
 
 def test_get_repository() -> None:
