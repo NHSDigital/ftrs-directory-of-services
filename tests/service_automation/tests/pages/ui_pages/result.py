abd1008f39d63196f8b13975047370b68ec40e5e
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2025-05-21 10:47:51 +0100

    DOSIS-13501 DOSIS-13051 Fixing ui test

--- tests/service_automation/tests/pages/ui_pages/result.py
+++ tests/service_automation/tests/pages/ui_pages/result.py
@@ -1,13 +1,13 @@
 """
 This module contains DirectoryofServices,
-the page object for the MFA FAQ page.
+the page object for the new account page.
 """
 
 from playwright.sync_api import Page
 from typing import List
 
 
-class UserTestMfaHelpPage:
+class NewAccountPage:
 
     def __init__(self, page: Page) -> None:
         self.page = page
--- tests/service_automation/tests/step_definitions/ui_steps/test_ui_search.py
+++ tests/service_automation/tests/step_definitions/ui_steps/test_ui_search.py
@@ -3,8 +3,8 @@ These tests cover sample ui requests.
 """
 import pytest
 import allure
-from pages.ui_pages.result import UserTestMfaHelpPage
-from pages.ui_pages.search import UserTestLoginPage
+from pages.ui_pages.result import NewAccountPage
+from pages.ui_pages.search import LoginPage
 from playwright.sync_api import sync_playwright, expect, Page
 
 @pytest.fixture(scope="session")
@@ -21,17 +21,17 @@ def api_request_context():
 @pytest.mark.ui
 def test_basic_ui_search(
     page: Page,
-    search_page: UserTestLoginPage,
-    result_page: UserTestMfaHelpPage) -> None:
+    search_page: LoginPage,
+    result_page: NewAccountPage) -> None:
 
     # Given the UserTest home page is displayed
-    with allure.step("Given the UserTest home page is displayed"):
+    with allure.step("Given the home page is displayed"):
         search_page.load()
 
-    # When the MFA help link is clicked
-    with allure.step("When the MFA help link is clicked"):
+    # When the new account link is clicked
+    with allure.step("When the request new account link is clicked"):
         search_page.search()
 
-    # Then the helptest is displayed
-    with allure.step("Then the helptest is displayed"):
-        expect(result_page.page.get_by_text("How you log into DoS is changing")).to_be_visible
+    # Then the Request account form is displayed
+    with allure.step("Then  Request account is displayed"):
+        expect(result_page.page.get_by_text("Request account")).to_be_visible
