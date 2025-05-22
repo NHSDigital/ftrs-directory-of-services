c901b4564007e595eb997138aa6283c502f7e9e9
Tamara Goldschmidt <77108863+tamaragoldschmidt@users.noreply.github.com>
2025-05-22 14:53:19 +0100

    DOSIS-13501 tests for branch (#138)
    * DOSIS-13501 DOSIS-13051 Setting up schema test
    
    * DOSIS-13501 DOSIS-13051 Adding in odscode validation tests
    
    * DOSIS-13501 DOSIS-13051 Adding more odscode validatino tests
    
    * DOSIS-13501 DOSIS-13051 Adding more validation tests
    
    * DOSIS-13501 DOSIS-13051 Temp fix for dependency scanning
    
    * DOSIS-13501 DOSIS-13051 Fixing ui test
    
    * DOSIS-13501 Updating tests and schema
    
    * DOSIS-13501 Removing logger outputs
    
    * DOSIS-13501 Removing temp grype fix
    
    * DOSIS-13501 Removing temp grype fix
    
    * DOSIS-13501 Updating reqruiement versions of jsonschema
    
    * DOSIS-13501 Changes after code review
    
    * DOSIS-13501 Changes after code review

--- tests/service_automation/tests/pages/ui_pages/search.py
+++ tests/service_automation/tests/pages/ui_pages/search.py
@@ -6,16 +6,16 @@ the login page.
 from playwright.sync_api import Page
 
 
-class UserTestLoginPage:
+class LoginPage:
 
     URL = 'https://www.directoryofservices.nhs.uk/'
 
     def __init__(self, page: Page) -> None:
         self.page = page
-        self.mfa_help_link = page.locator('[href*="/app/controllers/mfa/mfa.php"]')
+        self.new_account_link = page.get_by_role("link", name="Request an account")
 
     def load(self) -> None:
         self.page.goto(self.URL)
 
     def search(self) -> None:
-        self.mfa_help_link.click()
+        self.new_account_link.click()
