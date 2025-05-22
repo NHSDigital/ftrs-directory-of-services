56802342ea137559ba718c99e2d202511a635a07
Thomas Cordall <thomas.cordall@kainos.com>
2025-05-22 15:25:39 +0100

    SIA-499 added test fix and put load data into etl pipeline

--- infrastructure/stacks/etl_ods/variables.tf
+++ infrastructure/stacks/etl_ods/variables.tf
@@ -54,3 +54,27 @@ variable "s3_versioning" {
   description = "Whether to enable versioning on the S3 bucket"
   type        = bool
 }
+
+variable "delay_seconds" {
+  description = "The number of seconds a message should be invisible to consumers"
+}
+
+variable "visibility_timeout_seconds" {
+  description = "How long a message remains invisible to other consumers after being received by one consumer"
+}
+
+variable "max_message_size" {
+  description = "The maximum size of the message"
+}
+
+variable "message_retention_seconds" {
+  description = "How long the SQS queue keeps a message"
+}
+
+variable "receive_wait_time_seconds" {
+  description = "Time period that a request could wait for a message to become available in the sqs queue"
+}
+
+variable "sqs_managed_sse_enabled" {
+  description = "Enables Server-Side Encryption for messages stored in the queue"
+}
