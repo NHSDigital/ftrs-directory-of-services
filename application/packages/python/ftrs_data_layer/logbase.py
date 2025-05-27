a6adc766ec5532327a0a661d1de595c5e3eb1df7
prjh2-nhs <178916511+prjh2-nhs@users.noreply.github.com>
2025-05-27 16:09:39 +0100

    FDOS-197 Fix ruff format

--- application/packages/python/ftrs_data_layer/logbase.py
+++ application/packages/python/ftrs_data_layer/logbase.py
@@ -37,51 +37,129 @@ class DDBLogBase(LogBase):
     DDB_CORE_014 = LogReference(level=ERROR, message="Error performing batch write")
     DDB_CORE_015 = LogReference(level=ERROR, message="Unprocessed items in batch write")
 
+
 class ETLPipelineLogBase(LogBase):
     """
     LogBase for ETL Pipeline operations
     """
-    ETL_EXTRACT_001 = LogReference(level=INFO, message="Extracting data to {output_path}")
-    ETL_EXTRACT_002 = LogReference(level=INFO, message="Percentage of service profiles: {service_profiles_percentage}%")
-    ETL_EXTRACT_003 = LogReference(level=INFO, message="Percentage of all data fields: {data_fields_percentage}%")
-    ETL_EXTRACT_004 = LogReference(level=INFO, message="Data extraction completed successfully.")
 
-    ETL_TRANSFORM_001 = LogReference(level=INFO, message="Transforming data from {input_path} to {output_path}")
-    ETL_TRANSFORM_002 = LogReference(level=INFO, message="Transform completed successfully.")
+    ETL_EXTRACT_001 = LogReference(
+        level=INFO, message="Extracting data to {output_path}"
+    )
+    ETL_EXTRACT_002 = LogReference(
+        level=INFO,
+        message="Percentage of service profiles: {service_profiles_percentage}%",
+    )
+    ETL_EXTRACT_003 = LogReference(
+        level=INFO, message="Percentage of all data fields: {data_fields_percentage}%"
+    )
+    ETL_EXTRACT_004 = LogReference(
+        level=INFO, message="Data extraction completed successfully."
+    )
 
-    ETL_LOAD_001 = LogReference(level=INFO, message="Loading {len_input_df} {table_value}s into {table_value}")
-    ETL_LOAD_002 = LogReference(level=INFO, message="Loaded {count} {table_value}s into the database.")
-    ETL_LOAD_003 = LogReference(level=INFO, message="Data loaded successfully into {env_value} environment, workspace: {workspace_value}")
+    ETL_TRANSFORM_001 = LogReference(
+        level=INFO, message="Transforming data from {input_path} to {output_path}"
+    )
+    ETL_TRANSFORM_002 = LogReference(
+        level=INFO, message="Transform completed successfully."
+    )
+
+    ETL_LOAD_001 = LogReference(
+        level=INFO, message="Loading {len_input_df} {table_value}s into {table_value}"
+    )
+    ETL_LOAD_002 = LogReference(
+        level=INFO, message="Loaded {count} {table_value}s into the database."
+    )
+    ETL_LOAD_003 = LogReference(
+        level=INFO,
+        message="Data loaded successfully into {env_value} environment, workspace: {workspace_value}",
+    )
 
     ETL_RESET_001 = LogReference(level=INFO, message="Initializing tables...")
-    ETL_RESET_002 = LogReference(level=ERROR, message="The init option is only supported for the local environment.")
-    ETL_RESET_003 = LogReference(level=INFO, message="Table {table_name} created successfully.")
-    ETL_RESET_004 = LogReference(level=INFO, message="Table {table_name} already exists.")
-    ETL_RESET_005 = LogReference(level=ERROR, message="Invalid environment: {env}. Only 'dev' and 'local' are allowed.")
-    ETL_RESET_006 = LogReference(level=INFO, message="Deleted {count} items from {table_name}")
-    ETL_RESET_007 = LogReference(level=ERROR, message="Unsupported entity type: {entity_type}")
+    ETL_RESET_002 = LogReference(
+        level=ERROR,
+        message="The init option is only supported for the local environment.",
+    )
+    ETL_RESET_003 = LogReference(
+        level=INFO, message="Table {table_name} created successfully."
+    )
+    ETL_RESET_004 = LogReference(
+        level=INFO, message="Table {table_name} already exists."
+    )
+    ETL_RESET_005 = LogReference(
+        level=ERROR,
+        message="Invalid environment: {env}. Only 'dev' and 'local' are allowed.",
+    )
+    ETL_RESET_006 = LogReference(
+        level=INFO, message="Deleted {count} items from {table_name}"
+    )
+    ETL_RESET_007 = LogReference(
+        level=ERROR, message="Unsupported entity type: {entity_type}"
+    )
+
 
 class UtilsLogBase(LogBase):
     """
     LogBase for utils operations
     """
-    UTILS_FILEIO_001 = LogReference(level=INFO, message="Reading from S3 path: {file_path}")
-    UTILS_FILEIO_002 = LogReference(level=INFO, message="Reading from local file path: {file_path}")
-    UTILS_FILEIO_003 = LogReference(level=ERROR, message="Unsupported path type: {path_type}")
-    UTILS_FILEIO_004 = LogReference(level=INFO, message="Writing to S3 path: {file_path}")
-    UTILS_FILEIO_005 = LogReference(level=INFO, message="Writing to local file path: {file_path}")
-    UTILS_FILEIO_006 = LogReference(level=ERROR, message="Unsupported path type: {path_type}")
 
-    UTILS_SECRET_001 = LogReference(level=ERROR, message="The environment or project name does not exist")
+    UTILS_FILEIO_001 = LogReference(
+        level=INFO, message="Reading from S3 path: {file_path}"
+    )
+    UTILS_FILEIO_002 = LogReference(
+        level=INFO, message="Reading from local file path: {file_path}"
+    )
+    UTILS_FILEIO_003 = LogReference(
+        level=ERROR, message="Unsupported path type: {path_type}"
+    )
+    UTILS_FILEIO_004 = LogReference(
+        level=INFO, message="Writing to S3 path: {file_path}"
+    )
+    UTILS_FILEIO_005 = LogReference(
+        level=INFO, message="Writing to local file path: {file_path}"
+    )
+    UTILS_FILEIO_006 = LogReference(
+        level=ERROR, message="Unsupported path type: {path_type}"
+    )
 
-    UTILS_VALIDATOR_001 = LogReference(level=INFO, message="Bucket {bucket_name} exists and is accessible.")
-    UTILS_VALIDATOR_002 = LogReference(level=WARNING, message="Error checking bucket {bucket_name}: {e}")
-    UTILS_VALIDATOR_003 = LogReference(level=INFO, message="Object {object_key} exists in bucket {bucket_name}.")
-    UTILS_VALIDATOR_004 = LogReference(level=WARNING, message="Error checking object {object_key} in bucket {bucket_name}: {e}")
-    UTILS_VALIDATOR_005 = LogReference(level=ERROR, message="Invalid S3 URI: {s3_uri}. Please provide a valid S3 URI and confirm you have access to the S3 bucket.")
-    UTILS_VALIDATOR_006 = LogReference(level=ERROR, message="File does not exist in S3: {s3_uri}. Please provide a valid S3 URI to an existing file.")
-    UTILS_VALIDATOR_007 = LogReference(level=ERROR, message="File already exists in S3: {s3_uri}. Please provide a different S3 URI.")
-    UTILS_VALIDATOR_008 = LogReference(level=ERROR, message="Parent directory does not exist: {parsed_path_parent}. Please provide a valid path to a file.")
-    UTILS_VALIDATOR_009 = LogReference(level=ERROR, message="File does not exist: {parsed_path}. Please provide a valid path to a file.")
-    UTILS_VALIDATOR_010 = LogReference(level=ERROR, message="File already exists: {parsed_path}. Please provide a different path.")
+    UTILS_SECRET_001 = LogReference(
+        level=ERROR, message="The environment or project name does not exist"
+    )
 
+    UTILS_VALIDATOR_001 = LogReference(
+        level=INFO, message="Bucket {bucket_name} exists and is accessible."
+    )
+    UTILS_VALIDATOR_002 = LogReference(
+        level=WARNING, message="Error checking bucket {bucket_name}: {e}"
+    )
+    UTILS_VALIDATOR_003 = LogReference(
+        level=INFO, message="Object {object_key} exists in bucket {bucket_name}."
+    )
+    UTILS_VALIDATOR_004 = LogReference(
+        level=WARNING,
+        message="Error checking object {object_key} in bucket {bucket_name}: {e}",
+    )
+    UTILS_VALIDATOR_005 = LogReference(
+        level=ERROR,
+        message="Invalid S3 URI: {s3_uri}. Please provide a valid S3 URI and confirm you have access to the S3 bucket.",
+    )
+    UTILS_VALIDATOR_006 = LogReference(
+        level=ERROR,
+        message="File does not exist in S3: {s3_uri}. Please provide a valid S3 URI to an existing file.",
+    )
+    UTILS_VALIDATOR_007 = LogReference(
+        level=ERROR,
+        message="File already exists in S3: {s3_uri}. Please provide a different S3 URI.",
+    )
+    UTILS_VALIDATOR_008 = LogReference(
+        level=ERROR,
+        message="Parent directory does not exist: {parsed_path_parent}. Please provide a valid path to a file.",
+    )
+    UTILS_VALIDATOR_009 = LogReference(
+        level=ERROR,
+        message="File does not exist: {parsed_path}. Please provide a valid path to a file.",
+    )
+    UTILS_VALIDATOR_010 = LogReference(
+        level=ERROR,
+        message="File already exists: {parsed_path}. Please provide a different path.",
+    )
