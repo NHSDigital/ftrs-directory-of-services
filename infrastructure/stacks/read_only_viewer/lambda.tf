88144ee61e128f991cf3fc1e576dc93b735ea633
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2025-05-30 10:33:57 +0100

    FDOS-222 Add permissions to read-only viewer lambda function

--- infrastructure/stacks/read_only_viewer/lambda.tf
+++ infrastructure/stacks/read_only_viewer/lambda.tf
@@ -13,14 +13,43 @@ module "frontend_lambda" {
   timeout                 = var.frontend_lambda_connection_timeout
   memory_size             = var.frontend_lambda_memory_size
 
-  number_of_policy_jsons = "0"
-  policy_jsons           = []
-  security_group_ids     = null
-  subnet_ids             = null
-  layers                 = []
-  environment_variables  = {}
+  number_of_policy_jsons = "2"
+  policy_jsons = [
+    data.aws_iam_policy_document.ssm_access_policy.json,
+    data.aws_iam_policy_document.execute_api_policy.json
+  ]
+  security_group_ids    = null
+  subnet_ids            = null
+  layers                = []
+  environment_variables = {}
 }
 
+data "aws_iam_policy_document" "ssm_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath"
+    ]
+    resources = [
+      "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/ftrs-dos-${var.environment}-crud-apis${local.workspace_suffix}/endpoint"
+    ]
+  }
+}
+
+# TODO: This is overly permissive and should be resolved when we have control over stack deployment order.
+data "aws_iam_policy_document" "execute_api_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "execute-api:Invoke"
+    ]
+    resources = [
+      "arn:aws:execute-api:*:*:*/*/*/*/*"
+    ]
+  }
+}
 
 resource "aws_lambda_function_url" "frontend_lambda_url" {
   function_name      = module.frontend_lambda.lambda_name
