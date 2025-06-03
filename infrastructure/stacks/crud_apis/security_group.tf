42b526a9a2650a1d4e3d040714b13a548deb40cb
emab1 <178916340+emab1@users.noreply.github.com>
2025-06-03 11:22:14 +0100

    FDOS-233 Add lambda terraform for locations

--- infrastructure/stacks/crud_apis/security_group.tf
+++ infrastructure/stacks/crud_apis/security_group.tf
@@ -27,3 +27,18 @@ resource "aws_vpc_security_group_egress_rule" "healthcare_service_api_allow_443"
   to_port           = 443
   from_port         = 443
 }
+
+resource "aws_security_group" "location_api_lambda_security_group" {
+  name        = "${local.resource_prefix}-${var.location_api_lambda_name}${local.workspace_suffix}-sg"
+  description = "Security group for crud api lambda"
+
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "location_api_allow_443" {
+  security_group_id = aws_security_group.location_api_lambda_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  to_port           = 443
+  from_port         = 443
+}
