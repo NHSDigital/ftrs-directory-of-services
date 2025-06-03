4d4affaed6f70b9c4a1bd5fe3024d90077dbc120
DomBaker <dominic.baker1@nhs.net>
2025-06-03 13:58:42 +0100

    DOSIS-13071 Add truststore init config for mtls

--- infrastructure/stacks/account_wide/truststore.tf
+++ infrastructure/stacks/account_wide/truststore.tf
@@ -3,10 +3,3 @@ module "trust_store_s3_bucket" {
   source      = "../../modules/s3"
   bucket_name = "${local.account_prefix}-${var.s3_trust_store_bucket_name}"
 }
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = module.trust_store_s3_bucket.s3_bucket_arn
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
