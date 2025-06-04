9d801217d71abbc853b21f1552798b17d10c34c9
DomBaker <dominic.baker1@nhs.net>
2025-06-04 12:34:59 +0100

    DOSIS-1307 Supply correct function name for api-gateway integration

--- infrastructure/stacks/gp_search/apig-search.tf
+++ infrastructure/stacks/gp_search/apig-search.tf
@@ -21,7 +21,7 @@ module "search_integrations_get" {
   account_id           = local.account_id
   rest_api_id          = module.search_rest_api.rest_api_id
   http_method          = aws_api_gateway_method.search_get.http_method
-  lambda_function_name = var.lambda_name
+  lambda_function_name = "${local.resource_prefix}-${var.lambda_name}"
   gateway_resource_id  = aws_api_gateway_resource.search_resource.id
 
   depends_on = [
