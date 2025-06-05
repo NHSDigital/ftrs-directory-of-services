688a64a52c823f2854fdd4fd958956c715cb3600
DomBaker <dominic.baker1@nhs.net>
2025-06-05 08:49:40 +0100

    DOSIS-1307 Remove whitespace from lambda tf

--- infrastructure/stacks/gp_search/search_lambda.tf
+++ infrastructure/stacks/gp_search/search_lambda.tf
@@ -34,7 +34,6 @@ module "lambda" {
     "NAMESPACE"           = "${var.gp_search_service_name}${local.workspace_suffix}"
     "DYNAMODB_TABLE_NAME" = var.dynamodb_organisation_table_name
   }
-
 }
 resource "aws_vpc_security_group_egress_rule" "lambda_allow_443_egress_to_anywhere" {
   security_group_id = aws_security_group.gp_search_lambda_security_group.id
