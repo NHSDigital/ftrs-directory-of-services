4ebfb5b496095ec85d6d4df6b973d9e341cc730e
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-06-11 16:07:49 +0100

    FDOS-388 Report any failed stoppages individually

--- scripts/workflow/stop-ingestion-pipelines.sh
+++ scripts/workflow/stop-ingestion-pipelines.sh
@@ -2,19 +2,25 @@
 # This script stops all active ingestion pipelines in all workspaces in the specified AWS region.
 # It is intended primarily for use in non-production environments to ensure that no ingestion pipelines
 # are running unless actually required.
-# fail on first error
-set -e
+
 
 echo "Stopping any active ingestion pipelines for all tables in all workspaces in region ${AWS_REGION}"
 
+OUTCOME=0
 aws osis list-pipelines \
-  --region "${AWS_REGION}" | jq -r '.Pipelines[] | select( .Status | contains("ACTIVE") ) | .PipelineName' | while read -r name; \
+  --region "${AWS_REGION}" | jq -r '.Pipelines[] | select( .Status | contains("ACTIVE") ) | .PipelineName' | while read -r PIPELINE_NAME; \
   do \
-    echo "Stopping pipeline ${name}";
-    aws osis stop-pipeline \
-    --pipeline-name "${name}" \
+    echo "Stopping pipeline ${PIPELINE_NAME}"
+    RESPONSE=$(aws osis stop-pipeline \
+    --pipeline-name "${PIPELINE_NAME}" \
     --region "${AWS_REGION}" | \
-    jq -r '.Pipeline | .Status'
+    jq -r '.Pipeline | .Status')
+    echo "${RESPONSE}"
+    if [ "${RESPONSE}" != "STOPPING" ]; then
+      echo "Pipeline ${PIPELINE_NAME} did not stop successfully"
+      OUTCOME=1
+    fi
   done
+  exit ${OUTCOME}
 
 
