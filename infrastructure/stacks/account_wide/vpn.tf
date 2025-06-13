14cd54f92196fca1f63cc3689d99b4e1f5a00f72
Michaela McGrath <michaela.mcgrath1@nhs.net>
2025-06-13 12:22:05 +0100

    FDOS-389 Bootstrap test and prod environments (#186)

--- infrastructure/stacks/account_wide/vpn.tf
+++ infrastructure/stacks/account_wide/vpn.tf
@@ -1,10 +1,12 @@
 resource "aws_ec2_client_vpn_endpoint" "vpn" {
+  count = var.environment == "dev" ? 1 : 0
+
   description            = "${local.account_prefix}-vpn"
   vpc_id                 = module.vpc.vpc_id
   client_cidr_block      = var.vpc["vpn_subnet"]
   server_certificate_arn = data.aws_acm_certificate.vpn_cert.arn
 
-  security_group_ids = [aws_security_group.vpn_security_group.id]
+  security_group_ids = [aws_security_group.vpn_security_group[0].id]
 
   transport_protocol = "tcp"
   split_tunnel       = true
@@ -16,27 +18,28 @@ resource "aws_ec2_client_vpn_endpoint" "vpn" {
 
   connection_log_options {
     enabled               = true
-    cloudwatch_log_group  = aws_cloudwatch_log_group.vpn_log_group.name
-    cloudwatch_log_stream = aws_cloudwatch_log_stream.vpn_log_stream.name
+    cloudwatch_log_group  = aws_cloudwatch_log_group.vpn_log_group[0].name
+    cloudwatch_log_stream = aws_cloudwatch_log_stream.vpn_log_stream[0].name
   }
 }
 
 resource "aws_ec2_client_vpn_network_association" "database_association" {
-  for_each = toset(module.vpc.database_subnets)
+  count = var.environment == "dev" ? length(module.vpc.database_subnets) : 0
 
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
-  subnet_id              = each.value
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn[0].id
+  subnet_id              = module.vpc.database_subnets[count.index]
 }
 
 resource "aws_ec2_client_vpn_authorization_rule" "vpn_authorization_rule" {
-  for_each = { for s in module.vpc.database_subnet_objects : s.id => s }
+  for_each = var.environment == "dev" ? { for s in module.vpc.database_subnet_objects : s.id => s } : {}
 
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn[0].id
   target_network_cidr    = each.value.cidr_block
   authorize_all_groups   = true
 }
 
 resource "aws_security_group" "vpn_security_group" {
+  count       = var.environment == "dev" ? 1 : 0
   name        = "${local.account_prefix}-vpn-sg"
   description = "Security Group for Client VPN Endpoint"
   vpc_id      = module.vpc.vpc_id
@@ -57,19 +60,23 @@ resource "aws_security_group" "vpn_security_group" {
 }
 
 resource "aws_cloudwatch_log_group" "vpn_log_group" {
+  count             = var.environment == "dev" ? 1 : 0
   name              = "${local.account_prefix}-vpn-log-group"
   retention_in_days = var.log_group_retention_in_days
 }
 
 resource "aws_cloudwatch_log_stream" "vpn_log_stream" {
+  count          = var.environment == "dev" ? 1 : 0
   name           = "${local.account_prefix}-vpn-log-stream"
-  log_group_name = aws_cloudwatch_log_group.vpn_log_group.name
+  log_group_name = aws_cloudwatch_log_group.vpn_log_group[0].name
 }
 
 resource "aws_secretsmanager_secret" "vpn_ca_cert_secret" {
-  name = "/${var.repo_name}/${var.environment}/vpn-ca-cert"
+  count = var.environment == "dev" ? 1 : 0
+  name  = "/${var.repo_name}/${var.environment}/vpn-ca-cert"
 }
 
 resource "aws_secretsmanager_secret" "vpn_ca_pk_secret" {
-  name = "/${var.repo_name}/${var.environment}/vpn-ca-pk"
+  count = var.environment == "dev" ? 1 : 0
+  name  = "/${var.repo_name}/${var.environment}/vpn-ca-pk"
 }
