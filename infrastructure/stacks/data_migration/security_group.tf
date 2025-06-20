02fe0903181d37d76a18bed8a77e9b7d5a9ceca7
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-06-20 16:26:44 +0100

    FDOS-362 Modify the data source of von security group to be only available for dev

--- infrastructure/stacks/data_migration/security_group.tf
+++ infrastructure/stacks/data_migration/security_group.tf
@@ -15,7 +15,7 @@ data "aws_security_group" "rds_security_group" {
 resource "aws_vpc_security_group_ingress_rule" "rds_allow_ingress_from_vpn" {
   count                        = local.deploy_databases ? 1 : 0
   security_group_id            = aws_security_group.rds_security_group[0].id
-  referenced_security_group_id = data.aws_security_group.vpn_security_group.id
+  referenced_security_group_id = data.aws_security_group.vpn_security_group[0].id
   from_port                    = var.rds_port
   ip_protocol                  = data.aws_ec2_client_vpn_endpoint.client_vpn_endpoint.transport_protocol
   to_port                      = var.rds_port
