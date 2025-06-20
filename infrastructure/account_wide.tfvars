2081b827ec845208de43ea1e3a34811b011f2c0e
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-06-20 13:14:04 +0100

    Task/FDOS-362 test env baseline 1 (#210)
    * FDOS-362 Add environment prefix to ssm and opensearch
    
    * FDOS-362 Add environment later for consumption
    
    * FDOS-362 Add env to CloudWatch log group
    
    * FDOS-362 Add env to opensearch collection
    
    * FDOS-362 Manage the length issue
    
    * FDOS-362 Rename from col to osc

--- infrastructure/account_wide.tfvars
+++ infrastructure/account_wide.tfvars
@@ -6,6 +6,6 @@ opensearch_type                  = "SEARCH"
 opensearch_standby_replicas      = "DISABLED"
 opensearch_create_access_policy  = false
 opensearch_create_network_policy = false
-opensearch_collection_name       = "-collection"
+opensearch_collection_name       = "-osc"
 
 s3_trust_store_bucket_name = "truststore"
--- infrastructure/stacks/account_wide/opensearch_collection.tf
+++ infrastructure/stacks/account_wide/opensearch_collection.tf
@@ -2,7 +2,7 @@ module "opensearch_serverless" {
   # Module version: 1.7.0
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-opensearch.git//modules/collection?ref=6f7113edebb53779de7225634f7e914bc6d59c8c"
 
-  name             = "${var.project}${var.opensearch_collection_name}"
+  name             = "${var.project}-${var.environment}${var.opensearch_collection_name}"
   description      = "OpenSearch Serverless collection for DynamoDB ingestion"
   type             = var.opensearch_type
   standby_replicas = var.opensearch_standby_replicas
