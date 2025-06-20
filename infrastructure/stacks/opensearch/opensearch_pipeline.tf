4f985d9c7ae0ac75730d0444defdd5f724e9aa37
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-06-20 10:37:54 +0100

    FDOS-362 Add environment prefix to ssm and opensearch

--- infrastructure/stacks/opensearch/opensearch_pipeline.tf
+++ infrastructure/stacks/opensearch/opensearch_pipeline.tf
@@ -1,7 +1,7 @@
 resource "aws_osis_pipeline" "dynamodb_to_opensearch_osis_pipeline" {
   for_each = toset(var.dynamodb_table_names_for_opensearch)
 
-  pipeline_name = "${each.value}${local.workspace_suffix}"
+  pipeline_name = "${var.environment}${each.value}${local.workspace_suffix}"
   min_units     = var.osis_pipeline_min_units
   max_units     = var.osis_pipeline_max_units
 
@@ -21,7 +21,7 @@ resource "aws_osis_pipeline" "dynamodb_to_opensearch_osis_pipeline" {
     role_arn             = aws_iam_role.osis_pipelines_role.arn
     region               = var.aws_region
     opensearch_endpoint  = data.aws_opensearchserverless_collection.opensearch_serverless_collection.collection_endpoint
-    index_name           = "${each.value}${local.workspace_suffix}"
+    index_name           = "${var.environment}${each.value}${local.workspace_suffix}"
     s3_bucket            = module.s3.s3_bucket_id
     s3_prefix            = var.ddb_export_s3_prefix
     network_policy_name  = "pipeline-${aws_opensearchserverless_security_policy.opensearch_serverless_network_access_policy.name}"
