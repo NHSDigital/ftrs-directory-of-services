57505b1aa9309f0a2afb3b12415ed156bc6d0641
lukeb-nhs <luke.biddell1@nhs.net>
2025-06-23 12:09:34 +0100

    DOSIS-1314 Use gp search security group in health check lambda

--- infrastructure/stacks/gp_search/health_check_lambda.tf
+++ infrastructure/stacks/gp_search/health_check_lambda.tf
@@ -18,7 +18,7 @@ module "health_check_lambda" {
   )
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
-  security_group_ids = [aws_security_group.health_check_lambda_security_group.id]
+  security_group_ids = [aws_security_group.gp_search_lambda_security_group.id]
 
   environment_variables = {
     "ENVIRONMENT"         = var.environment
--- infrastructure/stacks/gp_search/security_group.tf
+++ infrastructure/stacks/gp_search/security_group.tf
@@ -4,10 +4,3 @@ resource "aws_security_group" "gp_search_lambda_security_group" {
 
   vpc_id = data.aws_vpc.vpc.id
 }
-
-resource "aws_security_group" "health_check_lambda_security_group" {
-  name        = "${local.resource_prefix}-${var.health_check_lambda_name}${local.workspace_suffix}-sg"
-  description = "Security group for health check lambda for gp search"
-
-  vpc_id = data.aws_vpc.vpc.id
-}
