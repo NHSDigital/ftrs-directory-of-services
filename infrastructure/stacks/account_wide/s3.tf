49ef6514209ef6136794d61cad0c98b7a225850f
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-06-25 22:23:20 +0100

    FDOS-362 Enable subnet flow logs

--- infrastructure/stacks/account_wide/s3.tf
+++ infrastructure/stacks/account_wide/s3.tf
@@ -1,6 +1,7 @@
+# VPC Flow Logs S3 Bucket and Resource Policy
 module "vpc_flow_logs_s3_bucket" {
   source        = "../../modules/s3"
-  bucket_name   = "${local.resource_prefix}-${var.flow_logs_bucket_name}"
+  bucket_name   = "${local.resource_prefix}-${var.vpc_flow_logs_bucket_name}"
   versioning    = var.flow_log_s3_versioning
   force_destroy = var.flow_log_s3_force_destroy
   lifecycle_rule_inputs = [
@@ -47,5 +48,58 @@ data "aws_iam_policy_document" "vpc_flow_logs_s3_bucket_policy_doc" {
     actions = ["s3:GetBucketAcl"]
 
     resources = [module.vpc_flow_logs_s3_bucket.s3_bucket_arn]
+  }
+}
+
+# Subnet Flow Logs S3 Bucket and Resource Policy
+module "subnet_flow_logs_s3_bucket" {
+  source        = "../../modules/s3"
+  bucket_name   = "${local.resource_prefix}-${var.subnet_flow_logs_bucket_name}"
+  versioning    = var.flow_log_s3_versioning
+  force_destroy = var.flow_log_s3_force_destroy
+  lifecycle_rule_inputs = [
+    {
+      id      = "delete_logs_older_than_x_days"
+      enabled = true
+      filter = {
+        prefix = "/"
+      }
+      expiration = {
+        days = var.flow_logs_s3_expiration_days
+      }
+    }
+  ]
+}
+
+resource "aws_s3_bucket_policy" "subnet_flow_logs_s3_bucket_policy" {
+  bucket = module.subnet_flow_logs_s3_bucket.s3_bucket_id
+  policy = data.aws_iam_policy_document.subnet_flow_logs_s3_bucket_policy_doc.json
+}
+
+data "aws_iam_policy_document" "subnet_flow_logs_s3_bucket_policy_doc" {
+  statement {
+    sid = "AWSLogDeliveryWrite"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    resources = ["${module.subnet_flow_logs_s3_bucket.s3_bucket_arn}/*"]
+  }
+
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = ["s3:GetBucketAcl"]
+
+    resources = [module.subnet_flow_logs_s3_bucket.s3_bucket_arn]
   }
 }
