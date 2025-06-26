1d1d483d466a3116d4ad699e1b8a221be46030d5
Thomas Cordall <thomas.cordall@kainos.com>
2025-06-26 11:46:38 +0100

    SIA-671 added mtls variables

---
+++ infrastructure/stacks/mtls/variables.tf
@@ -0,0 +1,8 @@
+variable "mtls_store_bucket_name" {
+  description = "WIP mtls bucket store"
+}
+
+variable "s3_versioning" {
+  description = "Whether to enable versioning on the S3 bucket"
+  type        = bool
+}
