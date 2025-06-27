c28cb053f602794ca221b0f9ac3829b0c3dabf98
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-06-27 11:44:41 +0100

    FDOS-401 Add IAM role and policy for cross acount domain name permissions

--- infrastructure/stacks/domain_name/data.tf
+++ infrastructure/stacks/domain_name/data.tf
@@ -1,4 +1,5 @@
 data "aws_ssm_parameter" "account_id" {
-  for_each = toset(var.aws_accounts)
-  name     = "/${var.project}/${each.value}/aws_account_id"
+  for_each = var.environment == "mgmt" ? { for account in var.aws_accounts : account => account } : {}
+
+  name = "/${var.project}/${each.key}/aws_account_id"
 }
