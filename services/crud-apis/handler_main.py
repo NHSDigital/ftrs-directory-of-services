815e6aaaec32e7f3a2c97901ae719f0ef0afb4f2
nhs-shruthi-gowda <shruthi.gowda1@nhs.net>
2025-07-01 14:45:44 +0100

    FDOS-219 fixed the format error and added the location

--- services/crud-apis/handler_main.py
+++ services/crud-apis/handler_main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-from organisations.app.router import organisation
 from healthcare_service.app.router import healthcare
+from location.app.router import location
+from organisations.app.router import organisation
 
 app = FastAPI(title="FTRS Services API")
 app.include_router(organisation.router, prefix="/organisation", tags=["Organisation"])
 app.include_router(healthcare.router, prefix="/healthcare-service", tags=["Healthcare"])
+app.include_router(location.router, prefix="/location", tags=["Location"])
 
 handler = Mangum(app, lifespan="off")
