6c4a13d0de0aecd172d4cc8f3a868ed7404c5d7c
DomBaker <dominic.baker1@nhs.net>
2025-07-02 12:30:30 +0100

    DOSIS-420 Add policy to allow creation of hosted zone for gp search

--- infrastructure/stacks/gp_search/route53.tf
+++ infrastructure/stacks/gp_search/route53.tf
@@ -1,3 +1,24 @@
 resource "aws_route53_zone" "gp_search_subdomain" {
   name = var.subdomain_fqdn
 }
+
+resource "aws_iam_policy" "route53_policy" {
+  name        = "Route53HostedZonePolicy"
+  description = "Policy to manage Route 53 Hosted Zone"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "ManageHostedZone",
+        Effect = "Allow",
+        Action = [
+          "route53:ChangeResourceRecordSets",
+          "route53:GetHostedZone",
+          "route53:ListResourceRecordSets",
+          "route53:ListHostedZonesByName"
+        ],
+        Resource = "arn:aws:route53::${local.account_id}:hostedzone/${aws_route53_zone.gp_search_subdomain.zone_id}"
+      }
+    ]
+  })
+}
