ba81f7407c1c914749ceea2387fd67d4001b3a4d
DomBaker <dominic.baker1@nhs.net>
2025-07-02 14:14:18 +0100

    DOSIS-420 convert policy to policy document for route53

--- infrastructure/stacks/gp_search/route53.tf
+++ infrastructure/stacks/gp_search/route53.tf
@@ -2,23 +2,21 @@ resource "aws_route53_zone" "gp_search_subdomain" {
   name = "servicesearch.${var.environment}.${local.root_domain_name}"
 }
 
-resource "aws_iam_policy" "route53_policy" {
-  name        = "Route53HostedZonePolicy"
-  description = "Policy to manage Route 53 Hosted Zone"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "ManageHostedZone",
-        Effect = "Allow",
-        Action = [
-          "route53:ChangeResourceRecordSets",
-          "route53:GetHostedZone",
-          "route53:ListResourceRecordSets",
-          "route53:ListHostedZonesByName"
-        ],
-        Resource = "arn:aws:route53::${local.account_id}:hostedzone/${aws_route53_zone.gp_search_subdomain.zone_id}"
-      }
+data "aws_iam_policy_document" "route53_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:GetChange",
+      "route53:ListResourceRecordSets",
+      "route53:ListHostedZonesByName",
+      "route53:ListHostedZones",
+      "route53:GetHostedZone"
     ]
-  })
+    resources = [
+      "arn:aws:route53::${local.account_id}:hostedzone/${aws_route53_zone.gp_search_subdomain.zone_id}"
+    ]
+  }
 }
+
+
