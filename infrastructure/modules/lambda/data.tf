744f75e0fe9b4dbb965d1b1b061623ae95f40bd7
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-07-02 11:10:40 +0100

    FDOS-408 Test module changes on deploy

---
+++ infrastructure/modules/lambda/data.tf
@@ -0,0 +1,23 @@
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.account_prefix}-vpc"]
+  }
+}
+
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["${var.account_prefix}-vpc-private-*"]
+  }
+}
+
+data "aws_subnet" "private_subnets_details" {
+  for_each = toset(data.aws_subnets.private_subnets.ids)
+  id       = each.value
+}
