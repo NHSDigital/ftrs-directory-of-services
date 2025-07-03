6615d1eafc2c2611057d3e8529cc8962a216243a
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-07-03 09:24:00 +0100

    FDOS-408 Refactor data and outputs

--- infrastructure/stacks/crud_apis/data.tf
+++ infrastructure/stacks/crud_apis/data.tf
@@ -21,3 +21,37 @@ data "aws_subnet" "private_subnets_details" {
   for_each = toset(data.aws_subnets.private_subnets.ids)
   id       = each.value
 }
+
+data "aws_iam_policy_document" "s3_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.crud_apis_bucket.s3_bucket_arn}/",
+      "${module.crud_apis_bucket.s3_bucket_arn}/*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "dynamodb_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Scan",
+      "dynamodb:Query"
+    ]
+    resources = flatten([
+      for table in local.dynamodb_tables : [
+        table.arn,
+        "${table.arn}/index/*"
+      ]
+    ])
+  }
+}
