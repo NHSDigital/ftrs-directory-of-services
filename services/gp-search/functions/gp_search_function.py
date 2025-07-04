b2822648f6c2edd339d2ef3b9f1c94463f16e236
lukeb-nhs <luke.biddell1@nhs.net>
2025-07-04 17:04:25 +0100

    DOSIS-1628 Add APIG correlation path

--- services/gp-search/functions/gp_search_function.py
+++ services/gp-search/functions/gp_search_function.py
@@ -1,5 +1,6 @@
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response
+from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from fhir.resources.R4B.fhirresourcemodel import FHIRResourceModel
 from pydantic import ValidationError
@@ -50,7 +51,11 @@ def create_response(status_code: int, fhir_resource: FHIRResourceModel) -> Respo
     )
 
 
-@logger.inject_lambda_context(log_event=True, clear_state=True)
+@logger.inject_lambda_context(
+    correlation_id_path=correlation_paths.API_GATEWAY_REST,
+    log_event=True,
+    clear_state=True,
+)
 @tracer.capture_lambda_handler
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     return app.resolve(event, context)
