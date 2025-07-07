683c5a539e178df00441109b5cf6da6882623531
Sindhu <nsindhupriya@hotmail.co.uk>
2025-07-07 16:34:38 +0100

    changing variable name truststore

--- infrastructure/stacks/account_wide/truststore.tf
+++ infrastructure/stacks/account_wide/truststore.tf
@@ -1,5 +1,5 @@
 module "trust_store_s3_bucket" {
   # This module creates an S3 bucket for the trust store used for MTLS Certificates.
   source      = "../../modules/s3"
-  bucket_name = "${local.account_prefix}-${var.s3_trust_store_bucket_name}"
+  bucket_name = "${local.account_prefix}-${local.s3_trust_store_bucket_name}"
 }
