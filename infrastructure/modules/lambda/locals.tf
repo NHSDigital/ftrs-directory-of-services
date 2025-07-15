ab7e502c90f87fb13f5a0f51ebcef1bfda550f39
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-07-15 09:59:27 +0100

    FDOS-489 Adding back the deny statement for lambda

--- infrastructure/modules/lambda/locals.tf
+++ infrastructure/modules/lambda/locals.tf
@@ -8,6 +8,7 @@ locals {
     data.aws_iam_policy_document.allow_private_subnet_policy.json,
     data.aws_iam_policy_document.limit_to_environment_vpc_policy.json,
     data.aws_iam_policy_document.enforce_vpc_lambda_policy.json,
+    data.aws_iam_policy_document.deny_lambda_function_access_policy.json,
     data.aws_iam_policy_document.vpc_access_policy.json
   ]))
 }
