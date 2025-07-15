e744bcaa40af05baba6c3adbe9d270d71fd3f540
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-07-15 16:36:29 +0100

    FDOS-489 Add KMS resource policy

---
+++ infrastructure/stacks/account_wide/kms.tf
@@ -0,0 +1,45 @@
+resource "aws_kms_key" "secrets_manager_key" {
+  count               = contains(["dev", "test"], var.environment) ? 1 : 0
+  description         = "KMS CMK for encrypting Secrets Manager secrets"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_key_policy" "secrets_manager_kms_key_policy" {
+  key_id = aws_kms_key.secrets_manager_key.id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Id      = "key-Secrets-Manager"
+      Statement = [
+        {
+          Sid    = "Enable IAM User Permissions"
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${local.account_id}:root"
+          },
+          Action   = "kms:*"
+          Resource = "*"
+        },
+        {
+          Sid    = "Enable Secrets Manager Permission"
+          Effect = "Allow"
+          Principal = {
+            Service = "secretsmanager.amazonaws.com"
+          },
+          Action = ["kms:Encrypt*",
+            "kms:Decrypt*",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey*",
+          "kms:Describe*"]
+          Resource = [aws_kms_key.secrets_manager_key.arn]
+          condition = {
+            StringEquals = {
+              "kms:ViaService"    = "secretsmanager.${var.region}.amazonaws.com"
+              "kms:CallerAccount" = local.account_id
+            }
+          }
+        }
+      ]
+    }
+  )
+}
