db0025e885ac720054da79c28790fafea19b74ca
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-07-16 09:38:23 +0100

    Task/FDOS-489 kms resource policy (#258)
    * FDOS-489 Add KMS resource policy
    
    * FDOS-489 Fix pipeline issuee
    
    * FDOS-489 Use correct variable
    
    * FDOS-489 Review comments addressed

---
+++ infrastructure/stacks/account_wide/kms.tf
@@ -0,0 +1,48 @@
+resource "aws_kms_key" "secrets_manager_key" {
+  count               = contains(["dev", "test"], var.environment) ? 1 : 0
+  description         = "KMS CMK for encrypting Secrets Manager secrets"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_key_policy" "secrets_manager_kms_key_policy" {
+  count  = contains(["dev", "test"], var.environment) ? 1 : 0
+  key_id = aws_kms_key.secrets_manager_key[0].id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Id      = "Key-Policy-For-Secrets-Manager"
+      Statement = [
+        {
+          Sid    = "Enable IAM User Permissions"
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${local.account_id}:root"
+          },
+          Action   = "kms:*"
+          Resource = "*"
+        },
+        {
+          Sid    = "Enable Secrets Manager Permission"
+          Effect = "Allow"
+          Principal = {
+            Service = "secretsmanager.amazonaws.com"
+          },
+          Action = [
+            "kms:Encrypt*",
+            "kms:Decrypt*",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey*",
+            "kms:Describe*"
+          ]
+          Resource = [aws_kms_key.secrets_manager_key[0].arn]
+          condition = {
+            StringEquals = {
+              "kms:ViaService"    = "secretsmanager.${var.aws_region}.amazonaws.com"
+              "kms:CallerAccount" = local.account_id
+            }
+          }
+        }
+      ]
+    }
+  )
+}
--- infrastructure/stacks/account_wide/secrets.tf
+++ infrastructure/stacks/account_wide/secrets.tf
@@ -1,17 +1,11 @@
-resource "aws_kms_key" "secrets_key" {
-  count               = contains(["dev", "test"], var.environment) ? 1 : 0
-  description         = "KMS CMK for encrypting Secrets Manager secrets"
-  enable_key_rotation = true
-}
-
 resource "aws_secretsmanager_secret" "api_ca_cert_secret" {
   count      = contains(["dev", "test"], var.environment) ? 1 : 0
   name       = "/${var.repo_name}/${var.environment}/api-ca-cert"
-  kms_key_id = aws_kms_key.secrets_key[0].arn
+  kms_key_id = aws_kms_key.secrets_manager_key[0].arn
 }
 
 resource "aws_secretsmanager_secret" "api_ca_pk_secret" {
   count      = contains(["dev", "test"], var.environment) ? 1 : 0
   name       = "/${var.repo_name}/${var.environment}/api-ca-pk"
-  kms_key_id = aws_kms_key.secrets_key[0].arn
+  kms_key_id = aws_kms_key.secrets_manager_key[0].arn
 }
