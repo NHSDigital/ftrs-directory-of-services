67a2126ccb1510a3d06812d5723215507d7d1855
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-07-21 15:40:50 +0100

    FDOS-336 Add test and prod roles artefact access

--- infrastructure/stacks/artefact_management/s3.tf
+++ infrastructure/stacks/artefact_management/s3.tf
@@ -18,7 +18,9 @@ data "aws_iam_policy_document" "artefacts_bucket_policy" {
         "arn:aws:iam::${local.account_id}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Developer_b0ffd523c3b8ddb9",
         "arn:aws:iam::${local.account_id}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Infrastructure_e5f5de072b3e7cf8",
         "${data.aws_iam_role.app_github_runner_iam_role.arn}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
       ]
     }
     actions = [
@@ -36,7 +38,9 @@ data "aws_iam_policy_document" "artefacts_bucket_policy" {
         "arn:aws:iam::${local.account_id}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Developer_b0ffd523c3b8ddb9",
         "arn:aws:iam::${local.account_id}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Infrastructure_e5f5de072b3e7cf8",
         "${data.aws_iam_role.app_github_runner_iam_role.arn}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
       ]
     }
     actions = [
