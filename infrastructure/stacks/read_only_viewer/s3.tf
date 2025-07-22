a83fd6f6a9d0842c90b6071026651744bb0ba338
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-07-22 12:27:32 +0100

    FDOS-416 Add policy to allow put object for cloudfront principal

--- infrastructure/stacks/read_only_viewer/s3.tf
+++ infrastructure/stacks/read_only_viewer/s3.tf
@@ -73,10 +73,50 @@ module "access_logging_bucket" {
 
 }
 
-resource "aws_s3_bucket_ownership_controls" "access_logging_bucket_ownership_controls" {
-  bucket = module.access_logging_bucket.s3_bucket_id
+data "aws_iam_policy_document" "access_logging_bucket_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
 
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+    actions = ["s3:PutObject"]
+    resources = [
+      "${module.access_logging_bucket.s3_bucket_arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values = [
+        module.read_only_viewer_cloudfront.cloudfront_distribution_arn,
+      ]
+    }
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        data.aws_iam_role.app_github_runner_iam_role.arn,
+        "arn:aws:iam::${data.aws_ssm_parameter.dos_aws_account_id_mgmt.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
+      ]
+    }
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "${module.access_logging_bucket.s3_bucket_arn}/*",
+    ]
   }
 }
+
+# resource "aws_s3_bucket_ownership_controls" "access_logging_bucket_ownership_controls" {
+#   bucket = module.access_logging_bucket.s3_bucket_id
+
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
