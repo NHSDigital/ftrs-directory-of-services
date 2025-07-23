2c19b143303d1903e32ababc4be3e2d50ae1f129
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-07-23 10:13:20 +0100

    FDOS-416 Add ignore for avd aws 0010

--- infrastructure/stacks/read_only_viewer/cloudfront.tf
+++ infrastructure/stacks/read_only_viewer/cloudfront.tf
@@ -1,3 +1,4 @@
+#trivy:ignore:AVD-AWS-0010
 module "read_only_viewer_cloudfront" {
   # Module version: v4.1.0
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudfront.git?ref=d66669f42ec922cb4b1acea8e4a17e5f6c6c9a15"
