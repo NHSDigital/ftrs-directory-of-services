e23ae39ce5801c0969ac6f4adaa236ea851bd386
lukeb-nhs <luke.biddell1@nhs.net>
2025-07-29 10:49:24 +0100

    DOSIS-1668 Ignore extra env variables to fix issues when creating subclass of Settings

--- application/packages/python/ftrs_common/utils/config.py
+++ application/packages/python/ftrs_common/utils/config.py
@@ -3,7 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
     env: str = Field("local", alias="ENVIRONMENT")
     workspace: str | None = Field(None, alias="WORKSPACE")
     endpoint_url: str | None = Field(None, alias="ENDPOINT_URL")
