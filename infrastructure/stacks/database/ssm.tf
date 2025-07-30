510de4834f028af1ef946472ad86670bdf7dcdce
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-07-30 14:34:14 +0100

    FDOS-461 Limit ssm parameter to default workspace

--- infrastructure/stacks/database/ssm.tf
+++ infrastructure/stacks/database/ssm.tf
@@ -1,4 +1,6 @@
 resource "aws_ssm_parameter" "backup_arns" {
+  count = local.deploy_backup_ssm ? 1 : 0
+
   name        = "/${local.resource_prefix}${local.workspace_suffix}/tables-backup-arns"
   description = "The latest backup ARNs for the DynamoDB tables"
   type        = "String"
