c94b73b8ca315268fe305cc522feec38731d254f
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2025-07-30 12:12:34 +0100

    FDOS-491 Add sqs file

---
+++ infrastructure/stacks/data_migration/sqs.tf
@@ -0,0 +1,22 @@
+resource "aws_sqs_queue" "rds_event_listener" {
+  name = "${local.resource_prefix}-rds-events"
+}
+
+resource "aws_sqs_queue_policy" "rds_event_listener_policy" {
+  queue_url = aws_sqs_queue.rds_event_listener.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action   = "sqs:SendMessage",
+        Resource = aws_sqs_queue.rds_event_listener.arn,
+      }
+    ]
+  })
+
+}
