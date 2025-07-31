4aaa62f6e9a1c2f48e8c1ba322226b96f1c88d7f
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-07-31 15:37:52 +0100

    FDOS-524 Lambda to use SQS id from SSM parameter

--- infrastructure/stacks/data_migration/lambda_function.py
+++ infrastructure/stacks/data_migration/lambda_function.py
@@ -7,15 +7,52 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 sqs = boto3.client("sqs")
-queue_url = os.environ["SQS_QUEUE_URL"]
+ssm = boto3.client("ssm")
+path = os.environ["SQS_SSM_PATH"]
 
 
 def lambda_handler(event, context):
-    logger.info("Queue URL: %s", queue_url)
+
+    message = get_message_from_event(event)
+
+    workspaces = get_dms_workspaces()
+
+    for workspace_queue_url in workspaces:
+        try:
+            response = sqs.send_message(
+                QueueUrl=workspace_queue_url, MessageBody=json.dumps(message)
+            )
+
+            logger.info(
+                "Message sent to SQS for workspace %s. Message ID: %s",
+                workspace_queue_url,
+                response.get("MessageId"),
+            )
+        except Exception as e:
+            logger.error(
+                "Failed to send message to SQS for workspace %s. Error: %s",
+                workspace_queue_url,
+                str(e),
+            )
+
+
+def get_message_from_event(event):
     logger.info("Received event: %s", json.dumps(event))
 
     message = {"source": "aurora_trigger", "event": event}
+    return message
 
-    response = sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
 
-    logger.info("Message sent to SQS. Message ID: %s", response.get("MessageId"))
+def get_dms_workspaces():
+    try:
+        paginator = ssm.get_paginator("get_parameters_by_path")
+        workspaces = []
+
+        for page in paginator.paginate(Path=path, Recursive=True, WithDecryption=True):
+            workspaces.extend([param["Value"] for param in page["Parameters"]])
+
+        logger.info("Retrieved DMS workspaces: %s", workspaces)
+        return workspaces
+    except Exception as e:
+        logger.error("Error retrieving DMS workspaces: %s", str(e))
+        raise e
