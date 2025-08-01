eb94952e2c8f5e6c40322e5c5c9628a526c67548
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-08-01 09:27:42 +0100

    FDOS-524 Add cloudwatch log retention for dms task

--- infrastructure/stacks/data_migration/ssm.tf
+++ infrastructure/stacks/data_migration/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "rds_event_listener_workspace_sqs_id" {
-  name  = "${var.sqs_ssm_path_for_ids}${local.resource_prefix}-sqs-param-${local.workspace_suffix}"
+  name  = "${var.sqs_ssm_path_for_ids}${var.environment}/${local.resource_prefix}-sqs-param${local.workspace_suffix}"
   type  = "String"
   value = aws_sqs_queue.rds_event_listener.id
 }
