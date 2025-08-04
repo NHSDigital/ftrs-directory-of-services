cc40cf92e5d831409beeca6adbd00b26d8aa0e4d
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-08-04 12:12:16 +0100

    FDOS-524 Fix pipeline failures

--- infrastructure/stacks/data_migration/secrets_manager.tf
+++ infrastructure/stacks/data_migration/secrets_manager.tf
@@ -44,13 +44,14 @@ resource "aws_secretsmanager_secret_version" "source_rds_credentials" {
 }
 
 resource "aws_secretsmanager_secret" "target_rds_credentials" {
-  count = local.deploy_databases && (var.environment == "dev" || var.environment == "test") ? 1 : 0
+  count = local.deploy_databases ? 1 : 0
 
   name = "/${var.project}/${var.environment}/target-rds-credentials"
 }
 
+//FIXME: Why does this need to be restricted to only dev and test
 resource "aws_secretsmanager_secret_version" "target_rds_credentials" {
-  count = local.deploy_databases && (var.environment == "dev" || var.environment == "test") ? 1 : 0
+  count = local.deploy_databases ? 1 : 0
 
   secret_id = aws_secretsmanager_secret.target_rds_credentials[0].id
   secret_string = jsonencode({
