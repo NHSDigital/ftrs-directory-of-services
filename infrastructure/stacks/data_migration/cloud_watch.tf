2d9b4b2578268d6ac3a5d977995f7235a1158ae3
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-08-04 12:12:16 +0100

    FDOS-524 Fix pipeline failures

--- infrastructure/stacks/data_migration/cloud_watch.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_cloudwatch_log_group" "dms_log_group" {
-  count = local.deploy_databases ? 1 : 0
-
-  name              = "/aws/dms/${local.resource_prefix}-etl-replication-task"
-  retention_in_days = var.cloudwatch_log_retention_days
-}
--- infrastructure/stacks/data_migration/sqs.tf
+++ infrastructure/stacks/data_migration/sqs.tf
@@ -1,5 +1,13 @@
+resource "aws_sqs_queue" "rds_event_listener_dlq" {
+  name = "${local.resource_prefix}-rds-events-dlq${local.workspace_suffix}"
+}
+
 resource "aws_sqs_queue" "rds_event_listener" {
   name = "${local.resource_prefix}-rds-events${local.workspace_suffix}"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.rds_event_listener_dlq.arn
+    maxReceiveCount     = 5
+  })
 }
 
 resource "aws_sqs_queue_policy" "rds_event_listener_policy" {
