e0396254d07a5cc2dfa10e69ea3a00e117c49020
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-08-12 23:17:50 +0100

    FDOS-524 Add event bridge to handle full replication completion

--- infrastructure/stacks/data_migration/eventbridge.tf
+++ infrastructure/stacks/data_migration/eventbridge.tf
@@ -1,26 +1,31 @@
 resource "aws_cloudwatch_event_rule" "dms_full_replication_task_completed" {
+  count = local.is_primary_environment ? 1 : 0
+
   name        = "${local.resource_prefix}-etl-full-replication-task-completed"
   description = "Trigger Lambda on DMS full replication task completion"
   event_pattern = jsonencode({
     "source" : ["aws.dms"],
-    "detail-type" : ["DMS Replication Task State Change"],
+    "detail-type" : ["DMS Full Replication Task State Change"],
     "detail" : {
       "ReplicationTaskId" : ["${aws_dms_replication_task.dms_full_replication_task[0].replication_task_id}"],
-      "Status" : ["completed"]
+      "Status" : ["stopped"]
     }
   })
 }
 
-# resource "aws_cloudwatch_event_target" "dms_full_replication_task_lambda" {
-#   rule      = aws_cloudwatch_event_rule.dms_full_replication_task_completed.name
-#   arn       = //FIXME Add Tom Lambda handler arn here
-#   # You can add input/transform here if needed
-# }
+resource "aws_cloudwatch_event_target" "dms_full_replication_task_lambda" {
+  count = local.is_primary_environment ? 1 : 0
 
-# resource "aws_lambda_permission" "allow_eventbridge_invoke" {
-#   statement_id  = "AllowExecutionFromEventBridge"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.dms_task_completion_handler.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.dms_full_replication_task_completed.arn
-# }
+  rule = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].name
+  arn  = module.queue_populator_lambda.lambda_function_arn
+}
+
+resource "aws_lambda_permission" "eventbridge_lambda_permission" {
+  count = local.is_primary_environment ? 1 : 0
+
+  statement_id  = "AllowLambdaExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = module.queue_populator_lambda.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].arn
+}
