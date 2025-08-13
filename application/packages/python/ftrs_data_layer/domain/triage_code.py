ce8fedcdf302910c0ec2550a699da1e483d46eb7
nhs-shruthi-gowda <shruthi.gowda1@nhs.net>
2025-08-13 11:56:54 +0100

    FDOS-375 Initial commit for table creation

---
+++ application/packages/python/ftrs_data_layer/domain/triage_code.py
@@ -0,0 +1,14 @@
+from datetime import datetime, UTC
+
+from pydantic import Field
+
+from ftrs_data_layer.domain import DBModel
+from ftrs_data_layer.domain.clinical_code import BaseClinicalCode
+
+class TriageCode(BaseClinicalCode,DBModel):
+    field:str | None = "item"
+    synonyms: list[str] = Field(default_factory=list)
+    time: int | None = 0
+    zCodeExists: bool | None
+    combinations: list[str] = Field(default_factory=list)
+    dx_group_ids: list[int] = Field(default_factory=list)
