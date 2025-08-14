6c3a57a94e66587b5f33b58a103e18402ae1679f
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-08-14 19:03:34 +0100

    FDOS-524 Fix pipeline issue

--- services/data-migration/pipeline/dms_db.py
+++ services/data-migration/pipeline/dms_db.py
@@ -47,7 +47,7 @@ def execute_rds_command(engine: Engine, rds_username: str, rds_password: str) ->
                 IF NOT EXISTS (
                 SELECT FROM pg_catalog.pg_roles WHERE rolname = '{rds_username}'
                 ) THEN
-                CREATE ROLE {rds_username} LOGIN PASSWORD :'password';
+                CREATE ROLE {rds_username} LOGIN PASSWORD '{rds_password}';
                 GRANT rds_replication TO {rds_username};
                 GRANT SELECT ON ALL TABLES IN SCHEMA public TO {rds_username};
                 END IF;
