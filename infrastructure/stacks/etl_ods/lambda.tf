9bb370c8c59aec552a3d96b45ceb697ece39c41a
AITR1 <178915313+AITR1@users.noreply.github.com>
2025-08-19 15:22:19 +0100

    SIA-699 Increase timeout for ETL processor (#332)

--- infrastructure/stacks/etl_ods/lambda.tf
+++ infrastructure/stacks/etl_ods/lambda.tf
@@ -25,7 +25,7 @@ module "processor_lambda" {
   s3_bucket_name          = local.artefacts_bucket
   s3_key                  = "${terraform.workspace}/${var.commit_hash}/${var.project}-${var.stack_name}-lambda-${var.application_tag}.zip"
   ignore_source_code_hash = false
-  timeout                 = var.lambda_connection_timeout
+  timeout                 = var.processor_lambda_connection_timeout
   memory_size             = var.lambda_memory_size
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
@@ -66,7 +66,7 @@ module "consumer_lambda" {
   s3_bucket_name          = local.artefacts_bucket
   s3_key                  = "${terraform.workspace}/${var.commit_hash}/${var.project}-${var.stack_name}-lambda-${var.application_tag}.zip"
   ignore_source_code_hash = false
-  timeout                 = var.lambda_connection_timeout
+  timeout                 = var.consumer_lambda_connection_timeout
   memory_size             = var.lambda_memory_size
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
