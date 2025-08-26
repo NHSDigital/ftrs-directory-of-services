51b277955dca4190df49adcb54ee7bd4a8057153
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-08-26 11:52:15 +0100

    FDOS-550 Move account var to domain name stack

--- infrastructure/common/common-variables.tf
+++ infrastructure/common/common-variables.tf
@@ -80,11 +80,6 @@ variable "dynamodb_table_names" {
   type        = list(string)
 }
 
-variable "aws_accounts" {
-  description = "List of AWS account environments"
-  type        = list(string)
-}
-
 variable "mgmt_account_id" {
   description = "Management account ID"
   type        = string
