d13493a2a097b46d4dfff3a5457bed2d91e6100a
ThomasC-Kainos <106971950+ThomasC-Kainos@users.noreply.github.com>
2025-08-27 10:49:40 +0100

    SIA-581 logging for invalid date parameter (#345)

--- application/packages/python/ftrs_data_layer/logbase.py
+++ application/packages/python/ftrs_data_layer/logbase.py
@@ -321,6 +321,10 @@ class OdsETLPipelineLogBase(LogBase):
         level=INFO,
         message="Fetching organisation uuid for ods code {ods_code}.",
     )
+    ETL_PROCESSOR_029 = LogReference(
+        level=WARNING,
+        message="Error processing date with code: {status_code} and message: {error_message}.",
+    )
     ETL_CONSUMER_001 = LogReference(
         level=INFO,
         message="Received event for ODS ETL consumer lambda.",
