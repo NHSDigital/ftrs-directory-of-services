490984eb4112c85f8c6d421fe0a9a62d102d6f5e
Sindhu Natarajan <nsindhupriya@hotmail.co.uk>
2025-08-29 09:05:38 +0100

    Allowing certificate and key to be deployed in int environment

--- infrastructure/stacks/account_wide/secrets.tf
+++ infrastructure/stacks/account_wide/secrets.tf
@@ -1,13 +1,15 @@
 resource "aws_secretsmanager_secret" "api_ca_cert_secret" {
   # checkov:skip=CKV2_AWS_57:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
-  count = contains(["dev", "test"], var.environment) ? 1 : 0
+  count = local.is_primary_environment ? 1 : 0
   name  = "/${var.repo_name}/${var.environment}/api-ca-cert"
+  description = "Public certificate for mTLS auth"
 }
 
 resource "aws_secretsmanager_secret" "api_ca_pk_secret" {
   # checkov:skip=CKV2_AWS_57:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
-  count = contains(["dev", "test"], var.environment) ? 1 : 0
+  count = local.is_primary_environment ? 1 : 0
   name  = "/${var.repo_name}/${var.environment}/api-ca-pk"
+  description = "Private key for mTLS auth"
 }
