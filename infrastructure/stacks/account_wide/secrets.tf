74e668a2ff7aa4df79921317389b33b82358898b
Sindhu Natarajan <nsindhupriya@hotmail.co.uk>
2025-08-29 15:46:06 +0100

    Making changes to push commit

--- infrastructure/stacks/account_wide/secrets.tf
+++ infrastructure/stacks/account_wide/secrets.tf
@@ -3,7 +3,7 @@ resource "aws_secretsmanager_secret" "api_ca_cert_secret" {
   # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   count       = local.is_primary_environment ? 1 : 0
   name        = "/${var.repo_name}/${var.environment}/api-ca-cert"
-  description = "Public certificate for mTLS auth"
+  description = "Public certificate for mTLS authentication"
 }
 
 resource "aws_secretsmanager_secret" "api_ca_pk_secret" {
@@ -11,5 +11,5 @@ resource "aws_secretsmanager_secret" "api_ca_pk_secret" {
   # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   count       = local.is_primary_environment ? 1 : 0
   name        = "/${var.repo_name}/${var.environment}/api-ca-pk"
-  description = "Private key for mTLS auth"
+  description = "Private key for mTLS authentication"
 }
