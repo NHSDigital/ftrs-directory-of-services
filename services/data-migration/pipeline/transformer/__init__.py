ece3c067ab153b436c36a4a1cccf0acdc5247d51
prjh2-nhs <178916511+prjh2-nhs@users.noreply.github.com>
2025-08-29 16:52:02 +0100

    FDOS-520 Create GP Protected Learning Time Services

--- services/data-migration/pipeline/transformer/__init__.py
+++ services/data-migration/pipeline/transformer/__init__.py
@@ -1,10 +1,14 @@
 from pipeline.transformer.base import ServiceTransformer, ServiceTransformOutput
 from pipeline.transformer.gp_enhanced_access import GPEnhancedAccessTransformer
 from pipeline.transformer.gp_practice import GPPracticeTransformer
+from pipeline.transformer.gp_protected_learning_time import (
+    GPProtectedLearningTimeTransformer,
+)
 
 SUPPORTED_TRANSFORMERS: list[ServiceTransformer] = [
     GPPracticeTransformer,
     GPEnhancedAccessTransformer,
+    GPProtectedLearningTimeTransformer,
 ]
 
 __all__ = [
