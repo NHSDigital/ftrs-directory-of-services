c17060d208fe0f104f3cbe299b58bc2e9cb4a456
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-04 16:13:42 +0100

    FDOS-565 Initial commit to manage default SG rules for the VPC

--- infrastructure/stacks/account_wide/vpc.tf
+++ infrastructure/stacks/account_wide/vpc.tf
@@ -39,6 +39,10 @@ module "vpc" {
   flow_log_destination_type = var.flow_log_destination_type
   flow_log_destination_arn  = module.vpc_flow_logs_s3_bucket.s3_bucket_arn
   flow_log_file_format      = var.flow_log_file_format
+
+  # Manage Default SG rules for the VPC
+  default_security_group_egress  = []
+  default_security_group_ingress = []
 }
 
 locals {
