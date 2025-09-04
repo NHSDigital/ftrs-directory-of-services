cc930b3e98bcd931e5bd025a5431e3dbc4e2857b
AITR1 <178915313+AITR1@users.noreply.github.com>
2025-09-04 15:28:20 +0100

    SIA-774 Fix to remove forward slash routing

--- services/crud-apis/organisations/app/handler_organisation.py
+++ services/crud-apis/organisations/app/handler_organisation.py
@@ -5,7 +5,7 @@ from mangum import Mangum
 
 from organisations.app.router import organisation
 
-app = FastAPI(title="Organisations API", root_path="/Organization")
+app = FastAPI(title="Organisations API")
 app.include_router(organisation.router)
 
 handler = Mangum(app, lifespan="off")
