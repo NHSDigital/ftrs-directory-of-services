c1d773fbf67e2bbed9c9ede610e5e95bd6284fad
Thomas Cordall <thomas.cordall@kainos.com>
2025-09-09 10:42:37 +0100

    SIA-602 length validation for name

--- services/crud-apis/organisations/app/services/validators.py
+++ services/crud-apis/organisations/app/services/validators.py
@@ -6,16 +6,22 @@ from organisations.app.models.organisation import (
 )
 
 NAME_EMPTY_ERROR = "Name cannot be empty."
+NAME_MAX_LENGTH_ERROR = "Name cannot be longer than 100 characters."
 CREATED_BY_EMPTY_ERROR = "createdBy cannot be empty."
 ODS_CODE_EMPTY_ERROR = "ODS code cannot be empty."
+
+NAME_MAX_LENGTH = 100
 
 
 class UpdatePayloadValidator(OrganisationUpdatePayload):
     @field_validator("name")
     def validate_name(cls, v: str) -> str:
-        """Validates the name field to ensure it is not empty or whitespace."""
+        """Validates the name field to ensure it is not empty or whitespace.
+        Checks the length of the field."""
         if not v.strip():
             raise ValueError(NAME_EMPTY_ERROR)
+        if len(v.strip()) > NAME_MAX_LENGTH:
+            raise ValueError(NAME_MAX_LENGTH_ERROR)
         return v
 
 
