b786bae15313b1531ad615c74cdeb16afdb66d98
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-11 09:39:57 +0100

    FDOS-565 Managed prefix list to SG and VPCE resource policy (#380)
    * FDOS-565 Added managed prefix list to SG used by Lambda and VPCE resource policy
    
    * FDOS-556 Modified the endpoint policy
    
    * FDOS-556 Modify the action for deny

--- infrastructure/stacks/crud_apis/security_group.tf
+++ infrastructure/stacks/crud_apis/security_group.tf
@@ -6,6 +6,15 @@ resource "aws_security_group" "organisation_api_lambda_security_group" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_organisation_api" {
+  security_group_id = aws_security_group.organisation_api_lambda_security_group.id
+  description       = "Organisation api egress rule to allow DynamoDB traffic"
+  prefix_list_id    = data.aws_prefix_list.dynamodb.id
+  ip_protocol       = "tcp"
+  from_port         = var.https_port
+  to_port           = var.https_port
+}
+
 # trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-511
 resource "aws_vpc_security_group_egress_rule" "organisation_api_allow_443" {
 
@@ -13,8 +22,8 @@ resource "aws_vpc_security_group_egress_rule" "organisation_api_allow_443" {
   description       = "Organisation api egress rule to allow 443"
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "tcp"
-  to_port           = 443
-  from_port         = 443
+  to_port           = var.https_port
+  from_port         = var.https_port
 }
 
 resource "aws_security_group" "healthcare_service_api_lambda_security_group" {
@@ -25,6 +34,15 @@ resource "aws_security_group" "healthcare_service_api_lambda_security_group" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_healthcare_service_api" {
+  security_group_id = aws_security_group.healthcare_service_api_lambda_security_group.id
+  description       = "Healthcare service api egress rule to allow DynamoDB traffic"
+  prefix_list_id    = data.aws_prefix_list.dynamodb.id
+  ip_protocol       = "tcp"
+  to_port           = var.https_port
+  from_port         = var.https_port
+}
+
 # trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-511
 resource "aws_vpc_security_group_egress_rule" "healthcare_service_api_allow_443" {
 
@@ -32,8 +50,8 @@ resource "aws_vpc_security_group_egress_rule" "healthcare_service_api_allow_443"
   description       = "Healthcare service api egress rule to allow 443"
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "tcp"
-  to_port           = 443
-  from_port         = 443
+  to_port           = var.https_port
+  from_port         = var.https_port
 }
 
 resource "aws_security_group" "location_api_lambda_security_group" {
@@ -44,12 +62,21 @@ resource "aws_security_group" "location_api_lambda_security_group" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_location_api" {
+  security_group_id = aws_security_group.location_api_lambda_security_group.id
+  description       = "Location api egress rule to allow DynamoDB traffic"
+  prefix_list_id    = data.aws_prefix_list.dynamodb.id
+  ip_protocol       = "tcp"
+  to_port           = var.https_port
+  from_port         = var.https_port
+}
+
 # trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-511
 resource "aws_vpc_security_group_egress_rule" "location_api_allow_443" {
   security_group_id = aws_security_group.location_api_lambda_security_group.id
   description       = "Location api egress rule to allow 443"
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "tcp"
-  to_port           = 443
-  from_port         = 443
+  to_port           = var.https_port
+  from_port         = var.https_port
 }
