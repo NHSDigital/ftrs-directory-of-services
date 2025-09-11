78033d957bdad8265d2f6186ce9febc5d7d8133a
lukeb-nhs <luke.biddell1@nhs.net>
2025-09-11 10:32:38 +0100

    DOSIS-1893 Use repository layer to get table status in health check

--- services/gp-search/health_check/health_check_function.py
+++ services/gp-search/health_check/health_check_function.py
@@ -1,25 +1,29 @@
-import boto3
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from ftrs_common.utils.db_service import get_service_repository
+from ftrs_data_layer.domain import Organisation
+from mypy_boto3_dynamodb import DynamoDBServiceResource
 
-from health_check.config import GpHealthCheckSettings
+app = APIGatewayRestResolver()
 
 
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
+@app.get("/_status")
+def get_status() -> Response:
     table_active = _is_table_active()
-
-    return {"statusCode": 200 if table_active else 500}
+    status_code = 200 if table_active else 500
+    return Response(status_code=status_code)
 
 
 def _is_table_active() -> bool:
     try:
-        dynamodb = boto3.client("dynamodb")
-        table_name = GpHealthCheckSettings().dynamodb_table_name
-
-        response = dynamodb.describe_table(TableName=table_name)
-
-        table_active = response.get("Table", {}).get("TableStatus", None) == "ACTIVE"
-
+        repository = get_service_repository(Organisation, "organisation")
+        table: DynamoDBServiceResource.Table = repository.table
+        table_status: str = table.table_status
     except Exception:
         return False
+    else:
+        return table_status == "ACTIVE"
 
-    return table_active
+
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
