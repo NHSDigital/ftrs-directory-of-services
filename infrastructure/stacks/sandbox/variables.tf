f6310ceca3cfd92eaa9bd9e51ae516755dff9b4f
Sindhu Natarajan <nsindhupriya@hotmail.co.uk>
2025-09-12 08:20:36 +0200

    Separation of module

---
+++ infrastructure/stacks/sandbox/variables.tf
@@ -0,0 +1,71 @@
+variable "application_tag" {
+  description = "The version or tag of the crud api application"
+  type        = string
+}
+
+variable "commit_hash" {
+  description = "The commit hash of the crud api application"
+  type        = string
+}
+
+variable "mock_api_lambda_runtime" {
+  description = "The runtime environment for the Lambda function"
+}
+
+variable "mock_api_lambda_name" {
+  description = "The name of the mock api Lambda function"
+}
+
+variable "mock_api_lambda_timeout" {
+  description = "The timeout for the mock api Lambda function"
+  type        = number
+}
+
+variable "mock_api_lambda_memory_size" {
+  description = "The memory size for the mock api Lambda function"
+  type        = number
+}
+
+variable "mock_api_lambda_handler" {
+  description = "The handler for the mock api Lambda function"
+  type        = string
+}
+
+variable "crud_apis_store_bucket_name" {
+  description = "The name of the S3 bucket to use for the crud apis"
+}
+
+variable "s3_versioning" {
+  description = "Whether to enable versioning on the S3 bucket"
+  type        = bool
+}
+
+variable "api_gateway_authorization_type" {
+  description = "The authorization type for the API Gateway"
+  type        = string
+}
+
+variable "api_gateway_payload_format_version" {
+  description = "The payload format version for the API Gateway"
+  type        = string
+}
+
+variable "api_gateway_integration_timeout" {
+  description = "The integration timeout for the API Gateway"
+  type        = number
+}
+
+variable "api_gateway_access_logs_retention_days" {
+  description = "The number of days to retain API Gateway access logs"
+  type        = number
+}
+
+variable "api_gateway_throttling_burst_limit" {
+  description = "The burst limit for API Gateway throttling"
+  type        = number
+}
+
+variable "api_gateway_throttling_rate_limit" {
+  description = "The rate limit for API Gateway throttling"
+  type        = number
+}
---
+++ services/sandbox/README.md
@@ -0,0 +1,13 @@
+# Internal CRUD APIs
+
+Directory containing the internal APIs triggered to make CRUD updates to the data store
+
+Any new internal API that makes CRUD updates should be added here
+
+## Table of Contents
+
+- [Organisation](#organisations)
+
+## Organisations
+
+This API is responsible for the updating of data in the Organisation table. More details can be found in the [README for the service](organisations/README.md)
