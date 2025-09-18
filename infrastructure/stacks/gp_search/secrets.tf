20cec3664c105fbb6c06d0f1e022184313bf11ee
DomBaker <dominic.baker1@nhs.net>
2025-09-18 14:45:55 +0100

    DOSIS-1812 Add secret managers for proxygen

--- infrastructure/stacks/gp_search/secrets.tf
+++ infrastructure/stacks/gp_search/secrets.tf
@@ -11,3 +11,17 @@ resource "aws_secretsmanager_secret" "proxygen_public_key" {
   name        = "/${var.project}/${var.environment}/proxygen-public-key"
   description = "public key for proxygen"
 }
+
+resource "aws_secretsmanager_secret" "proxygen_public_key" {
+  # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  name        = "/${var.project}/${var.environment}/proxygen-key-id"
+  description = "key id for proxygen"
+}
+
+resource "aws_secretsmanager_secret" "proxygen_public_key" {
+  # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  name        = "/${var.project}/${var.environment}/proxygen-client-id"
+  description = "client id for proxygen"
+}
