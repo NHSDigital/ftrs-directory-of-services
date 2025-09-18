9dfddc0eca498e57ce224bcb933e783b8b1d2595
DomBaker <dominic.baker1@nhs.net>
2025-09-18 15:59:18 +0100

    DOSIS-1812 update descriptions for secret managers

--- infrastructure/stacks/gp_search/secrets.tf
+++ infrastructure/stacks/gp_search/secrets.tf
@@ -2,26 +2,26 @@ resource "aws_secretsmanager_secret" "proxygen_private_key" {
   # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   name        = "/${var.project}/${var.environment}/proxygen-private-key"
-  description = "private key for proxygen"
+  description = "Private key for proxygen"
 }
 
 resource "aws_secretsmanager_secret" "proxygen_public_key" {
   # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   name        = "/${var.project}/${var.environment}/proxygen-public-key"
-  description = "public key for proxygen"
+  description = "Public key for proxygen"
 }
 
 resource "aws_secretsmanager_secret" "proxygen_key_id" {
   # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   name        = "/${var.project}/${var.environment}/proxygen-key-id"
-  description = "key id for proxygen"
+  description = "Key id for proxygen"
 }
 
 resource "aws_secretsmanager_secret" "proxygen_client_id" {
   # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   name        = "/${var.project}/${var.environment}/proxygen-client-id"
-  description = "client id for proxygen"
+  description = "Client id for proxygen"
 }
