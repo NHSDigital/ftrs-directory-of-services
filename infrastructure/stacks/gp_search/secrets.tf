4401bd454e812a88d4227442f989d0c28941e084
DomBaker <dominic.baker1@nhs.net>
2025-09-18 14:47:47 +0100

    DOSIS-1812 rename secret manager names in tf

--- infrastructure/stacks/gp_search/secrets.tf
+++ infrastructure/stacks/gp_search/secrets.tf
@@ -12,14 +12,14 @@ resource "aws_secretsmanager_secret" "proxygen_public_key" {
   description = "public key for proxygen"
 }
 
-resource "aws_secretsmanager_secret" "proxygen_public_key" {
+resource "aws_secretsmanager_secret" "proxygen_key_id" {
   # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   name        = "/${var.project}/${var.environment}/proxygen-key-id"
   description = "key id for proxygen"
 }
 
-resource "aws_secretsmanager_secret" "proxygen_public_key" {
+resource "aws_secretsmanager_secret" "proxygen_client_id" {
   # checkov:skip=CKV2_AWS_57: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   # checkov:skip=CKV_AWS_149: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
   name        = "/${var.project}/${var.environment}/proxygen-client-id"
