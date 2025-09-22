092f3281245eb35381f962e0c8987cd99ae89b3c
Thomas Cordall <thomas.cordall@kainos.com>
2025-09-22 13:06:43 +0100

    SIA-843 powertools correlation id middleware for crud

--- services/crud-apis/powertools_correlation_id.py
+++ services/crud-apis/powertools_correlation_id.py
@@ -1,6 +1,6 @@
 import uuid
 
-from aws_lambda_powertools.shared.functions import correlation_id
+from aws_lambda_powertools.logging import Logger
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
@@ -11,9 +11,13 @@ class PowertoolsCorrelationIdMiddleware(BaseHTTPMiddleware):
 
     This middleware:
     1. Extracts the correlation ID from request headers or generates a new one
-    2. Sets it in Lambda Powertools' correlation ID store
-    3. Adds it to response headers
+    2. Adds it to response headers
+    3. Makes it available to the AWS Lambda Powertools logger
     """
+
+    def __init__(self, app: any, logger: Logger = None) -> None:
+        super().__init__(app)
+        self.logger = logger
 
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
@@ -23,12 +27,14 @@ class PowertoolsCorrelationIdMiddleware(BaseHTTPMiddleware):
             "X-Correlation-ID", str(uuid.uuid4())
         )
 
-        # Set in Lambda Powertools
-        with correlation_id.set(correlation_id_value):
-            # Process the request
-            response = await call_next(request)
+        # Append correlation ID to logger if available
+        if self.logger:
+            self.logger.append_keys(correlation_id=correlation_id_value)
 
-            # Add to response headers
-            response.headers["X-Correlation-ID"] = correlation_id_value
+        # Process the request
+        response = await call_next(request)
 
-            return response
+        # Add to response headers
+        response.headers["X-Correlation-ID"] = correlation_id_value
+
+        return response
