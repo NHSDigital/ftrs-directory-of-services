bffb68aab14c338f15b409e04ef716820d07dcd2
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-23 09:50:46 +0100

    FDOS-56899 Address review comments around the metric name

--- infrastructure/stacks/account_wide/waf.tf
+++ infrastructure/stacks/account_wide/waf.tf
@@ -168,7 +168,7 @@ resource "aws_wafv2_web_acl" "waf_web_acl" {
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "friendly-metric-name"
+    metric_name                = "${local.resource_prefix}-metric"
     sampled_requests_enabled   = true
   }
 
