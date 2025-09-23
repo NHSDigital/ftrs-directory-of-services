e2d29cea49d772ec75c08ffceb20457ef6c0e78f
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2025-09-23 12:12:45 +0100

    SIA-501 Add checkov ignore for CMK requirement, take two

--- infrastructure/stacks/etl_ods/schedulers.tf
+++ infrastructure/stacks/etl_ods/schedulers.tf
@@ -5,8 +5,9 @@ resource "aws_scheduler_schedule_group" "etl_schedule_group" {
 }
 
 
-# checkov:skip=CKV_AWS_297: TODO Determine if we need a KMS key for Scheduler
+
 resource "aws_scheduler_schedule" "ods_etl_schedule" {
+  # checkov:skip=CKV_AWS_297: TODO Determine if we need a KMS key for Scheduler
   name        = "${local.resource_prefix}-ods-etl-schedule${local.workspace_suffix}"
   group_name  = aws_scheduler_schedule_group.etl_schedule_group.name
   description = "Schedule to trigger the ODS ETL processor lambda"
