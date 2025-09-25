6abf520f62e92dfe18824af64135b01ac82b6a55
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2025-09-25 16:43:40 +0100

    DOSIS-18121 Updating readme file

--- tests/service_automation/tests/utilities/common/resource_name.py
+++ tests/service_automation/tests/utilities/common/resource_name.py
@@ -7,11 +7,3 @@ def get_resource_name(project, workspace, env, stack, resource):
     resource_name = f"{project}-{env}-{stack}-{resource}{workspace_suffix}"
     logger.debug("resource name {}", resource_name)
     return resource_name
-
-
-def get_proxy_name(api_name,):
-    logger.debug(f"project: {project},  resource: {resource}, stack: {stack}, env: {env}, workspace: {workspace}")
-    workspace_suffix = f"-{workspace}" if workspace else ""
-    resource_name = f"{project}-{env}-{stack}-{resource}{workspace_suffix}"
-    logger.debug("resource name {}", resource_name)
-    return resource_name
