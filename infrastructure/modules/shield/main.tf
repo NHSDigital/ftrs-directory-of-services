6b2e2c6946a3a5ca5e5c3f9645a6c1a479ac7d63
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-26 23:01:47 +0100

    FDOS-601 Add shield module

---
+++ infrastructure/modules/shield/main.tf
@@ -0,0 +1,33 @@
+# Shield Advanced Protection for specified resources
+resource "aws_shield_protection" "shield_advanced_protection" {
+  for_each     = var.arns_to_protect
+  name         = "${var.resource_prefix}-${each.key}"
+  resource_arn = each.value
+
+  tags = {
+    Name = "${var.resource_prefix}-${each.key}"
+  }
+}
+
+# To set up proactive engagement for AWS Shield Advanced SRT team.
+# To be used only if we have business or enterprise support plan
+resource "aws_shield_proactive_engagement" "proactive_engagement" {
+  count = var.isProactiveEngagementEnabled ? 1 : 0
+
+  enabled = true
+
+  dynamic "emergency_contact" {
+    for_each = var.emergency_contacts
+    content {
+      email_address = each.email_address
+      phone_number  = each.phone_number
+      contact_notes = each.contact_notes
+    }
+  }
+}
+
+resource "aws_shield_drt_access_role_arn_association" "enable_srt_access" {
+  count = var.isSRTAccessEnabled ? 1 : 0
+
+  role_arn = aws_iam_role.srt_role.arn
+}
---
+++ infrastructure/modules/shield/variables.tf
@@ -0,0 +1,43 @@
+variable "arns_to_protect" {
+  description = "Map of name to arn to be protected with shield"
+  type        = map(string)
+  default     = {}
+}
+
+variable "evaluation_period" {
+  description = "The evaluation period for the CloudWatch alarm"
+  default     = 20
+  type        = string
+}
+
+variable "resource_prefix" {
+  description = "The prefix to use for resource names"
+  type        = string
+}
+
+variable "alarm_notification_email" {
+  description = "List of email addresses to receive SNS notifications for Shield DDoS alarms"
+  type        = list(string)
+}
+
+variable "emergency_contacts" {
+  description = "List of emergency contacts for Proactive engagement from AWS Shield Advanced SRT"
+  type = list(object({
+    email_address = string
+    phone_number  = string
+    contact_notes = optional(string)
+  }))
+  default = []
+}
+
+variable "isProactiveEngagementEnabled" {
+  description = "Whether to enable Proactive Engagement for AWS Shield Advanced"
+  type        = bool
+  default     = false
+}
+
+variable "isSRTAccessEnabled" {
+  description = "Whether to enable Shield Response Team (SRT) access"
+  type        = bool
+  default     = false
+}
