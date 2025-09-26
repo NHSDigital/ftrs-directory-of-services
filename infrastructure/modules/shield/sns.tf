78291e03c1a50c2fb4975f7585254aeb8ff2b378
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-26 23:01:47 +0100

    FDOS-601 Add shield module

---
+++ infrastructure/modules/shield/sns.tf
@@ -0,0 +1,16 @@
+# SNS Topic for Shield DDoS alerts
+resource "aws_sns_topic" "shield_ddos_alerts" {
+  name = "${var.resource_prefix}-shield-ddos-alerts"
+
+  tags = {
+    Name = "${var.resource_prefix}-shield-ddos-alerts"
+  }
+}
+
+# Email subscription for the SNS topic - one subscription for each email address
+resource "aws_sns_topic_subscription" "shield_ddos_email_subscription" {
+  for_each  = toset(var.alarm_notification_email)
+  topic_arn = aws_sns_topic.shield_ddos_alerts.arn
+  protocol  = "email"
+  endpoint  = each.value
+}
