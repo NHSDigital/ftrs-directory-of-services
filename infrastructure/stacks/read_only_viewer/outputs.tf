4451f327ee01f1662eb7ad1478fa3facce429162
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-29 10:20:31 +0100

    FDOS-601 Add CW metric alarm and route 53 health check for CloudFront 5xx errors

--- infrastructure/stacks/read_only_viewer/outputs.tf
+++ infrastructure/stacks/read_only_viewer/outputs.tf
@@ -2,3 +2,13 @@ output "read_only_viewer_cloudfront_distribution_url" {
   value       = module.read_only_viewer_cloudfront.cloudfront_distribution_domain_name
   description = "The CloudFront distribution URL for the read-only viewer"
 }
+
+output "read_only_viewer_cloudfront_distribution_id" {
+  value       = module.read_only_viewer_cloudfront.cloudfront_distribution_id
+  description = "The CloudFront distribution ID for the read-only viewer"
+}
+
+output "cloudfront_health_check_id" {
+  value       = aws_route53_health_check.cloudfront_5xx_errors.id
+  description = "The ID of the Route53 health check monitoring CloudFront 5xx errors"
+}
