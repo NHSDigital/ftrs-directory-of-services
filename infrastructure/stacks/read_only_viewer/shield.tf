422f8853c57e98dd5d9b078466532a7fec6cc0cb
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-29 14:32:44 +0100

    FDOS-601 Fix pipeline failure

--- infrastructure/stacks/read_only_viewer/shield.tf
+++ infrastructure/stacks/read_only_viewer/shield.tf
@@ -2,9 +2,9 @@ module "shield_protection" {
   count  = local.is_primary_environment ? 1 : 0
   source = "../../modules/shield"
 
-  arn_to_protect                     = aws_cloudfront_distribution.read_only_viewer_distribution.arn
+  arn_to_protect                     = module.read_only_viewer_cloudfront.cloudfront_distribution_arn
   health_check_association_arn       = ""
-  distribution_id_to_protect         = aws_cloudfront_distribution.read_only_viewer_distribution.id
+  distribution_id_to_protect         = module.read_only_viewer_cloudfront.cloudfront_distribution_id
   resource_name                      = "cloudfront-read-only-viewer"
   alarm_notification_email           = []
   emergency_contacts                 = [{}]
@@ -12,5 +12,5 @@ module "shield_protection" {
   isShieldSRTAccessEnabled           = var.isShieldSRTAccessEnabled
   isShieldAutomaticResponseEnabled   = var.isShieldAutomaticResponseEnabled
 
-  depends_on = [aws_cloudfront_distribution.read_only_viewer_distribution]
+  depends_on = [module.read_only_viewer_cloudfront]
 }
