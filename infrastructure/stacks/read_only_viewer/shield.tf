9c04dcab5887a0fcc781d220c9de77ea8350dd3f
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-29 14:18:13 +0100

    FDOS-601 Modifcation for build failure

--- infrastructure/stacks/read_only_viewer/shield.tf
+++ infrastructure/stacks/read_only_viewer/shield.tf
@@ -2,12 +2,10 @@ module "shield_protection" {
   count  = local.is_primary_environment ? 1 : 0
   source = "../../modules/shield"
 
-  arns_to_protect = {
-    "cloudfront-read-only-viewer" = aws_cloudfront_distribution.read_only_viewer_distribution.arn
-  }
-  health_check_associations          = {}
-  distribution_ids_to_protect        = [aws_cloudfront_distribution.read_only_viewer_distribution.id]
-  resource_prefix                    = local.resource_prefix
+  arn_to_protect                     = aws_cloudfront_distribution.read_only_viewer_distribution.arn
+  health_check_association_arn       = ""
+  distribution_id_to_protect         = aws_cloudfront_distribution.read_only_viewer_distribution.id
+  resource_name                      = "cloudfront-read-only-viewer"
   alarm_notification_email           = []
   emergency_contacts                 = [{}]
   isShieldProactiveEngagementEnabled = var.isShieldProactiveEngagementEnabled
