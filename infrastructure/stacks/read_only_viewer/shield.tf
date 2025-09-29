a1cae37461c05957f778c3667a71a64de8fccd74
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-29 15:08:31 +0100

    FDOS-601 Variable value fix

--- infrastructure/stacks/read_only_viewer/shield.tf
+++ infrastructure/stacks/read_only_viewer/shield.tf
@@ -7,7 +7,7 @@ module "shield_protection" {
   distribution_id_to_protect         = module.read_only_viewer_cloudfront.cloudfront_distribution_id
   resource_name                      = "cloudfront-read-only-viewer"
   alarm_notification_email           = []
-  emergency_contacts                 = [{}]
+  emergency_contacts                 = []
   isShieldProactiveEngagementEnabled = var.isShieldProactiveEngagementEnabled
   isShieldSRTAccessEnabled           = var.isShieldSRTAccessEnabled
   isShieldAutomaticResponseEnabled   = var.isShieldAutomaticResponseEnabled
