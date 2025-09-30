91a28b652f0a5c5b8139c6506046f47a9852ff6b
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-09-30 12:59:36 +0100

    FDOS-601 Output the calculated health check id

--- infrastructure/stacks/read_only_viewer/outputs.tf
+++ infrastructure/stacks/read_only_viewer/outputs.tf
@@ -22,3 +22,8 @@ output "cloudfront_latency_health_check_id" {
   value       = aws_route53_health_check.cloudfront_latency.id
   description = "The ID of the Route53 health check monitoring CloudFront latency"
 }
+
+output "calculated_health_check_id" {
+  value       = aws_route53_health_check.calculated_health_check.id
+  description = "The ID of the calculated health check monitoring CloudFront. This will be associated with Shield Advanced"
+}
