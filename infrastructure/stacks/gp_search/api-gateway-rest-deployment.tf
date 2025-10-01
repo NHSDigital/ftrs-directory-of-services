a1b5c9a50fb4b1a676835977fe64b0640f8901a7
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-01 16:05:23 +0100

    DOSIS-2245 Fix checof errors

--- infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
@@ -6,4 +6,8 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = aws_api_gateway_rest_api.api-gateway.id
   #stage_name = "dev"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
