909227542e9355a371964bb04c07f39af0e343ed
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-02 14:13:18 +0100

    DOSIS-2245 Attach Lambda to REST gateway integration

--- infrastructure/stacks/gp_search/api-gateway-rest-resource-status.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-resource-status.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_method" "status" {
   rest_api_id   = aws_api_gateway_rest_api.api-gateway.id
   resource_id   = aws_api_gateway_resource.status.id
   http_method   = "GET"
-  authorization = "CUSTOM"
+  authorization = "NONE"
 
   request_validator_id = aws_api_gateway_request_validator.validator.id
 }
@@ -20,7 +20,8 @@ resource "aws_api_gateway_integration" "status" {
   resource_id             = aws_api_gateway_resource.status.id
   http_method             = aws_api_gateway_method.status.http_method
   integration_http_method = "GET"
-  type                    = "MOCK"
+  type                    = "AWS"
+  uri                     = module.health_check_lambda.lambda_function_invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "status" {
