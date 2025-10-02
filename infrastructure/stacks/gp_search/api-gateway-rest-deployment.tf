4d22696855f7f0a746f088d3ec544e8dab642443
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-02 17:04:48 +0100

    DOSIS-2245 Attach Lambda to REST gateway integration

--- infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
@@ -1,13 +1,12 @@
-# resource "aws_api_gateway_deployment" "deployment" {
-#   depends_on = [
-#     aws_api_gateway_integration.organization,
-#     aws_api_gateway_integration.status, # Add this line
-#   ]
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.organization,
+    aws_api_gateway_integration.status,
+  ]
 
-#   rest_api_id = aws_api_gateway_rest_api.api-gateway.id
-#   #stage_name = "dev"
+  rest_api_id = aws_api_gateway_rest_api.api-gateway.id
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
