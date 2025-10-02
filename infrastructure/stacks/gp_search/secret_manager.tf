974f72f328cfb88f0608c1bb6c20d24c1aeae7a6
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-02 17:24:46 +0100

    DOSIS-2245 Attach Lambda to REST gateway integration

--- infrastructure/stacks/gp_search/secret_manager.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_secretsmanager_secret" "api_ca_pk_secret" {
-  # checkov:skip=CKV2_AWS_57:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
-  # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
-  name        = "/${var.repo_name}/${var.environment}/test"
-  description = "Test Secret"
-}
-
-
-
