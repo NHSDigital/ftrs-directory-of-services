d60cd509cfa705ac0c6050ee964e0d64c2617546
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-02 14:13:18 +0100

    DOSIS-2245 Attach Lambda to REST gateway integration

--- infrastructure/stacks/gp_search/health_check_lambda.tf
+++ infrastructure/stacks/gp_search/health_check_lambda.tf
@@ -1,5 +1,5 @@
 module "health_check_lambda" {
-  source                 = "github.com/NHSDigital/ftrs-directory-of-services?ref=57b53f778381f3c4cfaded5770a5eddd3ff8f6f5/infrastructure/modules/lambda"
+  source                 = "github.com/NHSDigital/ftrs-directory-of-services?ref=f5cc045/infrastructure/modules/lambda"
   function_name          = "${local.resource_prefix}-${var.health_check_lambda_name}"
   description            = "This lambda provides a health check for the search lambda"
   handler                = "health_check/health_check_function.lambda_handler"
