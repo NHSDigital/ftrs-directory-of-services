41cbe4102219d34a405b9d1b933daf9f459e2b33
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-02 14:32:22 +0100

    DOSIS-2245 Attach Lambda to REST gateway integration

--- infrastructure/stacks/gp_search/api-gateway-rest-resource-status.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-resource-status.tf
@@ -7,39 +7,39 @@ resource "aws_api_gateway_resource" "status" {
 # Method request / response and integration request / response
 
 # checkov:skip=CKV_AWS_59: False positive as all endpoints are protected by mTLS
-resource "aws_api_gateway_method" "status" {
-  rest_api_id   = aws_api_gateway_rest_api.api-gateway.id
-  resource_id   = aws_api_gateway_resource.status.id
-  http_method   = "GET"
-  authorization = "NONE"
+# resource "aws_api_gateway_method" "status" {
+#   rest_api_id   = aws_api_gateway_rest_api.api-gateway.id
+#   resource_id   = aws_api_gateway_resource.status.id
+#   http_method   = "GET"
+#   authorization = "NONE"
 
-  request_validator_id = aws_api_gateway_request_validator.validator.id
-}
+#   request_validator_id = aws_api_gateway_request_validator.validator.id
+# }
 
-resource "aws_api_gateway_integration" "status" {
-  rest_api_id             = aws_api_gateway_rest_api.api-gateway.id
-  resource_id             = aws_api_gateway_resource.status.id
-  http_method             = aws_api_gateway_method.status.http_method
-  integration_http_method = "GET"
-  type                    = "AWS"
-  uri                     = module.health_check_lambda.lambda_function_invoke_arn
-}
+# resource "aws_api_gateway_integration" "status" {
+#   rest_api_id             = aws_api_gateway_rest_api.api-gateway.id
+#   resource_id             = aws_api_gateway_resource.status.id
+#   http_method             = aws_api_gateway_method.status.http_method
+#   integration_http_method = "GET"
+#   type                    = "AWS"
+#   uri                     = module.health_check_lambda.lambda_function_invoke_arn
+# }
 
-resource "aws_api_gateway_method_response" "status" {
-  rest_api_id = aws_api_gateway_rest_api.api-gateway.id
-  resource_id = aws_api_gateway_resource.status.id
-  http_method = aws_api_gateway_method.status.http_method
-  status_code = "200"
-}
+# resource "aws_api_gateway_method_response" "status" {
+#   rest_api_id = aws_api_gateway_rest_api.api-gateway.id
+#   resource_id = aws_api_gateway_resource.status.id
+#   http_method = aws_api_gateway_method.status.http_method
+#   status_code = "200"
+# }
 
-resource "aws_api_gateway_integration_response" "status" {
-  rest_api_id = aws_api_gateway_rest_api.api-gateway.id
-  resource_id = aws_api_gateway_resource.status.id
-  http_method = aws_api_gateway_method.status.http_method
-  status_code = aws_api_gateway_method_response.status.status_code
+# resource "aws_api_gateway_integration_response" "status" {
+#   rest_api_id = aws_api_gateway_rest_api.api-gateway.id
+#   resource_id = aws_api_gateway_resource.status.id
+#   http_method = aws_api_gateway_method.status.http_method
+#   status_code = aws_api_gateway_method_response.status.status_code
 
-  depends_on = [
-    aws_api_gateway_method.status,
-    aws_api_gateway_integration.status
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_method.status,
+#     aws_api_gateway_integration.status
+#   ]
+# }
