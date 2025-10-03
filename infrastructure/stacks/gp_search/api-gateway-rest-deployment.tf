dab2557138235625a544e15212f5f9535de79872
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-03 16:08:27 +0100

    DOSIS-2245 Enhance rest gateway

--- infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
@@ -6,7 +6,40 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = aws_api_gateway_rest_api.api-gateway.id
 
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.organization.id,
+      aws_api_gateway_resource.status.id,
+      aws_api_gateway_method.organization.id,
+      aws_api_gateway_method.status.id,
+      aws_api_gateway_integration.organization.id,
+      aws_api_gateway_integration.status.id,
+    ]))
+  }
+
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_method_settings" "all" {
+  rest_api_id = aws_api_gateway_rest_api.api-gateway.id
+  stage_name  = aws_api_gateway_stage.default.stage_name
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled    = true
+    logging_level      = "OFF"
+    data_trace_enabled = true
+
+    throttling_burst_limit = -1
+    throttling_rate_limit  = -1
   }
 }
