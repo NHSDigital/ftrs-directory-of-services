6ccea36e50fcef549541e14ffc9bee7b058d29fc
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-10-03 10:56:20 +0100

    FDOS-601 Merging the two shield creation into one

--- infrastructure/stacks/domain_name/shield.tf
+++ infrastructure/stacks/domain_name/shield.tf
@@ -1,14 +1,14 @@
-module "shield_protection_mgmt_root_domain" {
-  count  = var.environment == "mgmt" && local.is_primary_environment ? 1 : 0
+module "shield_protection_domain" {
   source = "../../modules/shield"
 
   providers = {
     aws = aws.us-east-1
   }
 
-  arn_to_protect                     = aws_route53_zone.root_zone[0].arn
+  arn_to_protect = var.environment == "mgmt" ? aws_route53_zone.root_zone[0].arn : aws_route53_zone.environment_zone[0].arn
+
   health_check_association_arn       = ""
-  resource_name                      = "route53-mgmt-root-domain"
+  resource_name                      = var.environment == "mgmt" ? "route53-mgmt-root-domain" : "route53-env-domain"
   resource_prefix                    = local.resource_prefix
   alarm_notification_email           = var.alarm_notification_email
   emergency_contacts                 = var.emergency_contacts
@@ -16,26 +16,5 @@ module "shield_protection_mgmt_root_domain" {
   isShieldSRTAccessEnabled           = var.isShieldSRTAccessEnabled
   isShieldAutomaticResponseEnabled   = var.isShieldAutomaticResponseEnabled
 
-  depends_on = [aws_route53_zone.root_zone]
-}
-
-module "shield_protection_env_domain" {
-  count  = var.environment == "mgmt" && local.is_primary_environment ? 0 : 1
-  source = "../../modules/shield"
-
-  providers = {
-    aws = aws.us-east-1
-  }
-
-  arn_to_protect                     = aws_route53_zone.environment_zone[0].arn
-  health_check_association_arn       = ""
-  resource_name                      = "route53-env-domain"
-  resource_prefix                    = local.resource_prefix
-  alarm_notification_email           = var.alarm_notification_email
-  emergency_contacts                 = var.emergency_contacts
-  isShieldProactiveEngagementEnabled = var.isShieldProactiveEngagementEnabled
-  isShieldSRTAccessEnabled           = var.isShieldSRTAccessEnabled
-  isShieldAutomaticResponseEnabled   = var.isShieldAutomaticResponseEnabled
-
-  depends_on = [aws_route53_zone.environment_zone]
+  depends_on = var.environment == "mgmt" ? [aws_route53_zone.root_zone] : [aws_route53_zone.environment_zone]
 }
