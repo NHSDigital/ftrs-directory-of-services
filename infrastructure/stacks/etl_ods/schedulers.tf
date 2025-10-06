c8ceee2e6b0c0abe55c5052ebeb86fa23bb63846
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2025-10-06 13:35:44 +0100

    SIA-501 Try precoding greater than and less than characters in json payload

--- infrastructure/stacks/etl_ods/schedulers.tf
+++ infrastructure/stacks/etl_ods/schedulers.tf
@@ -21,7 +21,7 @@ resource "aws_scheduler_schedule" "ods_etl_schedule" {
     role_arn = aws_iam_role.ods_etl_scheduler_invoke_role.arn
 
     input = jsonencode({
-      "trigger-time" = "<aws.scheduler.scheduled-time>"
+      "trigger-time" = "\u003caws.scheduler.scheduled-time\u003e"
     })
 
     retry_policy {
