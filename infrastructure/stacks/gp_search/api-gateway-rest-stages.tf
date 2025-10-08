b166adc480514c359e857dd5a3f18286bc6b9009
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-08 17:08:19 +0100

    DOSIS-2245 Turn general logging off because we are using custom logging

--- infrastructure/stacks/gp_search/api-gateway-rest-stages.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-stages.tf
@@ -1,6 +1,7 @@
 resource "aws_api_gateway_stage" "default" {
   # checkov:skip=CKV2_AWS_29: DOSIS-2197 - Deploy and attach WAF
   # checkov:skip=CKV2_AWS_51: False positive, the API is secured by mTLS via DNS domain certificate
+  # checkov:skip=CKV2_AWS_4: False positive, we are configuring custom logging
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api-gateway.id
   stage_name    = "default"
