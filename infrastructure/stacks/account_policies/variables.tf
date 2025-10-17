a3c8ca2627b27c66336b3edd30c95f95a8e93662
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-10-17 09:58:51 +0100

    fix(wafr): FDOS-632 Address review comments from PR

--- infrastructure/stacks/account_policies/variables.tf
+++ infrastructure/stacks/account_policies/variables.tf
@@ -2,3 +2,9 @@ variable "athena_output_bucket_name" {
   description = "The name of the S3 bucket for Athena query results"
   type        = string
 }
+
+variable "steampipe_role_name" {
+  description = "The name of the Steampipe read-only IAM role"
+  type        = string
+  default     = "steampipe-readonly-role"
+}
