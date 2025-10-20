30ce67c393a879f0ad0aaff1b281dc22def940cd
jack-tsien-nhse <237961220+jack-tsien-nhse@users.noreply.github.com>
2025-10-20 15:48:18 +0100

    refactor: DOSIS-2419 Added new secret resources with standardised name/path formatting

--- infrastructure/stacks/account_wide/secrets.tf
+++ infrastructure/stacks/account_wide/secrets.tf
@@ -13,3 +13,19 @@ resource "aws_secretsmanager_secret" "api_ca_pk_secret" {
   name        = "/${var.repo_name}/${var.environment}/api-ca-pk"
   description = "Private key for mTLS authentication"
 }
+
+resource "aws_secretsmanager_secret" "new_api_ca_cert_secret" {
+  # checkov:skip=CKV2_AWS_57:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  count       = local.is_primary_environment ? 1 : 0
+  name        = "/${var.project}/${var.environment}/api-ca-cert"
+  description = "Public certificate for mTLS authentication"
+}
+
+resource "aws_secretsmanager_secret" "new_api_ca_pk_secret" {
+  # checkov:skip=CKV2_AWS_57:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  # checkov:skip=CKV_AWS_149:TODO - https://nhsd-jira.digital.nhs.uk/browse/FDOS-405
+  count       = local.is_primary_environment ? 1 : 0
+  name        = "/${var.project}/${var.environment}/api-ca-pk"
+  description = "Private key for mTLS authentication"
+}
