88ba53768439a5c247953d43fb06b25a8d311670
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-10-22 09:32:39 +0100

    fix: FTRS-316 Update AWS Security Hub standards ARN to use correct variable for region

--- infrastructure/stacks/account_security/security_hub.tf
+++ infrastructure/stacks/account_security/security_hub.tf
@@ -1,7 +1,7 @@
 resource "aws_securityhub_standards_subscription" "cis_aws_foundations_benchmark_1_4_0" {
-  standards_arn = "arn:aws:securityhub:${var.region}::standards/cis-aws-foundations-benchmark/v/1.4.0"
+  standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/cis-aws-foundations-benchmark/v/1.4.0"
 }
 
 resource "aws_securityhub_standards_subscription" "cis_aws_foundations_benchmark_3_0_0" {
-  standards_arn = "arn:aws:securityhub:${var.region}::standards/cis-aws-foundations-benchmark/v/3.0.0"
+  standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/cis-aws-foundations-benchmark/v/3.0.0"
 }
