4909d5148355e81602e76cf8e91126f9e7d4f38a
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2025-10-23 18:29:31 +0100

    feat(jmeter): DOSIS-2259 Extracting groovy code to separate file after review comments

---
+++ tests/performance/scripts/LoadParams.groovy
@@ -0,0 +1,58 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+def csvPath = "parameter_files/plan_params.csv"
+def file = new File(csvPath)
+
+if (file.exists()) {
+
+    def lines = file.readLines()
+
+    // For each line, split by comma and set variables
+    lines.eachWithIndex { line, idx ->
+        def values = line.split(",")
+        log.info("here1")
+
+        // Set each value as a variable, e.g. param_0_0, param_0_1, ...
+        values.eachWithIndex { val, col ->
+            vars.put("param_${idx}_${col}", val)
+        }
+    }
+} else {
+    log.warn("Plan_params file not found: " + csvPath)
+    }
+
+
+def pp_endpoint = vars.get("param_0_0")
+def pp_apim_env = vars.get("param_0_1")
+def pp_apikey = vars.get("param_0_2")
+def pp_kid = vars.get("param_0_3")
+
+def serviceendpoint = props.get("serviceendpoint")
+if (serviceendpoint == null || serviceendpoint == "") {
+    serviceendpoint = pp_endpoint
+}
+vars.put("ServiceEndpoint", serviceendpoint)
+
+
+def kid = props.get("kid")
+if (kid == null || kid == "") {
+  kid = pp_kid
+}
+vars.put("Kid", kid)
+
+
+def Apim_Env = props.get("apim_env")
+if (Apim_Env == null || Apim_Env == "") {
+    Apim_Env = pp_apim_env
+}
+vars.put("Apim_Env", Apim_Env)
+
+def apikey = props.get("apikey")
+if (apikey == null || apikey == "") {
+    apikey = pp_apikey
+}
+vars.put("ApiKey", apikey)
+
+
+
