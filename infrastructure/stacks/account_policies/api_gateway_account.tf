7a9b399f619cf128bb4422a96e6136ea3a0aa1a0
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-23 16:50:03 +0100

    fix(api): DOSIS-22453 DOSIS-2245 Temp removal of API Gateway account

--- infrastructure/stacks/account_policies/api_gateway_account.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_api_gateway_account" "api_gateway_account" {
-  cloudwatch_role_arn = aws_iam_role.cloudwatch_api_gateway_role.arn
-
-  depends_on = [
-    aws_iam_role_policy.api_gateway_cloudwatch_policy
-  ]
-}
