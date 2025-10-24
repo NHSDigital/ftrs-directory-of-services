23ea84f4d6dafb28d9b2b3ba115fac53f879cb2a
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-24 16:34:53 +0100

    feat(api): DOSIS-2245 Create REST API Gateway

--- infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-deployment.tf
@@ -28,7 +28,7 @@ resource "aws_api_gateway_deployment" "deployment" {
     create_before_destroy = true
   }
 }
-# checkov:skip=CKV_AWS_120: Caching breaks the tests
+# checkov:skip=CKV_AWS_225: Caching breaks the tests
 resource "aws_api_gateway_method_settings" "all" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   stage_name  = aws_api_gateway_stage.default.stage_name
