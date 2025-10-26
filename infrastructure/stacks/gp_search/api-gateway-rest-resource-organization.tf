da83de7800c18d5a497b56865668a4d46d13154d
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-26 11:04:56 +0000

    feat(2245): DOSIS-2245 Fix gateway deployment trigger

--- infrastructure/stacks/gp_search/api-gateway-rest-resource-organization.tf
+++ infrastructure/stacks/gp_search/api-gateway-rest-resource-organization.tf
@@ -1,7 +1,7 @@
 resource "aws_api_gateway_resource" "organization" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "Org"
+  path_part   = "Organization"
 }
 
 # Method request / response and integration request / response
