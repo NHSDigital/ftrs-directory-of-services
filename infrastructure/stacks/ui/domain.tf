7d121ecd6c4dc82294fc257a781eea06d5d2f985
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-10-27 13:49:51 +0000

    feat: FTRS-261 Add IPv6 Route53 records for read-only and UI stacks

--- infrastructure/stacks/ui/domain.tf
+++ infrastructure/stacks/ui/domain.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "ui_record" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "ui_record_ipv6" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${var.stack_name}${local.workspace_suffix}.${local.env_domain_name}"
+  type    = "AAAA"
+
+  alias {
+    name                   = module.ui_cloudfront.cloudfront_distribution_domain_name
+    zone_id                = module.ui_cloudfront.cloudfront_distribution_hosted_zone_id
+    evaluate_target_health = false
+  }
+}
