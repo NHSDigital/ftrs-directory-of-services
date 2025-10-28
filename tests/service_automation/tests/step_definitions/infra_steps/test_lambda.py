f343d7a9143cf9ceb262b7475147aa15f9f248e8
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2025-10-28 15:03:31 +0000

    feat(api): DOSIS-2245 switch to rest gateway (#495)
    * feat(api): DOSIS-2245 Initial set of tf files for API Gateway Rest
    
    feat(api): DOSIS-2245 Fix checof errors
    
    feat(api): DOSIS-2245 Fix checof errors
    
    feat(api): DOSIS-2245 Expose lambda invoke arn for REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Attach Lambda to REST gateway integration
    
    feat(api): DOSIS-2245 Align lamdba permissions with REST gateway
    
    feat(api): DOSIS-2245 Enhance rest gateway
    
    feat(api): DOSIS-2245 Added cloudwatch log group
    
    feat(api): DOSIS-2245 Fix checkov issues
    
    feat(api): DOSIS-2245 Turn general logging off because we are using custom logging
    
    feat(api): DOSIS-2245 Turn general logging off because we are using custom logging
    
    feat(api): DOSIS-2245 Turn general logging off because we are using custom logging
    
    feat(api): DOSIS-2245 Tidy up code
    
    feat(api): DOSIS-2245 Tidy up code
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(api): DOSIS-2245 Create REST API Gateway
    
    * feat(2245): DOSIS-2245 Fix gateway deployment trigger
    
    * feat(2245): DOSIS-2245 Fix gateway deployment trigger
    
    * feat(2245): DOSIS-2245 Fix gateway deployment trigger
    
    * feat(2245): DOSIS-2245 Tidy code
    
    * feat(2245): DOSIS-2245 Fix Lambda module reference
    
    * feat(api): DOSIS-2245 Fix Lambda module lock version
    
    * feat(api): DOSIS-2245 Remove unrequired variables

--- tests/service_automation/tests/step_definitions/infra_steps/test_lambda.py
+++ tests/service_automation/tests/step_definitions/infra_steps/test_lambda.py
@@ -80,18 +80,14 @@ def count_resources(lambda_response, resource_type):
 
 def create_lambda_params(odscode):
     return {
-        "version": "2.0",
-        "rawPath": "/Organization",
+        "path": "/Organization",
+        "httpMethod": "GET",
         "queryStringParameters": {
             "identifier": f"odsOrganisationCode|{odscode}",
             "_revinclude": "Endpoint:organization",
         },
         "requestContext": {
             "requestId": "796bdcd6-c5b0-4862-af98-9d2b1b853703",
-            "stage": "$default",
-            "http": {
-                "method": "GET",
-            },
         },
         "body": None,
     }
