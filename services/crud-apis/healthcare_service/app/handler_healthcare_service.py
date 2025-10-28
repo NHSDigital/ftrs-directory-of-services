a594f8371b7901850326a1e1e3d33d2d045adb65
Thomas Cordall <thomas.cordall@kainos.com>
2025-10-28 13:10:46 +0000

    feat(common): SIA-846 unit tests for requestId and adding requestId mapping to handlers

--- services/crud-apis/healthcare_service/app/handler_healthcare_service.py
+++ services/crud-apis/healthcare_service/app/handler_healthcare_service.py
@@ -1,7 +1,9 @@
+from aws_lambda_powertools.utilities.typing import LambdaContext
 from fastapi import FastAPI
 from ftrs_common.api_middleware.correlation_id_middleware import CorrelationIdMiddleware
 from ftrs_common.api_middleware.request_id_middleware import RequestIdMiddleware
 from ftrs_common.logger import Logger
+from ftrs_common.utils.request_id import fetch_or_set_request_id
 from mangum import Mangum
 
 from healthcare_service.app.router import healthcare
@@ -13,4 +15,12 @@ app.add_middleware(CorrelationIdMiddleware)
 app.add_middleware(RequestIdMiddleware)
 app.include_router(healthcare.router)
 
-handler = Mangum(app, lifespan="off")
+
+def handler(event: dict, context: LambdaContext) -> dict:
+    fetch_or_set_request_id(
+        context_id=getattr(context, "aws_request_id", None) if context else None,
+        header_id=event.get("headers", {}).get("X-Request-ID"),
+    )
+
+    mangum_handler = Mangum(app, lifespan="off")
+    return mangum_handler(event, context)
