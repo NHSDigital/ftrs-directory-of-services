dcc8ec938405f3d00b3f31b924ce8f3e590a0e3f
Thomas Cordall <thomas.cordall@kainos.com>
2025-10-30 14:12:23 +0000

    feat(common): SIA-846 removed passing requestId in utilities

--- services/etl-ods/pipeline/utilities.py
+++ services/etl-ods/pipeline/utilities.py
@@ -12,7 +12,7 @@ from ftrs_common.utils.correlation_id import (
     fetch_or_set_correlation_id,
     get_correlation_id,
 )
-from ftrs_common.utils.request_id import REQUEST_ID_HEADER, generate_request_id
+from ftrs_common.utils.request_id import REQUEST_ID_HEADER
 from ftrs_data_layer.logbase import OdsETLPipelineLogBase
 
 ods_utils_logger = Logger.get(service="ods_utils")
