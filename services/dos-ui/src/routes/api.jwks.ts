ff98808ccb8c68af77a02938859a2103c1c11992
nhs-shruthi-gowda <shruthi.gowda1@nhs.net>
2025-11-06 13:13:16 +0000

    feat(dos-ui): FTRS-269  Setup public key JWKS endpoint (#521)
    * feat(dos-ui): FTRS-271 added the code getting authorisation url
    
    * feat(dos-ui): FTRS-271 added the logger tool.
    
    * feat(dos-ui): FTRS-271 parameterised the aws util linted all the files
    
    * feat(dos-ui): FTRS-271 renamed files to match
    
    * feat(dos-ui): FTRS-271 added the permission needed for the ui lambda
    
    * fix: FTRS-271 Minor modifications to reuse values available from common vars or locals
    
    * feat(dos-ui): FTRS-269 added server router
    
    * feat(dos-ui): FTRS-269 added the public key api
    
    * feat(dos-ui): FTRS-269 fixed lint erros
    
    * feat(dos-ui): FTRS-269 review comments fixed
    
    * feat(dos-ui): FTRS-269 removed the files which are added while rebasing
    
    ---------
    
    Co-authored-by: ri-nhs <198660903+ri-nhs@users.noreply.github.com>

---
+++ services/dos-ui/src/routes/api.jwks.ts
@@ -0,0 +1,20 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { getCIS2PublicKey } from "@/utils/api-jwksUtil.ts";
+
+export const Route = createFileRoute("/api/jwks")({
+  server: {
+    handlers: {
+      GET: async () => {
+        try {
+          const cis2PublicKey = await getCIS2PublicKey();
+          return new Response(cis2PublicKey, { "headers": { "Content-Type": "application/json" }});
+        } catch (error) {
+          return new Response(
+            JSON.stringify({ message: "Error retrieving JWKS" }),
+            { status: 500 },
+          );
+        }
+      },
+    },
+  },
+});
