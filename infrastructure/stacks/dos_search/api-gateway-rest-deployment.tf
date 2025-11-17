5f01f9b4437d7147403f84910076309d81722c5f
lukeb-nhs <luke.biddell1@nhs.net>
2025-11-17 17:00:13 +0000

    feat(api-gateway): FTRS-727 Change redeployment triggers from local and var to the resource itself

--- infrastructure/stacks/dos_search/api-gateway-rest-deployment.tf
+++ infrastructure/stacks/dos_search/api-gateway-rest-deployment.tf
@@ -22,13 +22,7 @@ resource "aws_api_gateway_deployment" "deployment" {
       aws_api_gateway_method.status,
       aws_api_gateway_integration.organization,
       aws_api_gateway_integration.status,
-      [for k in sort(keys(local.gateway_responses)) : {
-        key           = k
-        response_type = local.gateway_responses[k].response_type
-        status_code   = local.gateway_responses[k].status_code
-        template      = local.gateway_responses[k].template
-      }],
-      var.fhir_content_type_header,
+      aws_api_gateway_gateway_response.default_gateway_response,
     ]))
   }
 
