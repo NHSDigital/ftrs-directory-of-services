f2ecb4fd2ef116de42a646a3e5625d82318d0a8d
jack-tsien-nhse <237961220+jack-tsien-nhse@users.noreply.github.com>
2025-11-17 17:14:07 +0000

    chore: FTRS-893 Merge logging branch

--- services/dos-search/functions/ftrs_service/ftrs_service.py
+++ services/dos-search/functions/ftrs_service/ftrs_service.py
@@ -1,11 +1,10 @@
-from aws_lambda_powertools import Logger
 from fhir.resources.R4B.bundle import Bundle
 from ftrs_common.utils.db_service import get_service_repository
 from ftrs_data_layer.domain import Organisation
 
+# Import instantiated logger from dos_search_ods_code_function to persist log context from beginning of Lambda execution
+from functions.ftrs_logger import ftrs_logger
 from functions.ftrs_service.fhir_mapper.bundle_mapper import BundleMapper
-
-logger = Logger()
 
 
 class FtrsService:
@@ -15,20 +14,19 @@ class FtrsService:
 
     def endpoints_by_ods(self, ods_code: str) -> Bundle:
         try:
-            logger.info("Retrieving organisation by ods_code")
+            ftrs_logger.info("Retrieving organisation by ods_code")
 
             organisation = self.repository.get_first_record_by_ods_code(ods_code)
 
-            logger.append_keys(
-                organization_id=organisation.id if organisation else "None"
+            ftrs_logger.info(
+                "Mapping organisation to fhir_bundle",
+                organization_id=organisation.id if organisation else "None",
             )
-
-            logger.info("Mapping organisation to fhir_bundle")
 
             fhir_bundle = self.mapper.map_to_fhir(organisation, ods_code)
 
         except Exception:
-            logger.exception("Error occurred while processing")
+            ftrs_logger.exception("Error occurred while processing")
             raise
 
         else:
