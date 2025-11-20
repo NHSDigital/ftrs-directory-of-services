15abbb8348db16e4dcbf44bd1c6f0219f4a60b48
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-11-20 12:58:20 +0000

    fix: FTRS-354 Associate correct environments

--- infrastructure/stacks/artefact_management/s3.tf
+++ infrastructure/stacks/artefact_management/s3.tf
@@ -42,10 +42,10 @@ data "aws_iam_policy_document" "artefacts_bucket_policy" {
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Infrastructure_e5f5de072b3e7cf8",
         "${data.aws_iam_role.app_github_runner_iam_role.arn}",
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-dev-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-test-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-int-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-ref-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-sandpit-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-test-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-int-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-ref-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-sandpit-${var.app_github_runner_role_name}",
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-prod-${var.app_github_runner_role_name}"
       ]
     }
