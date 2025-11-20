69bd6994029e86de4cfcd84047b4d0c86dcffc16
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-11-20 11:54:13 +0000

    fix: FTRS-354 Add S3 permission to artefact bucket to new variation of github runner roles

--- infrastructure/stacks/artefact_management/data.tf
+++ infrastructure/stacks/artefact_management/data.tf
@@ -6,6 +6,14 @@ data "aws_ssm_parameter" "aws_account_id_dev" {
   name = "/dos/aws_account_id_dev"
 }
 
+data "aws_ssm_parameter" "aws_account_id_int" {
+  name = "/dos/aws_account_id_int"
+}
+
+data "aws_ssm_parameter" "aws_account_id_ref" {
+  name = "/dos/aws_account_id_ref"
+}
+
 data "aws_ssm_parameter" "aws_account_id_test" {
   name = "/dos/aws_account_id_test"
 }
--- infrastructure/stacks/artefact_management/s3.tf
+++ infrastructure/stacks/artefact_management/s3.tf
@@ -18,9 +18,11 @@ data "aws_iam_policy_document" "artefacts_bucket_policy" {
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Developer_b0ffd523c3b8ddb9",
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Infrastructure_e5f5de072b3e7cf8",
         "${data.aws_iam_role.app_github_runner_iam_role.arn}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_int.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_ref.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}"
       ]
     }
     actions = [
@@ -38,9 +40,11 @@ data "aws_iam_policy_document" "artefacts_bucket_policy" {
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Developer_b0ffd523c3b8ddb9",
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-FtRS-RW-Infrastructure_e5f5de072b3e7cf8",
         "${data.aws_iam_role.app_github_runner_iam_role.arn}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-${var.app_github_runner_role_name}",
-        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-${var.app_github_runner_role_name}"
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_int.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_ref.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_prod.value}:role/${var.repo_name}-*-${var.app_github_runner_role_name}"
       ]
     }
     actions = [
