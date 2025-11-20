92dcceeb81797b0f9265f4e20244f131f5479806
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-11-20 12:24:06 +0000

    fix: FTRS-354 Remove the account ids for int and ref as they share the env

--- infrastructure/stacks/artefact_management/data.tf
+++ infrastructure/stacks/artefact_management/data.tf
@@ -6,14 +6,6 @@ data "aws_ssm_parameter" "aws_account_id_dev" {
   name = "/dos/aws_account_id_dev"
 }
 
-data "aws_ssm_parameter" "aws_account_id_int" {
-  name = "/dos/aws_account_id_int"
-}
-
-data "aws_ssm_parameter" "aws_account_id_ref" {
-  name = "/dos/aws_account_id_ref"
-}
-
 data "aws_ssm_parameter" "aws_account_id_test" {
   name = "/dos/aws_account_id_test"
 }
