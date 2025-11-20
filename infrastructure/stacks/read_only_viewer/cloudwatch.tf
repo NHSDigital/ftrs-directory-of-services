a9941de8dacd75608fc1fb4475fb6039d41ae493
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-11-20 10:57:55 +0000

    chore: FTRS-331 Use provider for us east region

--- infrastructure/stacks/read_only_viewer/cloudwatch.tf
+++ infrastructure/stacks/read_only_viewer/cloudwatch.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_log_group" "log_group" {
   # checkov:skip=CKV_AWS_338: TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-378
   name              = "${local.resource_prefix}-log-group${local.workspace_suffix}"
   retention_in_days = var.cloudfront_logs_retention_in_days
-  provider          = { aws = aws.us-east-1 }
+  provider          = aws.us-east-1
 }
 
 # Define a source
@@ -11,14 +11,14 @@ resource "aws_cloudwatch_log_delivery_source" "delivery_source" {
   name         = "${local.resource_prefix}-log-source${local.workspace_suffix}"
   log_type     = "ACCESS_LOGS"
   resource_arn = module.read_only_viewer_cloudfront.cloudfront_distribution_arn
-  provider     = { aws = aws.us-east-1 }
+  provider     = aws.us-east-1
 }
 
 # Define a destination - ie where the cloudfront logs go and format
 resource "aws_cloudwatch_log_delivery_destination" "delivery_destination" {
   name          = "${local.resource_prefix}-log-destination${local.workspace_suffix}"
   output_format = "json"
-  provider      = { aws = aws.us-east-1 }
+  provider      = aws.us-east-1
 
   delivery_destination_configuration {
     destination_resource_arn = aws_cloudwatch_log_group.log_group.arn
@@ -31,5 +31,5 @@ resource "aws_cloudwatch_log_delivery" "log_delivery" {
   delivery_destination_arn = aws_cloudwatch_log_delivery_destination.delivery_destination.arn
 
   record_fields = var.included_cloudfront_log_fields
-  provider      = { aws = aws.us-east-1 }
+  provider      = aws.us-east-1
 }
