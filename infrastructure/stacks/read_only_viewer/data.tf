bfe6b24a154629b395d721961cc3d723d185ae3d
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-11-20 15:11:23 +0000

    fix: FTRS-354 Get the correct role name

--- infrastructure/stacks/read_only_viewer/data.tf
+++ infrastructure/stacks/read_only_viewer/data.tf
@@ -23,7 +23,7 @@ data "aws_subnet" "private_subnets_details" {
 }
 
 data "aws_iam_role" "app_github_runner_iam_role" {
-  name = "${var.repo_name}-${var.app_github_runner_role_name}"
+  name = "${var.repo_name}-${var.environment}-${var.app_github_runner_role_name}"
 }
 
 data "aws_ssm_parameter" "dos_aws_account_id_mgmt" {
