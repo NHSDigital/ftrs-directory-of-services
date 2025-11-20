1423c2c58fcc42ff97fbf4ae54a05db7975b11d6
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2025-11-20 18:34:45 +0000

    feat(telecom): FTRS-1207 Update DoS Search for telecom changes

--- services/dos-search/functions/ftrs_service/fhir_mapper/organization_mapper.py
+++ services/dos-search/functions/ftrs_service/fhir_mapper/organization_mapper.py
@@ -1,30 +1,16 @@
 from fhir.resources.R4B.address import Address
-from fhir.resources.R4B.contactpoint import ContactPoint
 from fhir.resources.R4B.identifier import Identifier
 from fhir.resources.R4B.organization import Organization as FhirOrganization
+from ftrs_common.fhir.r4b.organisation_mapper import (
+    OrganizationMapper as CommonOrganizationMapper,
+)
 from ftrs_data_layer.domain import Organisation
 
 
 class OrganizationMapper:
     def map_to_fhir_organization(self, organisation: Organisation) -> FhirOrganization:
-        organization_id = str(organisation.id)
-        name = organisation.name
-        active = organisation.active
-        identifier = self._create_identifier(organisation)
-        telecom = self._create_telecom(organisation)
-        address = self._create_dummy_address()
-
-        fhir_organization = FhirOrganization.model_validate(
-            {
-                "id": organization_id,
-                "identifier": identifier,
-                "active": active,
-                "name": name,
-                "telecom": telecom,
-                "address": address,
-            }
-        )
-
+        fhir_organization = CommonOrganizationMapper().to_fhir(organisation)
+        fhir_organization.address = self._create_dummy_address()
         return fhir_organization
 
     def _create_identifier(self, organisation: Organisation) -> list[Identifier]:
@@ -37,21 +23,6 @@ class OrganizationMapper:
         )
 
         return [identifier]
-
-    def _create_telecom(self, organisation: Organisation) -> list[ContactPoint]:
-        telecom = organisation.telecom
-
-        if not telecom:
-            return []
-
-        telecom_entry = ContactPoint.model_validate(
-            {
-                "system": "phone",
-                "value": telecom,
-            }
-        )
-
-        return [telecom_entry]
 
     def _create_dummy_address(self) -> list[Address]:
         address = Address.model_validate(
