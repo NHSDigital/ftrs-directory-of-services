d14330d92e4821d7d1a5ff19f0b890c333f67d73
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2025-11-21 18:43:59 +0000

    fix(lambda): FTRS-1207 try removing pandas layer to prevent oversize issue

--- infrastructure/stacks/etl_ods/lambda.tf
+++ infrastructure/stacks/etl_ods/lambda.tf
@@ -41,8 +41,7 @@ module "processor_lambda" {
 
   layers = concat(
     [aws_lambda_layer_version.common_packages_layer.arn],
-    [aws_lambda_layer_version.python_dependency_layer.arn],
-    var.aws_lambda_layers
+    [aws_lambda_layer_version.python_dependency_layer.arn]
   )
 
   environment_variables = {
@@ -86,8 +85,7 @@ module "consumer_lambda" {
 
   layers = concat(
     [aws_lambda_layer_version.common_packages_layer.arn],
-    [aws_lambda_layer_version.python_dependency_layer.arn],
-    var.aws_lambda_layers
+    [aws_lambda_layer_version.python_dependency_layer.arn]
   )
 
   environment_variables = {
