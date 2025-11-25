1d948954fb9acd7b67ba5c975c8ea7dd63296d4d
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2025-11-25 11:07:11 +0000

    chore(telecom): FTRS-1207 Move comment to correct place

--- application/packages/python/ftrs_common/fhir/fhir_validator.py
+++ application/packages/python/ftrs_common/fhir/fhir_validator.py
@@ -56,11 +56,11 @@ class FhirValidator:
                     new_path = f"{path}.{k}" if path else k
                     stack.append((v, new_path))
             elif isinstance(current, list):
-                # Skipping telecom field validation as it may contain special characters
                 for idx, item in enumerate(current):
                     new_path = f"{path}[{idx}]"
                     stack.append((item, new_path))
             elif isinstance(current, str):
+                # Skipping telecom value field validation as it may contain special characters
                 if not (
                     path.startswith("telecom") and path.endswith("value")
                 ) and re.search(special_characters_pattern, current):
