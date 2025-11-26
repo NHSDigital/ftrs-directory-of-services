bf3546bdc1c59d0e79359d6e925496e7ddc2003f
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-11-26 12:39:20 +0000

    feat: FTRS-1178 Add state table

---
+++ infrastructure/stacks/data_migration/dynamodb.tf
@@ -0,0 +1,21 @@
+module "state_table" {
+  source = "../../modules/dynamodb"
+
+  table_name                     = "${local.resource_prefix}-state-table${local.workspace_suffix}"
+  hash_key                       = "source_record_id"
+  billing_mode                   = "PAY_PER_REQUEST"
+  point_in_time_recovery_enabled = true
+
+  attributes = [
+    {
+      name = "source_record_id"
+      type = "S"
+    },
+    {
+      name = "version"
+      type = "N"
+    }
+  ]
+
+  global_secondary_indexes = []
+}
