70b2df77d743739eb1c61eeca110d4e43fb85081
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2025-11-27 17:56:42 +0000

    fix: FTRS-354 To handle the different env behaviour

--- scripts/workflow/action-infra-stack.sh
+++ scripts/workflow/action-infra-stack.sh
@@ -33,9 +33,14 @@ export TF_VAR_stack_name
 export TF_VAR_mgmt_account_id="${MGMT_ACCOUNT_ID:-""}"
 
 # Override ENVIRONMENT to non-prod for account_policies or account_security stack
-if { [ "$STACK" = "account_policies" ] || [ "$STACK" = "account_security" ]; } && { [ "$ENVIRONMENT" = "ref" ] || [ "$ENVIRONMENT" = "dev" ] || [ "$ENVIRONMENT" = "test" ] || [ "$ENVIRONMENT" = "sandpit" ] || [ "$ENVIRONMENT" = "int" ] ; } then
+if { [ "$STACK" = "account_policies" ] || [ "$STACK" = "account_security" ]; } && { [ "$ENVIRONMENT" = "ref" ] || [ "$ENVIRONMENT" = "sandpit" ] || [ "$ENVIRONMENT" = "int" ] ; } then
   export ENVIRONMENT="non-prod"
   echo "Stack is $STACK - overriding ENVIRONMENT to non-prod"
+fi
+
+if { [ "$STACK" = "account_policies" ] || [ "$STACK" = "account_security" ]; } && { [ "$ENVIRONMENT" = "dev" ] || [ "$ENVIRONMENT" = "test" ] ; } then
+  export ENVIRONMENT="dev"
+  echo "Stack is $STACK - overriding ENVIRONMENT to dev"
 fi
 
 # needed for terraform management stack
