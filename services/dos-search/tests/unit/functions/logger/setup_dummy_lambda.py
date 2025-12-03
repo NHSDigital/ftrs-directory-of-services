effb26f0d395e83081043d733ad30257fc65d35e
jack-tsien-nhse <237961220+jack-tsien-nhse@users.noreply.github.com>
2025-12-03 15:50:38 +0000

    refactor: DOSIS-2163 FTRS-848 Address comments

--- services/dos-search/tests/unit/functions/logger/setup_dummy_lambda.py
+++ services/dos-search/tests/unit/functions/logger/setup_dummy_lambda.py
@@ -5,7 +5,7 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from functions.logger.dos_logger import DosLogger
 
 dos_logger = DosLogger.get(service="test_logger")
-logger = dos_logger._logger
+logger = dos_logger.logger
 
 
 @logger.inject_lambda_context(
@@ -18,7 +18,7 @@ def lambda_handler(
     function: Callable[[Any], Any],
     *,
     run_init: bool = True,
-) -> any:
+) -> Any:
     if run_init:
         dos_logger.init(event)
     return function()
