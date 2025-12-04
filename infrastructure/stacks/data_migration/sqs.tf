02ec70ec3eff0ed8292cdf5f3789f60eb576f0d6
timrickwood <48654585+timrickwood@users.noreply.github.com>
2025-12-04 11:54:11 +0000

    chore: FDOS-1588 Encrypt sqs for data migration and ods etl

--- infrastructure/stacks/data_migration/sqs.tf
+++ infrastructure/stacks/data_migration/sqs.tf
@@ -1,16 +1,18 @@
 resource "aws_sqs_queue" "dms_event_queue" {
   # checkov:skip=CKV_AWS_27: TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-545
   name                    = "${local.resource_prefix}-${var.dms_event_queue_name}${local.workspace_suffix}"
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled = var.sqs_managed_sse_enabled
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dms_event_queue_dlq.arn
     maxReceiveCount     = 5
   })
+  kms_master_key_id = data.aws_kms_key.sqs_kms_alias.arn
 }
 
 resource "aws_sqs_queue" "dms_event_queue_dlq" {
   name                    = "${local.resource_prefix}-${var.dms_event_queue_name}-dlq${local.workspace_suffix}"
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled = var.sqs_managed_sse_enabled
+  kms_master_key_id       = data.aws_kms_key.sqs_kms_alias.arn
 }
 
 resource "aws_sqs_queue_policy" "dms_event_queue_policy" {
@@ -35,7 +37,8 @@ resource "aws_sqs_queue" "eventbridge_event_full_migration_completion_dlq" {
   count = local.is_primary_environment ? 1 : 0
 
   name                    = "${local.resource_prefix}-${var.full_migration_completion_event_queue_name}-dlq"
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled = var.sqs_managed_sse_enabled
+  kms_master_key_id       = data.aws_kms_key.sqs_kms_alias.arn
 }
 
 resource "aws_sqs_queue_policy" "eventbridge_event_full_migration_completion_dlq_policy" {
