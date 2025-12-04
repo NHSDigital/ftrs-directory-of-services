82551128e57d0a5358f8cec3fdf2836f32f49f56
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2025-12-04 10:59:40 +0000

    fix(data-migration): FTRS-1720 Rewrite SQL lambda triggers (#599)
    * fix(data-migration): FTRS-1720 Rewrite SQL lambda triggers
    
    * feat(dms-lambda): FTRS-1702 fixed the unit tests and removed to post to workspace queues
    
    * feat(dms-lambda): FTRS-1702 fixed the unit tests and removed to post to workspace queues
    
    ---------
    
    Co-authored-by: nhs-shruthi-gowda <shruthi.gowda1@nhs.net>

--- services/data-migration/src/record_change_trigger/lambda_handler.py
+++ services/data-migration/src/record_change_trigger/lambda_handler.py
@@ -2,26 +2,24 @@ import json
 import os
 
 import boto3
-from aws_lambda_powertools.utilities.parameters import get_parameters
+from aws_lambda_powertools.utilities.parameters import get_parameter
 from ftrs_common.logger import Logger
 
 LOGGER = Logger.get(service="migration-copy-db-trigger")
 SQS_CLIENT = boto3.client("sqs")
 
 
-def get_dms_workspaces() -> list[str]:
+def get_dms_workspaces() -> str:
     ssm_path = os.environ.get("SQS_SSM_PATH")
     if not ssm_path:
         raise ValueError("Missing required environment variable: SQS_SSM_PATH")
 
-    params = get_parameters(
+    workspace = get_parameter(
         ssm_path,
-        recursive=True,
         decrypt=True,
         max_age=300,  # cache TTL = 300 seconds (5 minutes)
     )
-    workspaces = list(params.values())
-    return workspaces
+    return [workspace]
 
 
 def lambda_handler(event: dict, context: dict) -> None:
