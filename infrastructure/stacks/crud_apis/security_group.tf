2acae5338de5fd35bf3a951bc6cb1224d9044b1d
mmg-nhse <michaela.mcgrath1@nhs.net>
2025-12-10 16:10:24 +0000

    fix: FTRS-1713 Enable data migration deployment only on main and update Trivy ignore comments

--- infrastructure/stacks/crud_apis/security_group.tf
+++ infrastructure/stacks/crud_apis/security_group.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_organi
   to_port           = var.https_port
 }
 
-# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-511
+# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
 resource "aws_vpc_security_group_egress_rule" "organisation_api_allow_443" {
   security_group_id = aws_security_group.organisation_api_lambda_security_group.id
   description       = "Organisation api egress rule to allow 443"
@@ -42,7 +42,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_health
   from_port         = var.https_port
 }
 
-# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-511
+# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
 resource "aws_vpc_security_group_egress_rule" "healthcare_service_api_allow_443" {
 
   security_group_id = aws_security_group.healthcare_service_api_lambda_security_group.id
@@ -70,7 +70,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_locati
   from_port         = var.https_port
 }
 
-# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FDOS-511
+# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
 resource "aws_vpc_security_group_egress_rule" "location_api_allow_443" {
   security_group_id = aws_security_group.location_api_lambda_security_group.id
   description       = "Location api egress rule to allow 443"
