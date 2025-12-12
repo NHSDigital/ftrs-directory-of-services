95d00fbdf77c89e8f44b9ee826762b3459106cfb
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2025-12-12 10:43:09 +0000

    chore(telecom): FTRS-1207 Allow null and str telecom for Data Migration

--- application/packages/python/ftrs_data_layer/domain/organisation.py
+++ application/packages/python/ftrs_data_layer/domain/organisation.py
@@ -17,12 +17,12 @@ class Organisation(DBModel):
     identifier_ODS_ODSCode: str
     active: bool
     name: str
-    telecom: str | None = None
     type: OrganisationType | str | None = None
     primary_role_code: OrganisationTypeCode | str | None = None
     non_primary_role_codes: list[OrganisationTypeCode | str] = Field(
         default_factory=list
     )
-    telecom: list[Telecom]
+    telecom: list[Telecom] | str | None
+    type: OrganisationType | str
     endpoints: list["Endpoint"] = Field(default_factory=list)
     legalDates: LegalDates | None = None
