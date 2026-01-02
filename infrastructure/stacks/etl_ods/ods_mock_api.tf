b25ad2ac31ef632ebca14e79c533f504e88856c1
AITR1 <178915313+AITR1@users.noreply.github.com>
2026-01-02 16:26:52 +0000

    feat(terraform): FTRS-1376 Enablde deployment of mock on test env

--- infrastructure/stacks/etl_ods/ods_mock_api.tf
+++ infrastructure/stacks/etl_ods/ods_mock_api.tf
@@ -1,10 +1,10 @@
 module "ods_mock_api" {
-  count = var.environment == "dev" ? 1 : 0
+  count = contains(["dev", "test"], var.environment) ? 1 : 0
 
   source = "../../modules/ods-mock-api"
 
   api_gateway_name        = "${local.resource_prefix}-ods-mock-api${local.workspace_suffix}"
-  api_gateway_description = "Mock ODS Terminology API for ETL testing in dev environment"
+  api_gateway_description = "Mock ODS Terminology API for ETL testing in dev and test environments"
   environment             = var.environment
 
   throttle_burst_limit = 100
@@ -13,7 +13,7 @@ module "ods_mock_api" {
 }
 
 resource "aws_ssm_parameter" "ods_mock_api_url" {
-  count = var.environment == "dev" ? 1 : 0
+  count = contains(["dev", "test"], var.environment) ? 1 : 0
 
   name        = "/${local.project_prefix}/mock-api/endpoint-url${local.workspace_suffix}"
   description = "ODS Mock API endpoint URL for ETL testing"
@@ -23,14 +23,14 @@ resource "aws_ssm_parameter" "ods_mock_api_url" {
 
 resource "aws_secretsmanager_secret" "ods_mock_api_key" {
   # checkov:skip=CKV2_AWS_57: Mock API key doesn't require automatic rotation, manual rotation is sufficient for development testing
-  count = var.environment == "dev" ? 1 : 0
+  count = contains(["dev", "test"], var.environment) ? 1 : 0
 
   name        = "/${local.project_prefix}/mock-api/api-key${local.workspace_suffix}"
-  description = "API key for ODS Mock API in dev environment"
+  description = "API key for ODS Mock API in dev and test environments"
 }
 
 resource "aws_secretsmanager_secret_version" "ods_mock_api_key" {
-  count = var.environment == "dev" ? 1 : 0
+  count = contains(["dev", "test"], var.environment) ? 1 : 0
 
   secret_id     = aws_secretsmanager_secret.ods_mock_api_key[0].id
   secret_string = module.ods_mock_api[0].api_key_value
