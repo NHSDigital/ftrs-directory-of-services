181a46f169c11ba16b3186f59efd1e8f127edc13
timrickwood <48654585+timrickwood@users.noreply.github.com>
2026-01-05 15:48:12 +0000

    chore(data_migration): FTRS-1704 Limit data mig ssm to default workspace

--- infrastructure/stacks/data_migration/ssm.tf
+++ infrastructure/stacks/data_migration/ssm.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "rds_event_listener_workspace_sqs_id" {
   # checkov:skip=CKV2_AWS_34: Needs to be encrypted
+  count = local.is_primary_environment ? 1 : 0
   name  = "${var.sqs_ssm_path_for_ids}${var.environment}/${local.resource_prefix}-sqs-param${local.workspace_suffix}"
   type  = "String"
   value = aws_sqs_queue.dms_event_queue.id
