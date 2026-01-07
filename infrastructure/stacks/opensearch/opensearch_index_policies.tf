a0f018c57ce8fdc819a65b6b6e0224677ba98de6
Gurinder Brar <gurinder.brar1@nhs.net>
2026-01-07 12:23:16 +0000

    feat(opensearch): FTRS-856 Index creation and population

--- infrastructure/stacks/opensearch/opensearch_index_policies.tf
+++ infrastructure/stacks/opensearch/opensearch_index_policies.tf
@@ -1,7 +1,7 @@
 resource "aws_opensearchserverless_access_policy" "opensearch_index_data_access_policy" {
   count = local.workspace_suffix == "" ? 0 : 1
 
-  name        = "${var.environment}-${var.stack_name}${local.workspace_suffix}-index-dap"
+  name        = "${var.environment}-${var.stack_name}${local.workspace_suffix}-index-dp"
   type        = "data"
   description = "Index-level access policy for OpenSearch stack ${var.stack_name}${local.workspace_suffix} (allows document read/write and index management)"
 
