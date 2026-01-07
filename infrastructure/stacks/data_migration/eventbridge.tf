8a8ec5e0d92906538fd851dad278ccc6c96049ea
nhs-shruthi-gowda <shruthi.gowda1@nhs.net>
2026-01-07 11:20:31 +0000

    feat(data-dms-setup): FTRS-1708 added the invoke of DMS lambda into event

--- infrastructure/stacks/data_migration/eventbridge.tf
+++ infrastructure/stacks/data_migration/eventbridge.tf
@@ -12,23 +12,46 @@ resource "aws_cloudwatch_event_rule" "dms_full_replication_task_completed" {
   })
 }
 
-resource "aws_cloudwatch_event_target" "dms_full_replication_task_lambda" {
+resource "aws_cloudwatch_event_target" "dms_full_replication_task_queue_populator_lambda" {
   count = local.is_primary_environment ? 1 : 0
 
-  rule = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].name
-  arn  = module.queue_populator_lambda.lambda_function_arn
+  rule      = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].name
+  target_id = "queue-populator-lambda"
+  arn       = module.queue_populator_lambda.lambda_function_arn
 
   dead_letter_config {
     arn = aws_sqs_queue.eventbridge_event_full_migration_completion_dlq[0].arn
   }
 }
 
-resource "aws_lambda_permission" "eventbridge_lambda_permission" {
+resource "aws_cloudwatch_event_target" "dms_full_replication_task_dms_db_lambda" {
   count = local.is_primary_environment ? 1 : 0
 
-  statement_id  = "AllowLambdaExecutionFromEventBridge"
+  rule      = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].name
+  target_id = "dms-db-lambda"
+  arn       = module.dms_db_lambda.lambda_function_arn
+
+  dead_letter_config {
+    arn = aws_sqs_queue.eventbridge_event_full_migration_completion_dlq[0].arn
+  }
+}
+
+resource "aws_lambda_permission" "eventbridge_queue_populator_lambda_permission" {
+  count = local.is_primary_environment ? 1 : 0
+
+  statement_id  = "AllowQueuePopulatorExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
   function_name = module.queue_populator_lambda.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].arn
+}
+
+resource "aws_lambda_permission" "eventbridge_dms_db_lambda_permission" {
+  count = local.is_primary_environment ? 1 : 0
+
+  statement_id  = "AllowDmsDbLambdaExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = module.dms_db_lambda.lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].arn
 }
