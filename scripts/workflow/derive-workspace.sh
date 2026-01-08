634236086298a9d860139d197e2beb8fed127492
timrickwood <48654585+timrickwood@users.noreply.github.com>
2026-01-08 11:17:03 +0000

    chore: FTRS-1681 Test only change to derive workspace

--- scripts/workflow/derive-workspace.sh
+++ scripts/workflow/derive-workspace.sh
@@ -52,6 +52,8 @@ else
   IFS='/' read -r -a name_array <<< "$BRANCH_NAME"
   IFS='_-' read -r -a ref <<< "${name_array[1]}"
   WORKSPACE=$(echo "${ref[0]}-${ref[1]}" | tr "[:upper:]" "[:lower:]")
+  # TODO Remove
+  WORKSPACE="default"
   echo "Workspace from feature branch: $WORKSPACE"
 fi
 
