ece8e6f9346c88f9fd690c83aa4b8adcdc6c920b
AITR1 <178915313+AITR1@users.noreply.github.com>
2026-01-08 17:24:12 +0000

    chore(terraform): FTRS-964 temp change to internal qa

--- infrastructure/stacks/etl_ods/secrets.tf
+++ infrastructure/stacks/etl_ods/secrets.tf
@@ -1,7 +1,7 @@
 resource "aws_secretsmanager_secret" "dos_ingest_jwt_credentials" {
   # checkov:skip=CKV2_AWS_57: Justification: This will be generated manually .
   count       = local.is_primary_environment ? 1 : 0
-  name        = "/${var.project}/${var.environment}/dos-ingest-jwt-credentials"
+  name        = "/${var.project}/internal-qa/dos-ingest-jwt-credentials"
   description = "JWT token generation credentials"
   kms_key_id  = data.aws_kms_key.secrets_manager_kms_key.arn
 }
