5ace1debdf309d98bd82dd17390bda240b74d36b
nhs-shruthi-gowda <shruthi.gowda1@nhs.net>
2026-01-08 16:27:35 +0000

    feat(data-dms-setup): FTRS-1708 made comments field non mandatory

--- infrastructure/stacks/data_migration/eventbridge.tf
+++ infrastructure/stacks/data_migration/eventbridge.tf
@@ -11,8 +11,8 @@ resource "aws_cloudwatch_event_rule" "dms_full_replication_task_completed" {
     }
   })
 }
-
-resource "aws_cloudwatch_event_target" "dms_full_replication_task_queue_populator_lambda" {
+# Commented out the queue populator related resources for now as for the first phase we need more control over data migration process.
+/* resource "aws_cloudwatch_event_target" "dms_full_replication_task_queue_populator_lambda" {
   count = local.is_primary_environment ? 1 : 0
 
   rule      = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].name
@@ -22,7 +22,7 @@ resource "aws_cloudwatch_event_target" "dms_full_replication_task_queue_populato
   dead_letter_config {
     arn = aws_sqs_queue.eventbridge_event_full_migration_completion_dlq[0].arn
   }
-}
+} */
 
 resource "aws_cloudwatch_event_target" "dms_full_replication_task_dms_db_lambda" {
   count = local.is_primary_environment ? 1 : 0
@@ -35,8 +35,8 @@ resource "aws_cloudwatch_event_target" "dms_full_replication_task_dms_db_lambda"
     arn = aws_sqs_queue.eventbridge_event_full_migration_completion_dlq[0].arn
   }
 }
-
-resource "aws_lambda_permission" "eventbridge_queue_populator_lambda_permission" {
+# Commented out the queue populator related resources for now as for the first phase we need more control over data migration process.
+/* resource "aws_lambda_permission" "eventbridge_queue_populator_lambda_permission" {
   count = local.is_primary_environment ? 1 : 0
 
   statement_id  = "AllowQueuePopulatorExecutionFromEventBridge"
@@ -44,7 +44,7 @@ resource "aws_lambda_permission" "eventbridge_queue_populator_lambda_permission"
   function_name = module.queue_populator_lambda.lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.dms_full_replication_task_completed[0].arn
-}
+} */
 
 resource "aws_lambda_permission" "eventbridge_dms_db_lambda_permission" {
   count = local.is_primary_environment ? 1 : 0
