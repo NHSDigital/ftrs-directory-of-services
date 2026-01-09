e31e2b61cd0fa3293edd7d79556d7f3808afb28b
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-01-09 12:16:00 +0000

    fix: FTRS-1994 Remove the deprecated argument and use the non-deprecated one

--- infrastructure/modules/app-config/data.tf
+++ infrastructure/modules/app-config/data.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "appconfig_data_read" {
       "appconfig:StartConfigurationSession",
     ]
     resources = [
-      "arn:aws:appconfig:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:application/${module.app_config.application_id}/environment/*/configuration/*"
+      "arn:aws:appconfig:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:application/${module.app_config.application_id}/environment/*/configuration/*"
     ]
   }
 }
