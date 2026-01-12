3ed60cd20eb2adfbf2385db6b3958198a55d1719
ThomasC-Kainos <106971950+ThomasC-Kainos@users.noreply.github.com>
2026-01-12 17:11:34 +0000

    feat(etl): FTRS-964 separated consumer and producer code (#647)
    * feat(etl): FTRS-964 separated consumer and producer code
    
    * feat(etl): FTRS-964 fixed spacing readme
    
    * feat(etl): FTRS-964 fixed readme commands
    
    * feat(etl): FTRS-964 fixed unit test
    
    * feat(etl): FTRS-964 separate consumer and processor lambdas into independent packages
    
    * feat(etl): FTRS-964 updated makefile
    
    * feat(etl): FTRS-964 updated etl ods lambda terraform
    
    * fix(services): FTRS-964 trivy fix
    
    * fix(etl-ods): FTRS-964 deleted validation.py and its test because it was deleted on main
    
    * fix(telecom): FTRS-964 fix dos-ui issues
    
    * fix(etl-ods): FTRS-964 fix makefile
    
    * feat(etl-ods): FTRS-964 seperated utilities file
    
    ---------
    
    Co-authored-by: Martyna <245838359+mswierszcznhs@users.noreply.github.com>
    Co-authored-by: AITR1 <178915313+AITR1@users.noreply.github.com>

---
+++ services/etl-ods/tests/unit/test_consumer_handler.py
@@ -0,0 +1,132 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from ftrs_data_layer.logbase import OdsETLPipelineLogBase
+
+from consumer.consumer_handler import consumer_lambda_handler
+
+
+@patch("consumer.consumer_handler.process_message_and_send_request")
+def test_consumer_lambda_handler_success(
+    mock_process_message: MagicMock,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    event = {
+        "Records": [
+            {"messageId": "1", "path": "test1", "body": {"key": "value1"}},
+            {"messageId": "2", "path": "test2", "body": {"key": "value2"}},
+        ]
+    }
+
+    response = consumer_lambda_handler(event, {})
+
+    assert response["batchItemFailures"] == []
+    assert str(mock_process_message.call_count) == "2"
+
+    expected_processing_log_1 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_003.value.message.format(
+            message_id="1", total_records=2
+        )
+    )
+    expected_success_log_1 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_004.value.message.format(message_id="1")
+    )
+    expected_processing_log_2 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_003.value.message.format(
+            message_id="2", total_records=2
+        )
+    )
+    expected_success_log_2 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_004.value.message.format(message_id="2")
+    )
+    assert expected_processing_log_1 in caplog.text
+    assert expected_success_log_1 in caplog.text
+    assert expected_processing_log_2 in caplog.text
+    assert expected_success_log_2 in caplog.text
+
+
+@patch("consumer.consumer_handler.process_message_and_send_request")
+def test_consumer_lambda_handler_no_event_data(mock_process_message: MagicMock) -> None:
+    consumer_lambda_handler({}, {})
+
+    assert str(mock_process_message.call_count) == "0"
+
+
+@patch("consumer.consumer_handler.process_message_and_send_request")
+def test_consumer_lambda_handler_failure(
+    mock_process_message: MagicMock,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    event = {
+        "Records": [
+            {"messageId": "1", "path": "test1", "body": {"key": "value1"}},
+            {"messageId": "2", "path": "test2", "body": {"key": "value2"}},
+        ]
+    }
+
+    mock_process_message.side_effect = [Exception("Test exception"), None]
+
+    response = consumer_lambda_handler(event, {})
+
+    assert response["batchItemFailures"] == [{"itemIdentifier": "1"}]
+    assert str(mock_process_message.call_count) == "2"
+
+    expected_processing_log_1 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_003.value.message.format(
+            message_id="1", total_records=2
+        )
+    )
+    expected_failure_log_1 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_005.value.message.format(message_id="1")
+    )
+    expected_processing_log_2 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_003.value.message.format(
+            message_id="2", total_records=2
+        )
+    )
+    expected_success_log_2 = (
+        OdsETLPipelineLogBase.ETL_CONSUMER_004.value.message.format(message_id="2")
+    )
+
+    assert expected_processing_log_1 in caplog.text
+    assert expected_failure_log_1 in caplog.text
+    assert expected_processing_log_2 in caplog.text
+    assert expected_success_log_2 in caplog.text
+
+
+@pytest.mark.parametrize(
+    ("path", "body"),
+    [
+        (None, {"name": "Organization Name"}),
+        ("uuid", None),
+        ("", {"name": "Organization Name"}),
+        ("uuid", {}),
+    ],
+)
+def test_consumer_lambda_handler_handle_missing_message_parameters(
+    path: str,
+    body: dict,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    event = {
+        "Records": [
+            {
+                "messageId": "1",
+                "path": path,
+                "body": body,
+            }
+        ]
+    }
+
+    consumer_lambda_handler(event, {})
+
+    assert any(
+        record.levelname == "WARNING"
+        and "Message id: 1 is missing 'path' or 'body' fields." in record.message
+        for record in caplog.records
+    )
+    assert any(
+        record.levelname == "ERROR"
+        and "Failed to process message id: 1." in record.message
+        for record in caplog.records
+    )
