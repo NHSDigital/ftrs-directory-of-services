f354dae10b3b7fb7e90da12f5831b7d54d563ca7
Jonathan Pearce <25200019+jope1@users.noreply.github.com>
2026-01-16 14:56:11 +0000

    feat(infra): FTRS-2601 Restrict private subnets for VPC Endpoint

--- infrastructure/stacks/account_wide/vpce.tf
+++ infrastructure/stacks/account_wide/vpce.tf
@@ -53,7 +53,7 @@ resource "aws_vpc_endpoint" "current_dos_rds_endpoint" {
   vpc_id             = module.vpc.vpc_id
   service_name       = aws_ssm_parameter.texas_vpc_endpoint_service_name.value
   vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
+  subnet_ids         = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
   security_group_ids = [aws_security_group.vpce_rds_security_group.id]
 
   tags = {
