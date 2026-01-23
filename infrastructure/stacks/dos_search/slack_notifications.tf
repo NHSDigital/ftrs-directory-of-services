7308e9cfc48ab4dbd46ea9b24bc0159d90589705
Matas <matas.kaucikas1@nhs.net>
2026-01-23 22:51:27 +0000

    feat(dos-search): FTRS-765 better aligning approach

--- infrastructure/stacks/dos_search/slack_notifications.tf
+++ infrastructure/stacks/dos_search/slack_notifications.tf
@@ -3,59 +3,71 @@
 # Flattens CloudWatch alarm JSON and sends to Slack
 ################################################################################
 
-# IAM Role for Slack notification Lambda
-resource "aws_iam_role" "slack_notification_lambda_role" {
-  name = "${local.resource_prefix}-slack-notification-lambda-role${local.workspace_suffix}"
+# Archive the Lambda function code
+data "archive_file" "slack_notification" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../../application/lambda/slack_notification"
+  excludes    = ["build.sh", "README.md", "build"]
+  output_path = "${path.module}/.terraform/slack_notification.zip"
+}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
+# IAM Policy for Slack notification Lambda
+data "aws_iam_policy_document" "slack_notification_policy" {
+  statement {
+    sid    = "AllowKMSEncryption"
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
     ]
-  })
-
-  tags = {
-    Name = "${local.resource_prefix}-slack-notification-lambda-role${local.workspace_suffix}"
+    resources = [
+      "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:alias/aws/lambda"
+    ]
   }
 }
 
-# IAM Policy for basic Lambda execution (CloudWatch logs)
-resource "aws_iam_role_policy_attachment" "slack_notification_lambda_basic_execution" {
-  role       = aws_iam_role.slack_notification_lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+# Lambda function for Slack notifications
+module "slack_notification_lambda" {
+  source                  = "../../modules/lambda"
+  function_name           = "${local.resource_prefix}-slack-notification"
+  description             = "Lambda to send CloudWatch alarms to Slack"
+  handler                 = "index.lambda_handler"
+  runtime                 = "python3.11"
+  local_existing_package  = data.archive_file.slack_notification.output_path
+  ignore_source_code_hash = false
+  timeout                 = 30
+  memory_size             = 128
+  tracing_mode            = "Active"
+  attach_tracing_policy   = true
 
-# IAM Policy for X-Ray write access
-resource "aws_iam_role_policy_attachment" "slack_notification_lambda_xray_write" {
-  role       = aws_iam_role.slack_notification_lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
-}
+  subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
+  security_group_ids = [aws_security_group.dos_search_lambda_security_group.id]
 
-# IAM Policy for accessing Slack webhook URL from environment
-resource "aws_iam_role_policy" "slack_notification_lambda_secrets_policy" {
-  name = "${local.resource_prefix}-slack-notification-env-policy${local.workspace_suffix}"
-  role = aws_iam_role.slack_notification_lambda_role.id
+  number_of_policy_jsons = "1"
+  policy_jsons = [
+    data.aws_iam_policy_document.slack_notification_policy.json,
+  ]
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
+  environment_variables = {
+    "SLACK_WEBHOOK_URL" = var.slack_webhook_url
+    "ENVIRONMENT"       = var.environment
+  }
+
+  allowed_triggers = {
+    AllowExecutionFromSNS = {
+      service    = "sns"
+      source_arn = module.sns.topic_arn
+    }
+  }
+
+  account_id     = data.aws_caller_identity.current.account_id
+  account_prefix = local.account_prefix
+  aws_region     = var.aws_region
+  vpc_id         = data.aws_vpc.vpc.id
+
+  cloudwatch_logs_retention = var.lambda_cloudwatch_logs_retention_days
+
+  depends_on = [data.archive_file.slack_notification]
 }
 
 # Dead Letter Queue for Slack notification Lambda
@@ -69,76 +81,23 @@ resource "aws_sqs_queue" "slack_notification_dlq" {
   }
 }
 
-# Lambda function for Slack notifications
-resource "aws_lambda_function" "slack_notification" {
-  filename      = "${path.module}/../../lambda_functions/slack_notification.zip"
-  function_name = "${local.resource_prefix}-slack-notification${local.workspace_suffix}"
-  role          = aws_iam_role.slack_notification_lambda_role.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.11"
-  timeout       = 30
-  tracing_config {
-    mode = "Active"
-  }
-  dead_letter_config {
-    target_arn = aws_sqs_queue.slack_notification_dlq.arn
-  }
+# Update Lambda DLQ configuration
+resource "aws_lambda_function_event_invoke_config" "slack_notification" {
+  function_name                = module.slack_notification_lambda.lambda_function_name
+  maximum_event_age_in_seconds = 3600
+  maximum_retry_attempts       = 0
 
-  environment {
-    variables = {
-      SLACK_WEBHOOK_URL = var.slack_webhook_url
-      ENVIRONMENT       = var.environment
+  destination_config {
+    on_failure {
+      type        = "SQS"
+      destination = aws_sqs_queue.slack_notification_dlq.arn
     }
   }
-
-  kms_key_arn = "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:alias/aws/lambda"
-
-  reserved_concurrent_executions = 10
-  code_signing_config_arn        = aws_lambda_code_signing_config.slack_notification.arn
-
-  vpc_config {
-    subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
-    security_group_ids = [aws_security_group.dos_search_lambda_security_group.id]
-  }
-
-  tags = {
-    Name = "${local.resource_prefix}-slack-notification${local.workspace_suffix}"
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.slack_notification_lambda_basic_execution]
-}
-
-# Lambda Code Signing Configuration
-resource "aws_lambda_code_signing_config" "slack_notification" {
-  allowed_publishers {
-    signing_profile_version_arns = ["arn:aws:signer:${var.aws_region}:${data.aws_caller_identity.current.account_id}:signing-profile/*"]
-  }
-  description = "Code signing config for slack notification Lambda"
 }
 
 # SNS subscription for Slack notification Lambda
 resource "aws_sns_topic_subscription" "slack_notification" {
   topic_arn = module.sns.topic_arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.slack_notification.arn
-}
-
-# Permission for SNS to invoke the Lambda function
-resource "aws_lambda_permission" "slack_notification_sns" {
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.slack_notification.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = module.sns.topic_arn
-}
-
-# CloudWatch log group for Slack notification Lambda
-resource "aws_cloudwatch_log_group" "slack_notification_lambda_logs" {
-  name              = "/aws/lambda/${aws_lambda_function.slack_notification.function_name}"
-  retention_in_days = var.lambda_cloudwatch_logs_retention_days
-  kms_key_id        = "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:alias/aws/logs"
-
-  tags = {
-    Name = "/aws/lambda/${aws_lambda_function.slack_notification.function_name}"
-  }
+  endpoint  = module.slack_notification_lambda.lambda_function_arn
 }
