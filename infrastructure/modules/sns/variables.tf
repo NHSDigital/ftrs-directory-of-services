400cac6cb2aa6de3199682f8ee14cdcdafbb5401
Matas <matas.kaucikas1@nhs.net>
2026-01-23 13:59:16 +0000

    feat(dos-search): FTRS-765 moving to a module

---
+++ infrastructure/modules/sns/variables.tf
@@ -0,0 +1,20 @@
+variable "topic_name" {
+  description = "Name of the SNS topic"
+  type        = string
+}
+
+variable "display_name" {
+  description = "Display name of the SNS topic"
+  type        = string
+}
+
+variable "kms_key_id" {
+  description = "KMS key ID for SNS topic encryption"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the SNS topic"
+  type        = map(string)
+  default     = {}
+}
