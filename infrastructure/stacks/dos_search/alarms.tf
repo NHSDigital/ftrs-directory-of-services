6e691d5be194fdabffb859b78e8619eb06bbfdf9
Matas <matas.kaucikas1@nhs.net>
2026-01-25 15:54:00 +0000

    feat(dos-search): FTRS-765 further tweaks

--- infrastructure/stacks/dos_search/alarms.tf
+++ infrastructure/stacks/dos_search/alarms.tf
@@ -9,16 +9,3 @@ module "sns" {
     Name = "${local.resource_prefix}-lambda-alarms${local.workspace_suffix}"
   }
 }
-
-module "appconfig_alarms" {
-  source = "./appconfig_alarms"
-
-  sns_topic_arn                     = module.sns.topic_arn
-  search_lambda_function_name       = module.lambda.lambda_function_name
-  health_check_lambda_function_name = module.health_check_lambda.lambda_function_name
-  environment                       = var.environment
-
-  tags = {
-    Name = "${local.resource_prefix}-appconfig-alarms${local.workspace_suffix}"
-  }
-}
