0f417e8c988472766681c510a79e2e045c86420a
Gurinder Brar <gurinder.brar1@nhs.net>
2026-01-27 22:00:46 +0000

    feat(waf): FTRS-766 Deploy Regional WAF for API Gateway

--- infrastructure/stacks/dos_search/api-gateway-rest-stages.tf
+++ infrastructure/stacks/dos_search/api-gateway-rest-stages.tf
@@ -2,6 +2,7 @@ resource "aws_api_gateway_stage" "default" {
   # checkov:skip=CKV2_AWS_51: False positive, the API is secured by mTLS via DNS domain certificate
   # checkov:skip=CKV2_AWS_4: False positive, we are configuring custom logging
   # checkov:skip=CKV_AWS_120: Caching breaks the tests
+  # checkov:skip=CKV2_AWS_77: Not applicable — this stack does not use Java/Log4j, so AMR for Log4j is not required
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   stage_name    = "default"
