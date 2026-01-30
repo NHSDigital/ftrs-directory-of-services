701fc31f3b09df7635fed1aadc932fda703ec0be
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-01-30 16:17:26 +0000

    fix: FTRS-26291 ETL ODS security group consolidation

--- infrastructure/stacks/etl_ods/security_group.tf
+++ infrastructure/stacks/etl_ods/security_group.tf
@@ -1,46 +1,19 @@
-# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
-resource "aws_security_group" "extractor_lambda_security_group" {
+# Consolidated security group for all ETL ODS lambdas
+# Reduces ENI consumption from 3 to 1 per workspace
+resource "aws_security_group" "etl_ods_lambda_security_group" {
   # checkov:skip=CKV2_AWS_5: False positive due to module reference
-  name        = "${local.resource_prefix}-${var.extractor_name}${local.workspace_suffix}-sg"
-  description = "Security group for etl ods extractor lambda"
-  vpc_id      = data.aws_vpc.vpc.id
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow outbound HTTPS for API access"
-  }
-}
-
-# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
-resource "aws_security_group" "transformer_lambda_security_group" {
-  # checkov:skip=CKV2_AWS_5: False positive due to module reference
-  name        = "${local.resource_prefix}-${var.transformer_name}${local.workspace_suffix}-sg"
-  description = "Security group for etl ods transformer lambda"
-  vpc_id      = data.aws_vpc.vpc.id
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow outbound HTTPS for API access"
-  }
-}
-
-
-# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
-resource "aws_security_group" "consumer_lambda_security_group" {
-  # checkov:skip=CKV2_AWS_5: False positive due to module reference
-  name        = "${local.resource_prefix}-${var.consumer_name}${local.workspace_suffix}-sg"
-  description = "Security group for etl ods consumer lambda"
+  name        = "${local.resource_prefix}-etl-ods-lambda${local.workspace_suffix}-sg"
+  description = "Security group for all etl ods lambdas (extractor, transformer, consumer)"
 
   vpc_id = data.aws_vpc.vpc.id
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow outbound HTTPS for API access"
-  }
+}
+
+# trivy:ignore:aws-vpc-no-public-egress-sgr : TODO https://nhsd-jira.digital.nhs.uk/browse/FTRS-386
+resource "aws_vpc_security_group_egress_rule" "etl_ods_allow_443" {
+  security_group_id = aws_security_group.etl_ods_lambda_security_group.id
+  description       = "ETL ODS egress rule to allow HTTPS to internet"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
 }
