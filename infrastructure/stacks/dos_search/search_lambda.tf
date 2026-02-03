1a5ea7116b10307ccfc67c174f30d6df5e8ff22f
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-02-03 11:18:29 +0000

    feat: FTRS-26293 Move lambda to non workspaced sg

--- infrastructure/stacks/dos_search/search_lambda.tf
+++ infrastructure/stacks/dos_search/search_lambda.tf
@@ -41,7 +41,7 @@ module "lambda" {
   ]
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
-  security_group_ids = [aws_security_group.dos_search_lambda_security_group.id]
+  security_group_ids = [try(aws_security_group.dos_search_lambda_security_group[0].id, data.aws_security_group.dos_search_lambda_security_group[0].id)]
 
   environment_variables = {
     "ENVIRONMENT"  = var.environment
