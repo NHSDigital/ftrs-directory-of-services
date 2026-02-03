c33a91a9ab41649d011a83e5e42ccab166939fbe
Michaela McGrath <michaela.mcgrath1@nhs.net>
2026-02-03 13:32:57 +0000

    fix(lambda): FTRS-1916 Update S3 object version references for Lambda… (#802)
    * fix(lambda): FTRS-1916 Update S3 object version references for Lambda layers
    
    * fix(common): FTRS-1916 Update the import to use the new fhir package
    
    ---------
    
    Co-authored-by: AITR1 <178915313+AITR1@users.noreply.github.com>

--- infrastructure/stacks/etl_ods/lambda.tf
+++ infrastructure/stacks/etl_ods/lambda.tf
@@ -3,9 +3,9 @@ resource "aws_lambda_layer_version" "python_dependency_layer" {
   compatible_runtimes = [var.lambda_runtime]
   description         = "Common Python dependencies for Lambda functions"
 
-  s3_bucket        = local.artefacts_bucket
-  s3_key           = "${local.artefact_base_path}/${var.project}-${var.stack_name}-python-dependency-layer.zip"
-  source_code_hash = data.aws_s3_object.python_dependency_layer.checksum_sha256
+  s3_bucket         = local.artefacts_bucket
+  s3_key            = "${local.artefact_base_path}/${var.project}-${var.stack_name}-python-dependency-layer.zip"
+  s3_object_version = data.aws_s3_object.python_dependency_layer.version_id
 }
 
 resource "aws_lambda_layer_version" "common_packages_layer" {
@@ -13,9 +13,9 @@ resource "aws_lambda_layer_version" "common_packages_layer" {
   compatible_runtimes = [var.lambda_runtime]
   description         = "Common Python dependencies for Lambda functions"
 
-  s3_bucket        = local.artefacts_bucket
-  s3_key           = "${local.artefact_base_path}/${var.project}-python-packages-layer.zip"
-  source_code_hash = data.aws_s3_object.common_packages_layer.checksum_sha256
+  s3_bucket         = local.artefacts_bucket
+  s3_key            = "${local.artefact_base_path}/${var.project}-python-packages-layer.zip"
+  s3_object_version = data.aws_s3_object.common_packages_layer.version_id
 }
 
 module "extractor_lambda" {
