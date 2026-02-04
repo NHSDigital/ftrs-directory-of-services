e9b101dce32cbc4a7d0dc5dd4d7111e0a9be61ca
timrickwood <48654585+timrickwood@users.noreply.github.com>
2026-02-04 15:32:50 +0000

    chore(firehose): FTRS-561 Add cloudwatch log group for errors

--- infrastructure/stacks/splunk_integration/cloudwatch.tf
+++ infrastructure/stacks/splunk_integration/cloudwatch.tf
@@ -1,12 +1,8 @@
-# resource "aws_cloudwatch_log_subscription_filter" "to_splunk" {
-#   for_each = var.log_groups
-#   name            = "${var.project}-${var.environment}-subscription${replace(each.value, "/", "-")}"
-#   log_group_name  = each.value
-#   filter_pattern  = "" # empty = all logs
-#   destination_arn = aws_kinesis_firehose_delivery_stream.splunk.arn
-#   role_arn        = aws_iam_role.cw_to_firehose_role.arn
-# }
-# name from module
-# data look ups
-# boolean to pass
-# build where we build log groups with logic to decide if subs needed
+
+
+resource "aws_cloudwatch_log_group" "firehose_error_log_group" {
+  count = local.is_primary_environment ? 1 : 0
+  # checkov:skip=CKV_AWS_158: Justification: Using AWS default encryption.
+  name              = "${local.resource_prefix}-${var.firehose_error_log_group_name}"
+  retention_in_days = var.firehose_logs_retention_in_days
+}
