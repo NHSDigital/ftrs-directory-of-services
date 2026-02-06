4a49538781c7c3c0f4aafbb1fdb0a9d1de4cb632
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-02-06 17:59:15 +0000

    feat: FTRS-386 Add vpce for the services

--- infrastructure/stacks/account_wide/vpce.tf
+++ infrastructure/stacks/account_wide/vpce.tf
@@ -60,3 +60,133 @@ resource "aws_vpc_endpoint" "current_dos_rds_endpoint" {
     Name = "${local.resource_prefix}-current-dos-rds-vpc-endpoint"
   }
 }
+
+resource "aws_vpc_endpoint" "sqs_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.sqs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-sqs-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-ssm-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssmmessages_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-ssmmessages-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2messages_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-ec2messages-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "apigateway_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.execute-api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-apigateway-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "kms_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.kms"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-kms-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "secretsmanager_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-secretsmanager-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "rds_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.rds"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-rds-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "appconfig_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.appconfig"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-appconfig-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "appconfigdata_vpce" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.appconfigdata"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${local.resource_prefix}-appconfigdata-vpc-endpoint"
+  }
+}
