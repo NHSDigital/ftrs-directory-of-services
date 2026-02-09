2bed9ffe49c31bb7419450b675130d58d1dff837
mmg-nhse <michaela.mcgrath1@nhs.net>
2026-02-09 08:13:50 +0000

    feat(athena): FDOS-614 Remove default values temp

--- infrastructure/stacks/athena/variables.tf
+++ infrastructure/stacks/athena/variables.tf
@@ -1,3 +1,4 @@
+
 variable "athena_spill_bucket_retention_days" {
   description = "The retention period for the Athena spill bucket"
   type        = number
@@ -13,17 +14,9 @@ variable "athena_output_bucket_retention_days" {
 variable "athena_postgres_connector_app_id" {
   description = "SAR application ID for the Athena PostgreSQL Connector"
   type        = string
-  default     = "arn:aws:serverlessrepo:us-east-1:292517598671:applications/AthenaPostgreSQLConnector"
 }
 
 variable "athena_dynamodb_connector_app_id" {
   description = "SAR application ID for the Athena DynamoDB Connector"
   type        = string
-  default     = "arn:aws:serverlessrepo:us-east-1:292517598671:applications/AthenaDynamoDBConnector"
-}
-
-variable "target_rds_credentials" {
-  description = "The secrets manager name for the target RDS credentials"
-  type        = string
-  default     = "target-rds-credentials"
 }
