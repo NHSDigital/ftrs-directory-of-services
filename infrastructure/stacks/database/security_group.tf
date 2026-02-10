fb3d1df45ca9094f3626133cabbf366b6f2a355b
Lukasz Jercha <lukasz.jercha1@nhs.net>
2026-02-10 14:35:18 +0100

    fix: FTRS-1985 checkov

--- infrastructure/stacks/database/security_group.tf
+++ infrastructure/stacks/database/security_group.tf
@@ -1,4 +1,5 @@
 resource "aws_security_group" "version_history_lambda_security_group" {
+  # checkov:skip=CKV2_AWS_5: False positive due to module reference
   count = var.version_history_enabled ? 1 : 0
 
   name        = "${local.resource_prefix}-version-history-lambda-sg${local.workspace_suffix}"
