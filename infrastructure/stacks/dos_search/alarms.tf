d0ed3546804760535dabc2f0e38720257c6482ba
Matas <matas.kaucikas1@nhs.net>
2026-02-11 16:13:15 +0000

    feat(dos-search): FTRS-765 removing circular dependency

--- infrastructure/stacks/dos_search/alarms.tf
+++ infrastructure/stacks/dos_search/alarms.tf
@@ -5,13 +5,6 @@ module "sns" {
   display_name = "DoS Search Lambda Alarms"
   kms_key_id   = null # TODO Add KMS Key ID if needed for encryption
 
-  subscriptions = {
-    slack_lambda = {
-      protocol = "lambda"
-      endpoint = module.slack_notification_lambda.lambda_function_arn
-    }
-  }
-
   tags = {
     Name = "${local.resource_prefix}-lambda-alarms${local.workspace_suffix}"
   }
--- infrastructure/stacks/dos_search/slack_notifications.tf
+++ infrastructure/stacks/dos_search/slack_notifications.tf
@@ -48,3 +48,10 @@ module "slack_notification_lambda" {
 
   policy_jsons = []
 }
+
+# SNS subscription for Slack notification Lambda
+resource "aws_sns_topic_subscription" "slack_notification" {
+  topic_arn = module.sns.topic_arn
+  protocol  = "lambda"
+  endpoint  = module.slack_notification_lambda.lambda_function_arn
+}
