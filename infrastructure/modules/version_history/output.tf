cf8343f421dc53976150d6b7e8c6636dd02d5315
Lukasz Jercha <lukasz.jercha1@nhs.net>
2026-02-13 13:12:38 +0100

    refactor: FTRS-1985 removed module

--- infrastructure/modules/version_history/output.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_function_arn" {
-  description = "The ARN of the version history Lambda function"
-  value       = module.version_history_lambda.lambda_function_arn
-}
-
-output "lambda_function_name" {
-  description = "The name of the version history Lambda function"
-  value       = module.version_history_lambda.lambda_function_name
-}
--- infrastructure/stacks/database/lambda.tf
+++ infrastructure/stacks/database/lambda.tf
@@ -21,46 +21,89 @@ resource "aws_lambda_layer_version" "data_layer" {
   s3_object_version = data.aws_s3_object.data_layer[0].version_id
 }
 
-module "version_history" {
-  count  = local.version_history_enabled
-  source = "../../modules/version_history"
+# Version History Lambda Security Group
+resource "aws_security_group" "version_history_lambda_security_group" {
+  # checkov:skip=CKV2_AWS_5: False positive due to module reference
+  count = local.version_history_enabled
 
-  resource_prefix       = local.resource_prefix
-  lambda_name           = var.version_history_lambda_name
-  lambda_handler        = var.version_history_lambda_handler
-  lambda_runtime        = var.version_history_lambda_runtime
-  lambda_timeout        = var.version_history_lambda_timeout
-  lambda_memory_size    = var.version_history_lambda_memory_size
-  lambda_logs_retention = var.version_history_lambda_logs_retention
-  batch_size            = var.version_history_batch_size
-  maximum_concurrency   = var.version_history_maximum_concurrency
+  name        = "${local.resource_prefix}-version-history-lambda-sg${local.workspace_suffix}"
+  description = "Security group for version history lambda"
 
-  artefacts_bucket         = local.artefacts_bucket
-  lambda_s3_key            = "${local.artefact_base_path}/${var.project}-data-migration-lambda.zip"
-  lambda_s3_key_version_id = data.aws_s3_object.data_migration_lambda_package[0].version_id
+  vpc_id = data.aws_vpc.vpc.id
+}
 
-  lambda_layers = [
-    aws_lambda_layer_version.python_dependency_layer[0].arn,
-    aws_lambda_layer_version.data_layer[0].arn
-  ]
+resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_version_history_lambda" {
+  count = local.version_history_enabled
+
+  security_group_id = aws_security_group.version_history_lambda_security_group[0].id
+  description       = "Version history lambda egress rule to allow DynamoDB traffic"
+  prefix_list_id    = data.aws_prefix_list.dynamodb[0].id
+  ip_protocol       = "tcp"
+  from_port         = var.https_port
+  to_port           = var.https_port
+}
+
+# Version History Lambda Function
+module "version_history_lambda" {
+  count                   = local.version_history_enabled
+  source                  = "../../modules/lambda"
+  function_name           = "${local.resource_prefix}-${var.version_history_lambda_name}"
+  description             = "Lambda to track version history for Organisation, Location, and HealthcareService table changes"
+  handler                 = var.version_history_lambda_handler
+  runtime                 = var.version_history_lambda_runtime
+  s3_bucket_name          = local.artefacts_bucket
+  s3_key                  = "${local.artefact_base_path}/${var.project}-data-migration-lambda.zip"
+  ignore_source_code_hash = false
+  s3_key_version_id       = data.aws_s3_object.data_migration_lambda_package[0].version_id
+  timeout                 = var.version_history_lambda_timeout
+  memory_size             = var.version_history_lambda_memory_size
 
   subnet_ids         = [for subnet in data.aws_subnet.private_subnets_details : subnet.id]
   security_group_ids = [aws_security_group.version_history_lambda_security_group[0].id]
 
-  version_history_table_arn = module.dynamodb_tables["version-history"].dynamodb_table_arn
+  number_of_policy_jsons = "2"
+  policy_jsons = [
+    data.aws_iam_policy_document.version_history_dynamodb_access_policy[0].json,
+    data.aws_iam_policy_document.dynamodb_stream_access_policy[0].json
+  ]
 
-  table_streams = { for table_name in ["organisation", "location", "healthcare-service"] :
-    table_name => {
-      stream_arn = module.dynamodb_tables[table_name].dynamodb_table_stream_arn
-      table_arn  = module.dynamodb_tables[table_name].dynamodb_table_arn
-    }
+  layers = [
+    aws_lambda_layer_version.python_dependency_layer[0].arn,
+    aws_lambda_layer_version.data_layer[0].arn
+  ]
+
+  environment_variables = {
+    "ENVIRONMENT"  = var.environment
+    "WORKSPACE"    = terraform.workspace == "default" ? "" : terraform.workspace
+    "PROJECT_NAME" = var.project
   }
-
-  environment    = var.environment
-  workspace      = terraform.workspace == "default" ? "" : terraform.workspace
-  project        = var.project
   account_id     = data.aws_caller_identity.current.account_id
   account_prefix = local.account_prefix
   aws_region     = var.aws_region
   vpc_id         = data.aws_vpc.vpc.id
+
+  cloudwatch_logs_retention = var.version_history_lambda_logs_retention
+}
+
+resource "aws_lambda_event_source_mapping" "version_history_streams" {
+  for_each = local.version_history_enabled == 1 ? local.table_streams : {}
+
+  event_source_arn        = each.value.stream_arn
+  function_name           = module.version_history_lambda[0].lambda_function_name
+  starting_position       = "LATEST"
+  batch_size              = var.version_history_batch_size
+  enabled                 = true
+  function_response_types = ["ReportBatchItemFailures"]
+
+  filter_criteria {
+    filter {
+      pattern = jsonencode({
+        eventName = ["MODIFY"]
+      })
+    }
+  }
+
+  depends_on = [
+    module.version_history_lambda
+  ]
 }
