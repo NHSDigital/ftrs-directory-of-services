4d22523b96fd6d1be86b50e9cb66a2e3219e4620
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-02-13 17:03:01 +0000

    fix: FTRS-386 Remove duplicate code from rebase

--- infrastructure/stacks/account_wide/vpce.tf
+++ infrastructure/stacks/account_wide/vpce.tf
@@ -47,31 +47,3 @@ resource "aws_vpc_endpoint" "interface_vpce" {
     Name = "${local.resource_prefix}-${each.key}-vpc-endpoint"
   }
 }
-
-# Lambda VPC Endpoint (required for RDS Lambda invocation)
-resource "aws_vpc_endpoint" "lambda_vpce" {
-  vpc_id              = module.vpc.vpc_id
-  service_name        = "com.amazonaws.${var.aws_region}.lambda"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
-  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
-  private_dns_enabled = true
-
-  tags = {
-    Name = "${local.resource_prefix}-lambda-vpc-endpoint"
-  }
-}
-
-# CloudWatch Logs VPC Endpoint (required for RDS log exports)
-resource "aws_vpc_endpoint" "logs_vpce" {
-  vpc_id              = module.vpc.vpc_id
-  service_name        = "com.amazonaws.${var.aws_region}.logs"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
-  security_group_ids  = [aws_security_group.vpce_interface_security_group.id]
-  private_dns_enabled = true
-
-  tags = {
-    Name = "${local.resource_prefix}-logs-vpc-endpoint"
-  }
-}
