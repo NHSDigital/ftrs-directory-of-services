ee265e7b264ee72769b74ebe400e0cee56d6ad4e
marcin-lobocki-nhs <245834202+marcin-lobocki-nhs@users.noreply.github.com>
2026-02-18 13:09:39 +0100

    fix(etl): FTRS-2078 test outcome changes

--- services/etl-ods/transformer/uuid_fetcher.py
+++ services/etl-ods/transformer/uuid_fetcher.py
@@ -19,7 +19,7 @@ transformer_uuid_logger = Logger.get(service="ods_transformer")
 def fetch_organisation_uuid(ods_code: str, message_id: str) -> str | None:
     validate_ods_code(ods_code, message_id)
     base_url = get_base_apim_api_url()
-    identifier_param = f"odsOrganisationCode|{ods_code}"
+    identifier_param = f"https://fhir.nhs.uk/Id/ods-organization-code|{ods_code}"
     organisation_get_uuid_uri = (
         base_url + "/Organization?identifier=" + identifier_param
     )
