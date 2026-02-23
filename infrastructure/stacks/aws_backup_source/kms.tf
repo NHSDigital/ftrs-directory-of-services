a58a76381efbb8d708987750ea9d04ef30813682
mmg-nhse <michaela.mcgrath1@nhs.net>
2026-02-23 12:49:31 +0000

    feat: FTRS-344 Add AWS Backup resources and configurations

--- infrastructure/stacks/aws_backup_source/kms.tf
+++ infrastructure/stacks/aws_backup_source/kms.tf
@@ -4,5 +4,5 @@ module "backup_sns_kms_key" {
   alias_name       = local.kms_aliases.backup_sns
   account_id       = data.aws_caller_identity.current.account_id
   aws_service_name = "sns.amazonaws.com"
-  description      = "Encryption key for SNS in ${var.environment} environment"
+  description      = "Encryption key for AWS Backup notifications in ${var.environment} environment"
 }
