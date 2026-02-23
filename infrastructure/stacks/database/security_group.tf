ca1c13452b6048a4f3ea4f76c07abef2e719fea3
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-02-23 11:53:38 +0000

    fix: FTRS-3198 pipeline error

--- infrastructure/stacks/database/security_group.tf
+++ infrastructure/stacks/database/security_group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "version_history_lambda_security_group" {
   # checkov:skip=CKV2_AWS_5: False positive due to module reference
-  count = local.version_history_enabled && local.is_primary_environment ? 1 : 0
+  count = local.version_history_enabled == 1 && local.is_primary_environment ? 1 : 0
 
   name        = "${local.resource_prefix}-version-history-lambda-sg"
   description = "Security group for version history lambda"
@@ -9,7 +9,7 @@ resource "aws_security_group" "version_history_lambda_security_group" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_dynamodb_access_from_version_history_lambda" {
-  count = local.version_history_enabled && local.is_primary_environment ? 1 : 0
+  count = local.version_history_enabled == 1 && local.is_primary_environment ? 1 : 0
 
   security_group_id = aws_security_group.version_history_lambda_security_group[0].id
   description       = "Version history lambda egress rule to allow DynamoDB traffic"
