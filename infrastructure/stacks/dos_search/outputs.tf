467ca06b5f25bb15bd3104d2e39195e469321a3c
Matas <matas.kaucikas1@nhs.net>
2026-03-04 17:28:10 +0000

    feat(dos-search): FTRS-765 addressing validation

--- infrastructure/stacks/dos_search/outputs.tf
+++ infrastructure/stacks/dos_search/outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_monitoring_sns_topic_arn" {
   description = "ARN of the Lambda monitoring SNS topic"
-  value       = module.lambda_monitoring.sns_topic_arn
+  value       = local.is_primary_environment ? module.lambda_monitoring[0].sns_topic_arn : ""
 }
 
 output "lambda_monitoring_sns_topic_name" {
   description = "Name of the Lambda monitoring SNS topic"
-  value       = module.lambda_monitoring.sns_topic_name
+  value       = local.is_primary_environment ? module.lambda_monitoring[0].sns_topic_name : ""
 }
