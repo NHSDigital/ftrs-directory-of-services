b101dedaee8647efa8f7f9723c9430d5a0cdc252
Lukasz Jercha <lukasz.jercha1@nhs.net>
2026-03-06 12:20:08 +0100

    feat: FTRS-3208 include error response

--- services/data-migration/src/service_migration/processor.py
+++ services/data-migration/src/service_migration/processor.py
@@ -251,6 +251,8 @@ class DataMigrationProcessor:
                 self.logger.log(
                     DataMigrationLogBase.DM_ETL_041,
                     transact_items=transact_items,
+                    error=str(e),
+                    response=e.response if hasattr(e, "response") else None,
                 )
                 raise
 
