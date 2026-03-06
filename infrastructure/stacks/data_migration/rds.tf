6ad8e29d14be2db9df87ee7af2eee5bca1bc545a
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-03-06 16:04:12 +0000

    feat: FTRS-3308 Release pipeline enhancements (#972)

--- infrastructure/stacks/data_migration/rds.tf
+++ infrastructure/stacks/data_migration/rds.tf
@@ -84,6 +84,8 @@ module "rds_replication_target_db" {
   master_password             = random_password.rds_password[0].result
   database_name               = var.target_rds_database
 
+  preferred_backup_window = ""
+
   tags = local.backup_tags
 
   iam_database_authentication_enabled = true
