124da436efb7cae28912f5e708b4f4dd63081f5c
Matas <matas.kaucikas1@nhs.net>
2026-03-09 15:26:25 +0000

    feat(dos-search): FTRS-765 allowing sns for workspace

--- infrastructure/stacks/dos_search/outputs.tf
+++ infrastructure/stacks/dos_search/outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_monitoring_sns_topic_arn" {
   description = "ARN of the Lambda monitoring SNS topic"
-  value       = local.is_primary_environment ? module.lambda_monitoring[0].sns_topic_arn : ""
+  value       = module.lambda_monitoring[0].sns_topic_arn
 }
 
 output "lambda_monitoring_sns_topic_name" {
   description = "Name of the Lambda monitoring SNS topic"
-  value       = local.is_primary_environment ? module.lambda_monitoring[0].sns_topic_name : ""
+  value       = module.lambda_monitoring[0].sns_topic_name
 }
