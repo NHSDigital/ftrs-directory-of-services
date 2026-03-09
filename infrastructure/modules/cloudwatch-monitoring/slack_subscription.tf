fbd2fe8a0c19de8910fdb006fa0816ea85e0be01
Matas <matas.kaucikas1@nhs.net>
2026-03-09 16:42:45 +0000

    feat(dos-search): FTRS-765 remove count indexes

--- infrastructure/modules/cloudwatch-monitoring/slack_subscription.tf
+++ infrastructure/modules/cloudwatch-monitoring/slack_subscription.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_permission" "allow_sns_invoke" {
   # count         = var.slack_notifier_enabled ? 1 : 0
   statement_id  = "AllowExecutionFromSNS-${var.resource_prefix}"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.slack_notifier[0].function_name
+  function_name = data.aws_lambda_function.slack_notifier.function_name
   principal     = "sns.amazonaws.com"
   source_arn    = aws_sns_topic.alarms.arn
 }
@@ -16,7 +16,7 @@ resource "aws_sns_topic_subscription" "alarms_to_slack" {
   # count     = var.slack_notifier_enabled ? 1 : 0
   topic_arn = aws_sns_topic.alarms.arn
   protocol  = "lambda"
-  endpoint  = data.aws_lambda_function.slack_notifier[0].arn
+  endpoint  = data.aws_lambda_function.slack_notifier.arn
 
   depends_on = [aws_lambda_permission.allow_sns_invoke]
 }
