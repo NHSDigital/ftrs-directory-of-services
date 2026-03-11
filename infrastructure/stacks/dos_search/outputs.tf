848c287f77c1bde01a37adc6b794bed88688a6a6
Matas <matas.kaucikas1@nhs.net>
2026-03-11 16:38:42 +0000

    feat(dos-search): FTRS-2230 update outputs

--- infrastructure/stacks/dos_search/outputs.tf
+++ infrastructure/stacks/dos_search/outputs.tf
@@ -1,9 +1,19 @@
 output "lambda_monitoring_sns_topic_arn" {
   description = "ARN of the Lambda monitoring SNS topic"
-  value       = module.lambda_monitoring.sns_topic_arn
+  value       = try(module.lambda_monitoring[0].sns_topic_arn, null)
 }
 
 output "lambda_monitoring_sns_topic_name" {
   description = "Name of the Lambda monitoring SNS topic"
-  value       = module.lambda_monitoring.sns_topic_name
+  value       = try(module.lambda_monitoring[0].sns_topic_name, null)
+}
+
+output "api_gateway_monitoring_sns_topic_arn" {
+  description = "ARN of the API Gateway monitoring SNS topic"
+  value       = module.api_gateway_monitoring.sns_topic_arn
+}
+
+output "api_gateway_monitoring_sns_topic_name" {
+  description = "Name of the API Gateway monitoring SNS topic"
+  value       = module.api_gateway_monitoring.sns_topic_name
 }
