4915c8c77216c0db1d55f72a331b67989b4c498b
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2026-03-12 17:00:14 +0000

    chore(ods-etl): FTRS-35881 FTRS-3588 Change batching window on etl lambdas

--- infrastructure/stacks/etl_ods/lambda.tf
+++ infrastructure/stacks/etl_ods/lambda.tf
@@ -135,7 +135,7 @@ resource "aws_lambda_event_source_mapping" "transform_queue_trigger" {
     maximum_concurrency = 21
   }
 
-  maximum_batching_window_in_seconds = 2
+  maximum_batching_window_in_seconds = 1
 
   depends_on = [
     module.transformer_lambda
@@ -203,7 +203,7 @@ resource "aws_lambda_event_source_mapping" "consumer_queue_trigger" {
     maximum_concurrency = 21
   }
 
-  maximum_batching_window_in_seconds = 2
+  maximum_batching_window_in_seconds = 1
 
   depends_on = [
     module.consumer_lambda
