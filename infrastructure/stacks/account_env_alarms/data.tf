f236ee516d1568d3c653056973811c54421f341b
ri-nhs <198660903+ri-nhs@users.noreply.github.com>
2026-03-13 18:37:43 +0000

    fix: FTRS-330 remove the unnecessary data source

--- infrastructure/stacks/account_env_alarms/data.tf
+++ infrastructure/stacks/account_env_alarms/data.tf
@@ -85,11 +85,7 @@ data "aws_vpc_endpoint" "logs_vpce" {
 
 data "aws_vpc_endpoint" "current_dos_rds_endpoint" {
   vpc_id       = data.aws_vpc.vpc.id
-  service_name = data.aws_ssm_parameter.texas_vpc_endpoint_service_name.name
-}
-
-data "aws_ssm_parameter" "texas_vpc_endpoint_service_name" {
-  name = "/${local.project_prefix}-account-wide/texas-vpc-endpoint-service-name"
+  service_name = "${local.project_prefix}-account-wide-current-dos-rds-vpc-endpoint"
 }
 
 data "aws_acm_certificate" "custom_domain_api_cert" {
